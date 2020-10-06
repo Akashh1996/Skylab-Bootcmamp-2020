127c7fa22e25ef27e8481d498815c2d0a2f1ec08
Jorge <jorgengarriga@gmail.com>
2020-10-06 16:40:11 +0200

    Modificaciones Lecture del dia Modificacion Conecta 4 por sonarqube

---
+++ jorge-neira/hola-mundo/script.js
@@ -0,0 +1,21 @@
+function sayHi() {
+  let name = window.prompt('Escribe tu Nombre');
+  name = name.trim();
+	if (name === "") {
+		document.getElementById(
+			'displayGreeting'
+		).textContent = `Que pena, no has puesto nada!`;
+	} else {
+		document.getElementById(
+			'displayGreeting'
+		).textContent = `Buenos dias ${name}`;
+	}
+}
+
+function loadData() {
+  const data = fetch("heroes.txt").then((response) => {
+    const text = response.text().then(value => {
+      document.getElementById("data").innerText = value;
+    })
+  })
+}
