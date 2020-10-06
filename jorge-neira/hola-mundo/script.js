0d02403fec049f53612c4252d5afea6bc7b1ac62
Jorge Neira <jorgengarriga@gmail.com>
2020-10-06 17:14:36 +0200

    Last lecture modification

--- jorge-neira/hola-mundo/script.js
+++ jorge-neira/hola-mundo/script.js
@@ -12,10 +12,19 @@ function sayHi() {
 	}
 }
 
-function loadData() {
-  const data = fetch("heroes.txt").then((response) => {
-    const text = response.text().then(value => {
-      document.getElementById("data").innerText = value;
-    })
-  })
+// function loadData() {
+//   const data = fetch("heroes.txt").then((response) => {
+//     const text = response.text().then(value => {
+//       document.getElementById("data").innerText = value;
+//     })
+//   })
+// }
+
+async function loadData() {
+  const response = await fetch("heroes.txt")
+  const text = await response.text();
+      document.getElementById("data").innerText = text;
+
 }
+
+
