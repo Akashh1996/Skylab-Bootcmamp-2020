1f539497265b4d16b931b54ee5e57233aa1daded
Akash Sapkota <akashh.sapkota@gmail.com>
2020-10-06 17:35:27 +0200

    js actualizado

--- akash-sapkota/hola-mundo/index.js
+++ akash-sapkota/hola-mundo/index.js
@@ -18,5 +18,5 @@ function sayHi(){
 async function loadData(){
     const response = await fetch("heroes.txt")
         const text = await response.text()
-            document.querySelector(".data").innerHTML = text
+            document.querySelector(".data").innerHTML = value
 }
