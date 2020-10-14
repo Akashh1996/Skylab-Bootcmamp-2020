152f6d8e9a5d8bdb58d8a1efb0c84e3970ea8f75
Eric Martinez <martinezjimenezeric@gmail.com>
2020-10-14 17:29:25 +0200

    lecture deepClone

--- eric-martinez/js_equals/equals-object.js
+++ eric-martinez/js_equals/equals-object.js
@@ -1,45 +1,18 @@
-/* Strict equals function*/
-
-function strictEquals (a, b) {
-    if (Object.is(a,NaN) || Object.is(NaN,b)) {
-        return false;
-    } else if (Object.is(a,-0) || Object.is(b,-0)) {
-        return true;
-    } else if ((Object.is(a,0) || Object.is(b,0))) {
-        return true;
-    } else {
-        return Object.is(a, b)
-    }
-}
-//module.exports = strictEquals;
-
-
-/* Suma function*/
-function sum (a, b){
-    return a + b;
-}
-//module.exports = sum;
-
-
-/* Object function*/
-/* let x={id:10,name:'Eric',adress:300}
-function deepClone (a) {
-let b = Object.assign({},a);
-return b;
-}
-let y= deepClone(x);
-x.adress = 500 */
 
 function deepClone (original){
+    let newObject = {};
+    let property;
     if(!original){
         return original;
     }else{
-        if(typeof original === 'object') {
-
-        }else {
-            return {...original};
+        for(property in original){
+            if (typeof original[property]=== 'object'){
+                newObject[property] = deepClone(original[property]);
+            }else{
+                newObject[property] = original[property]
+            }
         }
-        return original;
+        return newObject;
     }
 }
 
