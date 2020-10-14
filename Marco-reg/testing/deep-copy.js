11e21a6ffd238fbf0c755d0a21c69325628bdbae
Marco-Reg <marcoregueiro@gmail.com>
2020-10-14 17:15:49 +0200

    testing

--- Marco-reg/testing/deep-copy.js
+++
@@ -1,7 +0,0 @@
-let car={wheels:4, color:"red",passangers:3}
-
-function clone(x){
-    const noClone=Object.assign({},x,{wheels:5 ,color:"blue",passangers:2});
-    return noClone;
-}
-module.exports=clone;
---
+++ Marco-reg/testing/deepClone.js
@@ -0,0 +1,7 @@
+function deepClone(original){
+    let objectClone=Object.assign({}, original,{skill:{}})
+    if(!original){
+        return original;
+    } else if(o) 
+}
+module.exports=deepClone;
