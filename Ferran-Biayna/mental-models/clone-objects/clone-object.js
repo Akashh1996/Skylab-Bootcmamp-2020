deb9b410a156cc9395e2435a4f02d072b0d3f075
fbiayna <ferranbiaynacosta@gmail.com>
2020-10-14 16:46:22 +0200

    lecture unit-testing final

--- Ferran-Biayna/mental-models/clone-objects/clone-object.js
+++ Ferran-Biayna/mental-models/clone-objects/clone-object.js
@@ -1,6 +1,9 @@
 function cloningObjects(object) {
 	let cloningObject = {};
 	let property;
+	if (!object) {
+		return object
+	}
 	for (property in object) {
 		if (typeof object[property] === 'object') {
 			cloningObject[property] = cloningObjects(object[property]);
