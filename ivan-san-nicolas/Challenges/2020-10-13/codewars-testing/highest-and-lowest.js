3473f82246a5880baf82aa9e1b0fb711e79b0d3a
Ivansannicolas <ivohan8@gmail.com>
2020-10-14 22:03:51 +0200

    highest-and-lowest.js done not tested

--- ivan-san-nicolas/Challenges/2020-10-13/codewars-testing/highest-and-lowest.js
+++ ivan-san-nicolas/Challenges/2020-10-13/codewars-testing/highest-and-lowest.js
@@ -13,7 +13,22 @@ and highest number is first.
 //////////////////////*/
 
 function highAndLow(numbers) {
-    // ...
+    debugger;
+    let newStr = '';
+    let maxNum = 0;
+    let minNum = 0;
+    let actualNum = '';
+    for (let i in numbers) {
+        if (numbers[i] !== ' ') actualNum += numbers[i];
+        else if (numbers[i] === ' ' && actualNum !== '') {
+            actualNum = +actualNum;
+            if (actualNum > maxNum) maxNum = actualNum;
+            else if (actualNum < minNum) minNum = actualNum;
+            actualNum = '';
+        }
+    }
+    newStr = `${maxNum} ${minNum}`;
+    return newStr;
 }
 
 module.exports = highAndLow;
