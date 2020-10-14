1afab204b37e6247588f9a913e39c2cf251ba079
Jorge <jorgengarriga@gmail.com>
2020-10-14 23:22:02 +0200

    casi casi

--- jorge-neira/Daily Challenge/CodewarsJest/happynumber.js
+++ jorge-neira/Daily Challenge/CodewarsJest/happynumber.js
@@ -1,12 +1,14 @@
 function isHappy(n, pow) {
-	n = n.toString().split('');
-	if (n.length === 1) {
-		return isHappy(n * pow);
-	} else if (n.length > 1) {
-    n.forEach(numArray => {
-      return 
-    });
-  }
+	let countArray = [];
+	if (n !== 1) {
+		let nums = n.toString().split('');
+		let sumNums = nums.reduce((acc, cur) => cur ** pow + acc, 0);
+		countArray.push(sumNums);
+		if (countArray[countArray.length - 1] === countArray) return countArray;
+		return isHappy(sumNums, pow);
+	} else {
+		return n;
+	}
 }
 
 export default isHappy;
