9167f75554d007539fa876440b7967fc83944d75
Darina Rybalchenko <dari.ryb@gmail.com>
2020-10-14 12:36:32 +0200

    add first test sum

---
+++ Darina-Rybalchenko/Lectures/strict-equals/sum.test.js
@@ -0,0 +1,15 @@
+const { describe } = require('yargs');
+const sum = require('./sum')
+
+describe('Sum'), () => {
+    test('should sum 1 + 2 equals 3', () => {
+        expect(sum(1, 2)).toBe(3)
+    });
+
+    test('should sum 1 + 2 equals 3', () => {
+        expect(sum(3, 5)).toBe(8)
+    });
+
+}
+
+
