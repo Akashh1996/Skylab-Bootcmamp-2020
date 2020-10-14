eb0377459098aa080bb28fb44cb832afc9366e3b
Jorge <jorgengarriga@gmail.com>
2020-10-14 21:32:34 +0200

    finish findtheoddint codewars

--- jorge-neira/Daily Challenge/CodewarsJest/disemvowel-trolls.test.js
+++ jorge-neira/Daily Challenge/CodewarsJest/disemvowel-trolls.test.js
@@ -1,5 +1,5 @@
-import disemvowel from './disemvowel-trolls';
-
+// import disemvowel from './disemvowel-trolls';
+const disemvowel = require('./disemvowel-trolls');
 describe('disemvowels CodeWars', () => {
 	test('take off all the vowels form a string', () => {
 		expect(disemvowel('This website is for losers LOL!')).toBe(
---
+++ jorge-neira/Daily Challenge/CodewarsJest/findTheOddInt.test.js
@@ -0,0 +1,7 @@
+import findOdd from './findTheOddInt';
+
+describe('Given an array of integers, find the one that appears an odd number of times', () => {
+	test('', () => {
+		expect(funcName('')).toBe('');
+	});
+});
