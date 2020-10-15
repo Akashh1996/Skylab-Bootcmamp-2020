17624b5ddcb931244f27af07a0a6355efcd6a555
Ivan Dobry <ivandobry@hotmail.es>
2020-10-15 21:35:01 +0200

    array methods

---
+++ ivan-dobry/Lectures/challenges/array-methods/arrayMethods.js
@@ -0,0 +1,23 @@
+let obj = {
+	0: 'hola',
+	1: 'adios',
+	2: 'adios',
+	3: 'adios',
+	length: 4
+};
+function mySecondMap(object, callback) {
+	const returnObject = {};
+	Object.entries(object).forEach((property) => {
+        if (property[0] === 'length') {
+            return returnObject;
+        }else{
+        returnObject[property[0]] = callback(property[1]);
+        }
+	});
+	return returnObject;
+}
+const result = mySecondMap(obj, (value) => {
+	return value + 'los callbacks son guais';
+});
+console.log(result);
+module.exports = obj;
