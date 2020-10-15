cb2b39a1bbe283028a97ab88c883a56cedbe2b7b
Eric Martinez <martinezjimenezeric@gmail.com>
2020-10-15 09:44:53 +0200

    challenge/unit-testing

--- eric-martinez/unit-testing/unit-testing.js
+++ eric-martinez/unit-testing/unit-testing.js
@@ -34,3 +34,18 @@ function disemvowel(str) {
   }
 
   module.exports = findOdd;
+
+  function lowestProduct(input) {
+    let array = input.split('').map(number => parseInt(number));
+    let lowest = [];
+    if (input.length < 4) {
+        return 'Number is too small';
+    } else {
+        for (let i = 0; i < array.length; i++) {
+            lowest.push(array[i] * array[i+1] * array[i+2] * array[i+3]);
+        }
+        return lowest.sort((a,b) => a - b)[0];
+    }
+};
+
+    module.exports = lowestProduct;  
--- eric-martinez/unit-testing/unit-testing.test.js
+++ eric-martinez/unit-testing/unit-testing.test.js
@@ -42,4 +42,18 @@ xdescribe('findOdd', () => {
         //assets
         expect(response).toBe(5);
 	});
+});
+
+const lowestProduct = require('./lowest-product');
+
+xdescribe ('lowestProduct', () => {
+    test('should return the odd number in the array', () => {
+        let input = '123456789';
+        expect(lowestProduct(input)).toBe(24);
+    });
+
+    test('should return "Number is too small" when there is only 3 digits', () => {
+        input = '567';
+        expect(lowestProduct(input)).toBe('Number is too small');
+    })
 }); 
