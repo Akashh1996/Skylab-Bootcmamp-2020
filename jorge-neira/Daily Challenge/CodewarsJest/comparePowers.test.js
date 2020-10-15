0cbf182ab0edf00679457be4065b13836931ce8a
Jorge <jorgengarriga@gmail.com>
2020-10-15 17:18:53 +0200

    ...

--- jorge-neira/Daily Challenge/CodewarsJest/comparePowers.test.js
+++ jorge-neira/Daily Challenge/CodewarsJest/comparePowers.test.js
@@ -2,24 +2,28 @@ const comparePowers = require('./comparePowers');
 
 describe('You certainly can tell which is the larger number between 210 and 215.', () => {
 	test('Compare both powers', () => {
-		const asw = ([2, 10], [2, 15]);
-		const response = comparePowers(asw);
-		expect(response).toStrictEqual(1);
+		const arr1 = [2, 10];
+		const arr2 = [2, 15];
+		const response = comparePowers(arr1, arr2);
+		expect(response).toBe(1);
 	});
 	test('Compare both powers', () => {
-		const asw = ([2, 10], [3, 10]);
-		const response = comparePowers(asw);
-		expect(response).toStrictEqual(1);
+		const arr1 = [2, 10];
+		const arr2 = [3, 10];
+		const response = comparePowers(arr1, arr2);
+		expect(response).toBe(1);
 	});
 	test('Compare both powers', () => {
-		const asw = ([2, 10], [2, 10]);
-		const response = comparePowers(asw);
-		expect(response).toStrictEqual(0);
+		const arr1 = [2, 10];
+		const arr2 = [2, 10];
+		const response = comparePowers(arr1, arr2);
+		expect(response).toBe(0);
 	});
 	test('Compare both powers', () => {
-		const asw = ([3, 9], [5, 6]);
-		const response = comparePowers(asw);
-		expect(response).toStrictEqual(-1);
+		const arr1 = [3, 9];
+		const arr2 = [5, 6];
+		const response = comparePowers(arr1, arr2);
+		expect(response).toBe(-1);
 	});
 	test('Compare both powers', () => {
 		const asw = ([7, 7], [5, 8]);
@@ -27,3 +31,5 @@ describe('You certainly can tell which is the larger number between 210 and 215.
 		expect(response).toStrictEqual(-1);
 	});
 });
+
+const comparePowers = require('./comparePowers');
