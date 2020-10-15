ae7bdefe3ce4df27b4b1f8cd4fc2a803fc2da59d
Jorge <jorgengarriga@gmail.com>
2020-10-15 09:23:53 +0200

    codewars challenge push what i finish

--- jorge-neira/Daily Challenge/CodewarsJest/happynumber.js
+++ jorge-neira/Daily Challenge/CodewarsJest/happynumber.js
@@ -1,13 +1,10 @@
 function isHappy(n, pow) {
 	let countArray = [];
-	if (n.length === 1) {
-		let nums = n.toString().split('');
-		let sumNums = nums.reduce((acc, cur) => cur ** pow + acc, 0);
-		countArray.push(sumNums);
-		if (countArray[countArray.length - 1] === countArray) return countArray;
+	let nums = n.toString().split('');
+	let sumNums = nums.reduce((acc, cur) => cur ** pow + acc, 0);
+	countArray.push(sumNums);
+	if (countArray[countArray.length - 1] === countArray) return countArray;
 		return isHappy(sumNums, pow);
-	} else {
-		return n;
 	}
 }
 
