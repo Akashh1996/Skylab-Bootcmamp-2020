9ff60ef33c561d8e03fba8d5a05792883e3a6826
Jorge <jorgengarriga@gmail.com>
2020-10-15 09:38:52 +0200

    removed comment con stric-equals-test

--- jorge-neira/Lecture/unit-testing/stric-equals.test.js
+++ jorge-neira/Lecture/unit-testing/stric-equals.test.js
@@ -66,27 +66,3 @@ xdescribe('Strict Equals comparison', () => {
 		expect(response).toBe(false);
 	});
 });
-// test('should compare 1 and 1', () => {
-// 	expect(strictEquals(1, 1)).toBe(true);
-// });
-// test('should compare NaN and NaN', () => {
-// 	expect(strictEquals(NaN, NaN)).toBe(false);
-// });
-// test('should compare 0 and -0', () => {
-// 	expect(strictEquals(0, -0)).toBe(true);
-// });
-// test('should compare -0 and 0', () => {
-// 	expect(strictEquals(-0, 0)).toBe(true);
-// });
-// test(`should compare 1 and "1"`, () => {
-// 	expect(strictEquals(1, '1')).toBe(false);
-// });
-// test('should compare true and false', () => {
-// 	expect(strictEquals(true, false)).toBe(false);
-// });
-// test('should compare false and false', () => {
-// 	expect(strictEquals(false, false)).toBe(true);
-// });
-// test(`should compare "Water" and "oil"`, () => {
-// 	expect(strictEquals('Water', 'Oil')).toBe(false);
-// });
