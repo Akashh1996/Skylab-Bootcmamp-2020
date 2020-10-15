050527abcf364bc3b130960b32ae1d443d731dca
Jorge <jorgengarriga@gmail.com>
2020-10-15 02:06:13 +0200

    happynumber in progress

--- jorge-neira/Daily Challenge/CodewarsJest/happynumbers.test.js
+++ jorge-neira/Daily Challenge/CodewarsJest/happynumbers.test.js
@@ -1,10 +1,12 @@
-import isHappy from './happynumber';
+const myJestTest = require('./happynumber');
 
-describe('A happy number is one where if you repeatedly square its digits and add them together, you eventually end up at 1.', () => {
-	test('return happy number of', () => {
-		expect(funcName('7, 2')).toBe([1]);
-	});
-	test('return happy number of', () => {
-		expect(funcName('42, 2')).toBe([42, 20, 4, 16, 37, 58, 89, 145, 42]);
-	});
+describe('', () => {
+  test('', () => {
+    //arrange
+    const a;
+    //act
+    const response = myJestTest(a);
+    //assert
+    expect(response).toBe('');
+  });
 });
