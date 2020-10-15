3e9f619103b462a8b2242fab7f52fb3d6f2b418e
Jorge <jorgengarriga@gmail.com>
2020-10-15 09:57:29 +0200

    fix complete test description

--- jorge-neira/Daily Challenge/CodewarsJest/happynumber.js
+++ jorge-neira/Daily Challenge/CodewarsJest/happynumber.js
@@ -4,8 +4,7 @@ function isHappy(n, pow) {
 	let sumNums = nums.reduce((acc, cur) => cur ** pow + acc, 0);
 	countArray.push(sumNums);
 	if (countArray[countArray.length - 1] === countArray) return countArray;
-		return isHappy(sumNums, pow);
-	}
+	return isHappy(sumNums, pow);
 }
 
 export default isHappy;
--- jorge-neira/Daily Challenge/CodewarsJest/simplePigLatin.test.js
+++ jorge-neira/Daily Challenge/CodewarsJest/simplePigLatin.test.js
@@ -1,7 +1,7 @@
 const myJestTest = require('./simplePigLatin');
 
 describe('Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.', () => {
-	test('should...', () => {
+	test('should return move first letter to the end and add ay to it', () => {
 		//arrange
 		const a = 'Pig latin is cool';
 		//act
@@ -9,7 +9,7 @@ describe('Move the first letter of each word to the end of it, then add "ay" to 
 		//assert
 		expect(response).toBe('igPay atinlay siay oolcay');
 	});
-	test('should...', () => {
+	test('should return move first letter to the end and add ay to it', () => {
 		//arrange
 		const a = 'This is my string';
 		//act
