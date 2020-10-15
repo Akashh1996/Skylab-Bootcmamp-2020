9ab03f01bbed9a2476cb9a1bc10563cd300eb7b5
Ivan Dobry <ivandobry@hotmail.es>
2020-10-15 11:17:45 +0200

    codewars update

--- ivan-dobry/Lectures/challenges/unit-testing/lowestProduct.js
+++ ivan-dobry/Lectures/challenges/unit-testing/lowestProduct.js
@@ -1,13 +1,17 @@
-function lowestProduct(num) { 
-    let total = 1;
-    let i = 0;
-  if (num.length < 4) {
-      return "Number is too small";
-  } else {
-    let newArr = num.split('').map(x=>+x);
-
-    return total;
+function lowestProduct(input) {
+  let separate = input.split("");
+  if(separate.length < 4) {
+    return "Number is too small";
   }
+  let cell = [];
+  for(let i = 0; i < separate.length; i++) {
+    cell.push(separate[i] * separate[i + 1] * separate[i + 2] * separate[i + 3]);
+  }
+  let order = cell.sort(compareNumbers);
+  return order[0];
+}
+function compareNumbers(a, b) {
+    return a - b;
 }
 
 module.exports = lowestProduct;
