a48499193f1b9895af97583ff61d0f7a2bb578e7
Jheavi <javierlaso213@hotmail.com>
2020-10-16 11:14:09 +0200

    added some more tests and removed from the function file

--- javi-laso/array-methods/some-function.js
+++ javi-laso/array-methods/some-function.js
@@ -10,7 +10,4 @@ Object.prototype.someFunction = function(callback) {
     return false;
 }
 
-const skylabArray = {0: 1, 1: 16, 2:0, 3:1, 4: 9, 5:3, length: 6};
-console.log(skylabArray.someFunction((value) => value === 1));
-
 //El método some() comprueba si al menos un elemento del array cumple con la condición implementada por la función proporcionada.
