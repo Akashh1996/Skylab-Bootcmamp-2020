045aab1807515c3420aa3f49d974186db0af2aff
Akash Sapkota <akashh.sapkota@gmail.com>
2020-10-17 01:28:30 +0200

    lecture about destructing,proto etc

--- akash-sapkota/Lecture/week2/destructing-day4/index.js
+++ akash-sapkota/Lecture/week2/destructing-day4/index.js
@@ -90,3 +90,168 @@ skylabArray['length'] = 2;
 
 
 //crear un prototype donde vas a implementar las arrays que vas a crear
+
+//hay 3 scoopes diferentes en Javascrypt - Global scope, function scope y block scope.
+//el scope global dentro de un script, es un objeto vacio.
+//el scope global del navegador es la ventana. El objeto de la ventana.
+
+//las var tienen hoisting, por lo que se puede acceder a esa variable antes de que este declarada, pero sin saber su valor, dando undefined.
+// en cambio let y const da reference Error.
+
+//Un callback es una funcion que es recibida como argumento por una funcion.
+//Un clousure es una funcion declarada dentro de otra funcion.
+
+function sum(a, b) {
+
+    function myLog(customLogFn) {
+        customLogFn('el primer valor introducido es: ', a);
+        customLogFn('el segundo valor introducido es: ', b);
+        customLogFn('el resultado es: ', a + b);
+    }
+    myLog(console.log);
+}
+
+sum(1, 2);
+
+
+//--------------------------------------------------------------------------------
+
+function sum(a, b) {
+
+    function myLog(customLogFn) {
+        customLogFn('el primer valor introducido es: ', a);
+        customLogFn('el segundo valor introducido es: ', b);
+        customLogFn('el resultado es: ', a + b);
+    }
+    return myLog;
+}
+
+sum(1, 2)(console.log);
+
+//--------------------------------------------------------------------------------
+
+
+function sum(a, b) {
+
+    function myLog(customLogFn) {
+        customLogFn('el primer valor introducido es: ', a);
+        customLogFn('el segundo valor introducido es: ', b);
+        customLogFn('el resultado es: ', a + b);
+    }
+    return {
+        myLog
+    };
+}
+
+sum(1, 2).myLog(console.log);
+
+//--------------------------------------------------------------------------------
+
+function sum(a, b) {
+
+    function myLog(customLogFn) {
+        return {
+            log: () => customLogFn('El resultado es:', a + b)
+        }
+    }
+    return {
+        myLog
+    };
+}
+
+sum(1, 2).myLog(console.log).log();
+
+
+//--------------------------------------------------------------------------------
+
+function sum(a, b) {
+
+    function myLog(customLogFn) {
+        return {
+            log: () => customLogFn('El resultado es:', a + b)
+        }
+    }
+    return {
+        x: myLog
+    };
+}
+
+sum(1, 2).x(console.log).log();
+
+//--------------------------------------------------------------------------------
+
+function sum(a, b) {
+
+    function myLog(customLogFn) {
+        return () => customLogFn('El resultado es:', a + b)
+    }
+    return {
+        x: myLog
+    };
+}
+
+sum(1, 2).x(console.log)();
+
+
+//--------------------------------------------------------------------------------
+
+function sum(a, b) {
+
+    function log(customLogFn) {
+        return () => customLogFn('El resultado es:', a + b)
+    }
+    return log;
+}
+
+sum(1, 2)(console.log)();
+
+
+//--------------------------------------------------------------------------------
+
+function sum(a, b) {
+
+    function log(customLogFn) {
+        return {
+            a: {
+                x: () => customLogFn('El resultado es: ', a + b)
+            }
+        };
+    }
+    return log;
+}
+
+sum(1, 2)(console.log).a.x();
+
+
+let mammal = {
+    brainy: true
+}
+let human = {
+    teeth: 32,
+    __proto__: mammal
+};
+let gwen = {
+    age: 19,
+    __proto__: human
+}; //__proto__ coge las propiedades de human, apuntando a su objecto, no a human variable.
+gwen.teeth = 31; // al cambiar la propiedad de gwen, no se modifica la del prototype.
+//Prototype Shadowing: 
+//Javascript siempre mira primero si el objeto tiene esa propiedad, si la tiene no va al prototype, sino, entra prototype a buscarla.
+//.hasOwnProperty('propiedad'); nos dice si el objecto tiene esa propiedad en su objeto o si la hereda de algun otro objecto con prototype.
+
+
+//__proto__ = null, convierte al objeto en un objeto sin prototype.
+
+//Prototype pollution:
+//si a partir del objecto original se hace algun cambio en el __proto__ object original, los objetos que apunten a ese proto de ese objeto tambien se agregaran a sus propiedades.
+//pero ninguno de ellos lo tendra como .hasOwnProperty.
+//intentar no hacer prototype pollution. Bad coding.
+
+class Spiderman { // la class nos sirve como base para crear otros nuevos objetos.
+    lookout() {
+        alert('My Spider-sense is tingling.');
+    }
+};
+
+let miles = new Spiderman();
+miles.lookout();
