43d0d33329ec9b4ab73202a367482350b5887f81
Jorge <jorgengarriga@gmail.com>
2020-10-16 13:42:40 +0200

    Pizzaaaa time

--- jorge-neira/Daily Challenge/ArrayMethods/ownArrayMethods.js
+++ jorge-neira/Daily Challenge/ArrayMethods/ownArrayMethods.js
@@ -61,11 +61,12 @@ const myProtos = {
 	myFill: (object, value, start, end) => {
 		if (Math.sign(start) === -1) start = object.length + start;
 		if (Math.sign(end) === -1) end = object.length + end;
-		
-		for (const property in object) {
-			if (object.hasOwnProperty()) {
+		for (let i = start; i < end; i++) {
+			if (object.hasOwnProperty(i)) {
+				object[i] = value;
 			}
 		}
+		return { ...object, __proto__: myProtos };
 	}
 };
 
