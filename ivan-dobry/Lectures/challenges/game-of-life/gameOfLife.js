b8cd4d212d450c00b8931fce3bf0ac6bb5dc6cdc
Ivan Dobry <ivandobry@hotmail.es>
2020-10-17 13:33:37 +0200

    more updates

--- ivan-dobry/Lectures/challenges/game-of-life/gameOfLife.js
+++ ivan-dobry/Lectures/challenges/game-of-life/gameOfLife.js
@@ -18,10 +18,8 @@ function life (array) {
     let neigbours = 0;
     for (let i = 0; i < array.length; i++) {
         for (let j = 0; j < array[i].length; j++){
-            //Calculo vecinos de esa celda
             neigbours = countNeigbours(i, j);
             console.log(neigbours)
-           // Aplico reglas segun los vecinos
            if (array[i][j] === 0 && neigbours === 3) {
                 blinkerFinalState[i][j] = 1;
            } else if (array[i][j] === 1 && neigbours > 3) {
@@ -33,13 +31,8 @@ function life (array) {
            } else {
                 blinkerFinalState[i][j] = 0;
            }
-           
-
-           // Si reglas es true hago algo sino otra cosa
-
         }
     }
-    
     blinker = blinkerFinalState
     reInitializeFinal ();
     console.log(blinker);
@@ -199,15 +192,6 @@ function reInitializeFinal () {
         [0, 0, 0, 0, 0]
     ]
 }
-
-// function lifeRules(neigbours) {
-//     if (neigbours >= 2) {
-//         return true;
-//     }else {
-//         return false;
-//     }
-    
-// }
 
 life(blinker);
 
