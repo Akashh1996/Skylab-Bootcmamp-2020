fce593bcc69f6a83d67d143291c0afb24bfb0483
Darina Rybalchenko <dari.ryb@gmail.com>
2020-10-17 13:59:19 +0200

    start gameoflife

---
+++ Darina-Rybalchenko/Challenges/GameOfLife/script.test.js
@@ -0,0 +1,23 @@
+const gameOfLife = require('./script')
+
+test('should modify array', () => {
+
+    const blinker = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0],
+    ]
+
+    const blinkerFinal = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+    ]
+
+
+}
+)
