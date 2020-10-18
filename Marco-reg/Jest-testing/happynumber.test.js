3cb5eb262b1490a04624413b11c5800db4c642c9
Marco-Reg <marcoregueiro@gmail.com>
2020-10-18 18:19:25 +0200

    final test

--- Marco-reg/Jest-testing/happynumber.test.js
+++ Marco-reg/Jest-testing/happynumber.test.js
@@ -3,18 +3,18 @@ const isHappy = require('./happynumber');
 describe('A happy number is one where if you repeatedly square its digits and add them together, you eventually end up at 1.', () => {
 	test('Should return 1 if 7 is a happy number', () => {
 		
-		const a = (7, 2);
+		const a = (7);
 		
 		const response = isHappy(a);
 	
-		expect(response).toBe([1]);
+		expect(response).toEqual([1]);
 	});
 	test('Should return the array of numbers if is not a happy number', () => {
 		
-		const a = (49, 2);
+		const a = (42);
 		
 		const response = isHappy(a);
 		
-		expect(response).toBe([42, 20, 4, 16, 37, 58, 89, 145, 42]);
+		expect(response).toEqual([42, 20, 4, 16, 37, 58, 89, 145, 42]);
 	});
 });
