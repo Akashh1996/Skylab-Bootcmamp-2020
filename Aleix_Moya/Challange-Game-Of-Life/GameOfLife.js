284bae5b2c5a6527e59fb4f279f09ca54827ce4d
Aleix Moya <aleixmoya99@gmail.com>
2020-10-18 12:14:48 +0200

    actualizacion

--- Aleix_Moya/Challange-Game-Of-Life/GameOfLife.js
+++ Aleix_Moya/Challange-Game-Of-Life/GameOfLife.js
@@ -1,41 +1,34 @@
-// crear y rellenar array actual y crear array futuro
-// array actual
-var grid = [];
-var rows = 10;
-var columns = 10;
-var alive = 0;
-grid.length = rows;
-for (let i = 0; i < grid.length; i++){
-    grid[i] = [];
-}
+function drawGrid(grid){
+    var Eden = getElementById(tabla);
 
-for(i = 0; i < grid.length; i++){
-    grid[i].length = columns;
+    Eden = "<table >";
+        for(let i = 0; i < grid.length; i++){
+            Eden +="<tr>";
+            for(let j = 0; j < grid[i].length; j++){
+                Eden +="<td>";
+                Eden += grid[i][j];
+                Eden+="</td>";
+            }
+            Eden+="</tr>";
+        }
+        Eden += "</table>";
 }
-
-for(i = 0; i < grid.length; i++){
-    for(let j = 0; j < grid[i].length; j++){
-        grid[i][j] = alive;
-    }
+function getRows() {
+    var x = document.getElementById("rows");
+    var row = 0;
+    row = x.elements[0].value;
+    return row;
 }
-console.log(grid);
-var fGrid = [];
-for (let i = 0; i < grid.length; i++){
-    fGrid[i] = [];
+function getColumn() {
+    var x = document.getElementById("columns");
+    var column = 0;
+    column = x.elements[1].value;
+    return column;
 }
-
-for(i = 0; i < grid.length; i++){
-    fGrid[i].length = columns;
+function lazarus(walk){
+    walk = 1;
 }
-
-for(i = 0; i < grid.length; i++){
-    for(let j = 0; j < fGrid[i].length; j++){
-        fGrid[i][j] = alive;
-    }
-}
-// funciones que interactuan con array
-
-function vivo(grid, fGrid){
+function itLives(grid, fGrid){
     for(i = 0; i < grid.length; i++){
         for(j = 0; j < grid[i].length; j++){
             // fila superior
@@ -49,26 +42,26 @@ function vivo(grid, fGrid){
                 if(grid[(i - 1)][j] === 1){
                     contamos++;
                 }
-                if((j + 1) <= grid[i].length){
+                if((j + 1) < grid[i].length){
                     if(grid[(i - 1)][(j + 1)] === 1){
                         contamos++;
                     }
                 }
             }
-            if((j - 1) <= 0){
+            if((j - 1) >= 0){
                 if(grid[i][(j - 1)] === 1){
                     contamos++;
                 }
             }
             //fila actual
-            if((j + 1) <= grid[i].length){
+            if((j + 1) < grid[i].length){
                 if(grid[i][(j + 1)] === 1){
                     contamos++;
                 }
             }
-            if((i + 1) >= grid.length){
+            if((i + 1) < grid.length){
                 // fila inferior
-                if((j - 1) <= 0){
+                if((j - 1) >= 0){
                     if(grid[(i + 1)][(j - 1)] === 1){
                         contamos++;
                     }
@@ -76,7 +69,7 @@ function vivo(grid, fGrid){
                 if(grid[(i + 1)][j] === 1){
                     contamos++;
                 }
-                if((j + 1) <= grid[i].length){
+                if((j + 1) < grid[i].length){
                     if(grid[(i + 1)][(j + 1)] === 1){
                         contamos++;
                     }
@@ -101,5 +94,41 @@ function vivo(grid, fGrid){
     }
     return fGrid;
 }
-vivo(grid, fGrid);
-console.log(fGrid);
+// crear y rellenar array actual y crear array futuro
+// array actual
+var grid = [];
+var alive = 0;
+grid.length = 10 /*getRows()*/;
+for (let i = 0; i < grid.length; i++){
+    grid[i] = [];
+}
+    
+for(i = 0; i < grid.length; i++){
+    grid[i].length = 10/*getColumns()*/;
+}
+    
+for(i = 0; i < grid.length; i++){
+    for(let j = 0; j < grid[i].length; j++){
+        grid[i][j] = alive;
+    }
+}
+console.log(grid);
+var fGrid = [];
+for (let i = 0; i < grid.length; i++){
+    fGrid[i] = [];
+}
+    
+for(i = 0; i < grid.length; i++){
+    fGrid[i].length = 10/*getColumns()*/;
+}
+    
+for(i = 0; i < grid.length; i++){
+    for(let j = 0; j < fGrid[i].length; j++){
+        fGrid[i][j] = alive;
+    }
+}
+
+// funciones que interactuan con array
+
+itLives(grid, fGrid);
+lazarus(grid[1][2]);
