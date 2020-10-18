bdd8cb40425fc8cd407f07c66e597475cc2d95f1
Jenniferpichperez <66845906+Jenniferpichperez@users.noreply.github.com>
2020-10-18 19:29:24 +0200

    delete file
    delete file that does not correspond to this folder

--- Jennifer/codewars-testing/codewars-maxmin.js
+++
@@ -1,9 +0,0 @@
-function highAndLow(numbers){
-    numbers=numbers.split(" ");
-    num1 = Math.max(...numbers).toString();
-    num2 = Math.min(...numbers).toString();
-    let result = num1 + " " + num2;
-    return result;
-}
-
-module.exports = highAndLow
