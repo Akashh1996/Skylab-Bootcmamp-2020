087512a15563d11a9017e4047556c7d1cf88c96c
Jorge <jorgengarriga@gmail.com>
2020-10-18 13:42:27 +0200

    Create canvas grid

--- jorge-neira/Challenge/GameOfLife/app/script.js
+++ jorge-neira/Challenge/GameOfLife/app/script.js
@@ -1,4 +1,50 @@
-// function gridArray(cols, rows) {
+// 'use strict';
+
+// if (state === 0 && neigthbors === 3) {
+// 	nextGen[x][y] = 1 // 1
+// } else if (state === 1 && (neigthbord < 2 || neigthbord > 3)) {
+// 	nextGen[x][y] = 0 // 0
+// } else {
+// 	nextGen[x][y] = prevGen[x][y]:
+// }
+// let state = 0;
+// const prevGen = [
+// 	[10, 20, 30, 40, 50, 60, 70],
+// 	[10, 20, 30, 40, 50, 60, 70],
+// 	[10, 20, 30, 40, 50, 60, 70],
+// 	[10, 20, 31, 41, 51, 60, 70],
+// 	[10, 20, 30, 40, 50, 60, 70],
+// 	[10, 20, 30, 40, 50, 60, 70],
+// 	[10, 20, 30, 40, 50, 60, 70]
+// ];
+
+// for (let x = 0; x < prevGen.length; x++) {
+// 	for (let y = 0; y < prevGen[x].length; y++) {
+// 		state = prevGen[x][y];
+// 		console.log(prevGen[x][y]);
+// 		console.log('----------->', prevGen[x].length);
+// 	}
+// }
+// function gameOfLife() {}
+
+// let grid;
+// let cols;
+// let rows;
+// let resolution = 15;
+
+// function setup() {
+// 	createCanvas(900, 600);
+// 	cols = width / resolution;
+// 	rows = height / resolution;
+
+// 	grid = create2dArray(cols, rows);
+// 	for (let i = 0; i < cols; i++) {
+// 		for (let j = 0; j < rows; j++) {
+// 			grid[i][j] = Math.floor(random(2));
+// 		}
+// 	}
+// }
+// function create2dArray(cols, rows) {
 // 	let arr = new Array(cols);
 // 	for (let i = 0; i < arr.length; i++) {
 // 		arr[i] = new Array(rows);
@@ -6,70 +52,93 @@
 // 	return arr;
 // }
 
-// let grid;
-// let cols = 20;
-// let rows = 20;
+// function draw() {
+// 	background(0);
+// 	textAlign(CENTER, CENTER);
 
-// function setGrid() {
-// 	grid = gridArray(cols, rows);
-// 	for (let i = 0; i < grid.length; i++) {
-// 		for (let x = 0; x < grid.length; x++) {
-// 			grid[i][x] = 0;
+// 	for (let i = 0; i < cols; i++) {
+// 		for (let j = 0; j < rows; j++) {
+// 			let x = i * resolution;
+// 			let y = j * resolution;
+// 			if (grid[i][j] == 1) {
+// 				fill(255);
+// 				stroke(0);
+// 				rect(x, y, resolution - 1, resolution - 1);
+// 			}
 // 		}
 // 	}
+
+// 	let next = create2dArray(cols, rows);
+
+// 	// Compute next based on grid
+// 	for (let i = 0; i < cols; i++) {
+// 		for (let j = 0; j < rows; j++) {
+// 			let state = grid[i][j];
+// 			// Count live neighbors!
+// 			let sum = 0;
+// 			let neighbors = countNeighbors(grid, i, j);
+
+// 			if (state == 0 && neighbors == 3) {
+// 				next[i][j] = 1;
+// 			} else if (state == 1 && (neighbors < 2 || neighbors > 3)) {
+// 				next[i][j] = 0;
+// 			} else {
+// 				next[i][j] = state;
+// 			}
+// 		}
+// 	}
+
+// 	grid = next;
 // }
 
-// setGrid();
+// function countNeighbors(grid, x, y) {
+// 	let sum = 0;
+// 	for (let i = -1; i < 2; i++) {
+// 		for (let j = -1; j < 2; j++) {
+// 			let col = (x + i + cols) % cols;
+// 			let row = (y + j + rows) % rows;
+// 			sum += grid[col][row];
+// 		}
+// 	}
+// 	sum -= grid[x][y];
+// 	return sum;
+// }
 
-function gameOfLife() {
-	const prevGen = [
-		[0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 1, 1, 1, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0]
-	];
-	const nextGen = [
-		[0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0]
-	];
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+canvas.style.border = '1px solid white';
+
+const resolution = 20;
+canvas.width = 1000;
+canvas.height = 600;
+
+const rows = canvas.height / resolution;
+const cols = canvas.width / resolution;
+const grid = create2dArray(rows, cols);
+
+function create2dArray(rows, cols) {
+	let arr = new Array(rows);
+	for (let i = 0; i < arr.length; i++) {
+		arr[i] = new Array(cols).fill(0);
+	}
+	return arr;
 }
 
-module.exports = gameOfLife;
-
-function test1() {
-	const test = [
-		[1, 2, 0, 0, 0, 0, 0],
-		[4, 3, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 1, 1, 1, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0]
-	];
-	let neigthbors = 0;
-
-	for (let x = 0; x < test.length; x++) {
-		console.log(test.length);
-		for (let y = 0; y < test[x].length; y++) {
-			console.log(test[x][y]);
-			console.log(test[x - 1][y]);
-			// console.log(test[x - 1][y + 1]);
-			// console.log(test[x][y + 1]);
-			// console.log(test[x + 1][y + 1]);
-			// console.log(test[x + 1][y]);
-			// console.log(test[x + 1][y - 1]);
-			// console.log(test[x][y - 1]);
-			// console.log(test[x - 1][y - 1]);
+function startGame() {
+	draw(grid);
+}
+function draw(grid) {
+	for (let row = 0; row < grid.length; row++) {
+		for (let col = 0; col < grid[row].length; col++) {
+			// const cell = grid[col][row];
+			ctx.beginPath();
+			ctx.rect(col * resolution, row * resolution, resolution, resolution);
+			ctx.strokeStyle = 'white';
+			ctx.stroke();
+			ctx.fillStyle = 'gray';
+			ctx.fill();
 		}
 	}
 }
 
-test1();
+startGame();
