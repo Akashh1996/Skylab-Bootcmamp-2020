b47e6af628633f79c5d7e593ea212d2658d5fc45
Marco-Reg <marcoregueiro@gmail.com>
2020-10-18 13:52:58 +0200

    still working

---
+++ Marco-reg/Jest-testing/happynumber.js
@@ -0,0 +1,28 @@
+/*function isHappy(n){
+    let result = n.toString();
+    let saftyCounter = 0;
+    
+    while(result != 1 && saftyCounter<1000){
+        result = happyStep(result)
+        saftyCounter++
+        console.log (result)
+    }
+
+    
+}
+
+function happyStep(n) {
+    let arrayNumbers = n.split('');
+    
+    let result = 0;
+    for (let i = 0 ; i<arrayNumbers.length;i++){
+        arrayNumbers[i] = parseInt(arrayNumbers[i]);
+        result += arrayNumbers[i]*arrayNumbers[i]
+        
+    }
+    console.log(result)
+    return result.toString();
+
+}
+console.log(isHappy("11"))
+module.exports = isHappy;
