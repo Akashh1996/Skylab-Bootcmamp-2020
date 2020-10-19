c315f998aefd5e9ce6e70127a198eed8882a668e
Alberto <algmebe1@gmail.com>
2020-10-19 14:51:53 +0200

    game-of-life

---
+++ alberto-gomez/challenges/game-of-life/index.js
@@ -0,0 +1,126 @@
+// FUNCIÓN PARA DEFINIR GRID INICIAL (RANDOM)
+
+function newGameGrid() {
+	let firstGenGrid = [
+		// matriz de 10x10
+		[, , , , , , , , , ,],
+		[, , , , , , , , , ,],
+		[, , , , , , , , , ,],
+		[, , , , , , , , , ,],
+		[, , , , , , , , , ,],
+		[, , , , , , , , , ,],
+		[, , , , , , , , , ,],
+		[, , , , , , , , , ,],
+		[, , , , , , , , , ,],
+		[, , , , , , , , , ,]
+	];
+
+	for (let i = 0; i < firstGenGrid.length; i++) {
+		for (j = 0; j < firstGenGrid[i].length; j++) {
+			firstGenGrid[i][j] = Math.round(Math.random());
+		}
+	}
+	return firstGenGrid;
+}
+
+// FUNCION PARA RECORRER TODO EL ARRAY Y COMPROBAR SI LA CELDA ESTÁ VIVA O MUERTA
+
+function nextGen(grid) {
+	let nextGenGrid = grid.map((arr) => [...arr]); // crea un duplicado del array
+	let nodeCell = document.querySelectorAll('td');
+	let pos = 0;
+
+	for (let row = 0; row < grid.length; row++) {
+		for (let col = 0; col < grid[row].length; col++) {
+			let cell = grid[row][col];
+			let neighbourCounter = 0;
+			// check a los vecinos
+			for (let i = -1; i < 2; i++) {
+				for (let j = -1; j < 2; j++) {
+					if (i === 0 && j === 0) {
+						continue;
+					}
+
+					let xNeighbour = row + i;
+					let yNeighbour = col + j;
+					// gestión de los bordes
+					if (
+						xNeighbour >= 0 &&
+						yNeighbour >= 0 &&
+						xNeighbour < grid[row].length &&
+						yNeighbour < grid.length
+					) {
+						let currentNeighbour = grid[row + i][col + j];
+						if (currentNeighbour === 1) {
+							neighbourCounter += 1;
+						}
+					}
+				}
+			}
+			// underpopulation
+			if (neighbourCounter < 2 && cell === 1) {
+				nextGenGrid[row][col] = 0;
+				// overpopulation
+			} else if (neighbourCounter > 3 && cell === 1) {
+				nextGenGrid[row][col] = 0;
+				// reproduction
+			} else if (neighbourCounter === 3 && cell === 0) {
+				nextGenGrid[row][col] = 1;
+			}
+			// representación en nuevo grid
+			if (nextGenGrid[row][col] === 1) {
+				let tableCell = nodeCell[pos];
+				tableCell.style.backgroundColor = 'black';
+				pos++;
+			} else if (nextGenGrid[row][col] === 0) {
+				let tableCell = nodeCell[pos];
+				tableCell.style.backgroundColor = 'white';
+				pos++;
+			}
+		}
+	}
+	return nextGenGrid;
+}
+
+// FUNCION SETUP FINAL DEL TURNO
+
+function endOfGen(firstGrid, lastGrid) {
+	for (i = 0; i < lastGrid.length; i++) {
+		for (j = 0; j < lastGrid[i].length; j++) {
+			lastGrid[i][j] = 0;
+		}
+	}
+	return firstGrid, lastGrid;
+}
+
+const newGameButton = document.getElementById('new-game-button');
+const startButton = document.getElementById('start-button');
+const stopButton = document.getElementById('stop-button');
+
+// TO REVIEW
+
+/* function startGame() {
+	nextGen(newGameGrid());
+	endOfGen(firstGenGrid, nextGenGrid);
+} */
+
+function gameGeneration() {
+	initialGrid = nextGen(initialGrid);
+	// endOfGen(firstGenGrid,ktGenGrid);
+}
+
+newGameButton.addEventListener('click', () => {
+	initialGrid = newGameGrid();
+	nextGen(initialGrid);
+});
+
+startButton.addEventListener('click', () => {
+	setInterval(gameGeneration, 500);
+});
+
+/* stopButton.addEventListener('click', () => {
+}) */
+
+let initialGrid;
+
+module.exports(nextGen);
