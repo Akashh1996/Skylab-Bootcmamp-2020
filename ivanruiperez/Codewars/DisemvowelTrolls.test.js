6cdb87782b8774c562feb4e366ea97df543b0b9f
Ivan Ruiperez Parra <ivan.ruiperez@gmail.com>
2020-10-19 19:06:05 +0200

    dashboard css

--- ivanruiperez/Codewars/DisemvowelTrolls.test.js
+++
@@ -1,7 +0,0 @@
-const disemvowel = require("./DisemvowelTrolls");
-
-test("delete vocals", () => {
-  expect(disemvowel("This website is for losers LOL!")).toBe(
-    "Ths wbst s fr lsrs LL!"
-  );
-});
