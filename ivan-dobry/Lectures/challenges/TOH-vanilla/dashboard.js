c9c82bb16fb2b54da1158d5b12430117711bdc14
Ivan Dobry <ivandobry@hotmail.es>
2020-10-20 13:14:18 +0200

    TOH update

--- ivan-dobry/Lectures/challenges/TOH-vanilla/dashboard.js
+++ ivan-dobry/Lectures/challenges/TOH-vanilla/dashboard.js
@@ -4,9 +4,10 @@ const topButtonHeroes = document.getElementById("buttons");
 
 function makeTopHeroes(array) {
   for (let i = 0; i < array.length; i++) {
-    let cell = document.createElement("button");
+    let cell = document.createElement("a");
     topButtonHeroes.appendChild(cell).className = "section__button";
     cell.innerHTML = array[i].name
+    cell.href=`details.html?heroId=${i + 2}`
   };
 };
 
--- ivan-dobry/Lectures/challenges/TOH-vanilla/heroes.js
+++ ivan-dobry/Lectures/challenges/TOH-vanilla/heroes.js
@@ -4,9 +4,10 @@ const heroesList = document.getElementById("heroes-list");
 
 function makeList(array) {
   for (let i = 0; i < array.length; i++) {
-    let cell = document.createElement("li");
+    let cell = document.createElement("a");
     heroesList.appendChild(cell).className = "heroes__list";
-    cell.innerHTML = array[i].id + '&nbsp' + '&nbsp' + '&nbsp' + array[i].name
+    cell.innerHTML = array[i].id + '&nbsp' + '&nbsp' + '&nbsp' + '&nbsp' + array[i].name
+    cell.href=`details.html?heroId=${i + 1}`
   };
 };
 
