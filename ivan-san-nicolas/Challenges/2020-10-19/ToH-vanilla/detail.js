6e6ab0c4359bfa98db5f64181ae10b90ca5ae9c7
Ivansannicolas <ivohan8@gmail.com>
2020-10-20 23:18:59 +0200

    automatic detail working, styles not done yet

--- ivan-san-nicolas/Challenges/2020-10-19/ToH-vanilla/detail.js
+++ ivan-san-nicolas/Challenges/2020-10-19/ToH-vanilla/detail.js
@@ -8,32 +8,36 @@ function getIdfromURL(location) {
 
 function heroesInfo(id, store) {
     const actualHero = store.getHeroeById(id);
-    document.getElementById('hero-detail-id').innerHTML = actualHero.id;
-    document.getElementById('hero-detail-title').innerHTML = `${actualHero.name} details!`;
-    document.getElementById('hero-detail-name').innerValue = actualHero.name;
-    document.getElementById('hero-detail-intelligence').innerHTML = actualHero.powerstats.intelligence;
-    document.getElementById('hero-detail-strength').innerHTML = actualHero.powerstats.strength;
-    document.getElementById('hero-detail-speed').innerHTML = actualHero.powerstats.speed;
-    document.getElementById('hero-detail-durability').innerHTML = actualHero.powerstats.durability;
-}
-
-function heroesInfo(id, store) {
-    const actualHero = store.getHeroeById(id);
+    debugger;
     const heroDetail = document.getElementById('hero-detail__info');
     for (property in actualHero) {
         if (typeof actualHero[property] !== 'object') {
             let element = document.createElement('div');
-            element.innerHTML = `
-            <div class="hero-detail__info__block">
-                    <span class="hero-detail__info__block__property-name" id="hero-detail-property"></span>
-                    <span class="hero-detail__info__block__property-value" id="hero-detail-value"></span>
+            element.innerHTML = `<div class="hero-detail__info__block">
+                    <span class="hero-detail__info__block__property-name" id="hero-detail-${property}"></span>
+                    <span class="hero-detail__info__block__property-value" id="hero-detail-${actualHero[property]}"></span>
                 </div>
-            `;
+                `;
             heroDetail.appendChild(element);
-            document.getElementById('hero-detail-property').innerHTML = actualHero[property];
-            document.getElementById('hero-detail-value').innerHTML = acutalHero.property;
-        } else if (typeof actualHero === 'object') {
-            heroesInfo(id, store);
+            document.getElementById(`hero-detail-${property}`).innerHTML = property;
+            document.getElementById(`hero-detail-${actualHero[property]}`).innerHTML = actualHero[property];
+        } else {
+            let element = document.createElement('section');
+            element.innerHTML = `<section class="hero-detail__info__block hero-properties" id="hero-detail-group-${property}">
+            </section>`;
+            heroDetail.appendChild(element);
+            document.getElementById(`hero-detail-group-${property}`).innerHTML = property;
+            for (value in actualHero[property]) {
+                let element = document.createElement('div');
+                element.innerHTML = `<div class="hero-detail__info__block">
+                    <span class="hero-detail__info__block__property-name" id="hero-detail-${value}"></span>
+                    <span class="hero-detail__info__block__property-value" id="hero-detail-${actualHero[property][value]}"></span>
+                </div>`;
+                heroDetail.appendChild(element);
+                document.getElementById(`hero-detail-${value}`).innerHTML = value;
+                document.getElementById(`hero-detail-${actualHero[property][value]}`).innerHTML = actualHero[property][value];
+            }
+
         }
     }
 }
