fa48550962f570bf2178ee8dc347713faff9f0b1
Ivansannicolas <ivohan8@gmail.com>
2020-10-20 15:05:15 +0200

    debugger removed

--- ivan-san-nicolas/Challenges/2020-10-19/ToH-vanilla/store.js
+++ ivan-san-nicolas/Challenges/2020-10-19/ToH-vanilla/store.js
@@ -1,28 +1,55 @@
-debugger;
-
-let _heroes = [
-	{ id: 11, name: 'Dr Nice' },
-	{ id: 12, name: 'Narco' },
-	{ id: 13, name: 'Bombasto' },
-	{ id: 14, name: 'Celeritas' },
-	{ id: 15, name: 'Magneta' },
-	{ id: 16, name: 'RubberMan' },
-	{ id: 17, name: 'Dynama' },
-	{ id: 18, name: 'Dr IQ' },
-	{ id: 19, name: 'Magma' },
-	{ id: 20, name: 'Tornado' }
-]; 
+let _heroes = [{
+		id: 11,
+		name: 'Dr Nice'
+	},
+	{
+		id: 12,
+		name: 'Narco'
+	},
+	{
+		id: 13,
+		name: 'Bombasto'
+	},
+	{
+		id: 14,
+		name: 'Celeritas'
+	},
+	{
+		id: 15,
+		name: 'Magneta'
+	},
+	{
+		id: 16,
+		name: 'RubberMan'
+	},
+	{
+		id: 17,
+		name: 'Dynama'
+	},
+	{
+		id: 18,
+		name: 'Dr IQ'
+	},
+	{
+		id: 19,
+		name: 'Magma'
+	},
+	{
+		id: 20,
+		name: 'Tornado'
+	}
+];
 
 class Store {
-    getHeroes() {
-        return _heroes;
-    }
-    getHeroeById(heroId) {
-        return this.getHeroes().find(hero => hero.id === heroId);
-    }
-    getTopHeroes() {
-        return this.getHeroes().slice(1, 5);
-    }
+	getHeroes() {
+		return _heroes;
+	}
+	getHeroeById(heroId) {
+		return this.getHeroes().find(hero => hero.id === heroId);
+	}
+	getTopHeroes() {
+		return this.getHeroes().slice(1, 5);
+	}
 }
 
 const store = new Store();
