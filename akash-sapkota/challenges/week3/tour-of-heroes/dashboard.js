0bb1ea68437153e16a65d5aa4778ae1f716e8323
Akash Sapkota <akashh.sapkota@gmail.com>
2020-10-20 09:33:02 +0200

    work in progress

--- akash-sapkota/challenges/week3/tour-of-heroes/dashboard.js
+++ akash-sapkota/challenges/week3/tour-of-heroes/dashboard.js
@@ -1,1 +1,15 @@
-let heroes = require("./superHeroData.json")
+function insertName(store) {
+    let dashboardName = store.getTopHeroes()
+    for (let index = 0; index < 4; index++) {
+
+        document.getElementById(`heroes_name${index}`).innerHTML = dashboardName[index].name
+        document.getElementById(`heroes_name${index}`).href = `details.html?q=${index+1}`
+
+    }
+
+
+}
+
+
+
+insertName(store)
