83036ae22b4cef598a0f7d3b5bc0068ab9161a38
Ivansannicolas <ivohan8@gmail.com>
2020-10-20 15:33:12 +0200

    aside.js ready for automatic

--- ivan-san-nicolas/Challenges/2020-10-19/ToH-vanilla/aside.js
+++ ivan-san-nicolas/Challenges/2020-10-19/ToH-vanilla/aside.js
@@ -6,26 +6,4 @@ function topHeroes(store) {
     }
 }
 
-function hasProperty(obj) {
-    for (property in obj) {
-        if (typeof obj[property] === 'object')
-    }
-}
-
-function deepClone(obj) {
-    let newObj = {};
-    let property = 0;
-    if (!obj) return obj;
-    else {
-        for (property in obj) {
-            if (typeof obj[property] === 'object') {
-                newObj[property] = deepClone(obj[property]);
-            } else {
-                newObj[property] = obj[property];
-            }
-        }
-    }
-    return newObj;
-}
-
 topHeroes(store);
