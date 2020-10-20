6b7f0d5d386a7b63bbbaae9176b69f06053fbd2c
Ivansannicolas <ivohan8@gmail.com>
2020-10-20 15:03:39 +0200

    automatic dashboard done, working on automatic aside

--- ivan-san-nicolas/Challenges/2020-10-19/ToH-vanilla/aside.js
+++ ivan-san-nicolas/Challenges/2020-10-19/ToH-vanilla/aside.js
@@ -1,8 +1,31 @@
 function topHeroes(store) {
     const asideHeroes = store.getTopHeroes();
-    for (i in asideHeroes) {
-        document.getElementById(`aside__heroe-${i}`).innerHTML = asideHeroes[i].name;
+    const heroesBlock = document.getElementById('aside-heroes-block');
+    for (let index = 1; index < 11; index++) {
+        let element = document.createElement('a')
     }
+}
+
+function hasProperty(obj) {
+    for (property in obj) {
+        if (typeof obj[property] === 'object')
+    }
+}
+
+function deepClone(obj) {
+    let newObj = {};
+    let property = 0;
+    if (!obj) return obj;
+    else {
+        for (property in obj) {
+            if (typeof obj[property] === 'object') {
+                newObj[property] = deepClone(obj[property]);
+            } else {
+                newObj[property] = obj[property];
+            }
+        }
+    }
+    return newObj;
 }
 
 topHeroes(store);
--- ivan-san-nicolas/Challenges/2020-10-19/ToH-vanilla/dashboard.js
+++ ivan-san-nicolas/Challenges/2020-10-19/ToH-vanilla/dashboard.js
@@ -1,10 +1,13 @@
 function topHeroes(store) {
     const topHeroes = store.getTopHeroes();
-    const dashboardHeroes = document.getElementById('dashboardHeroes')
-    for (i in topHeroes) {
-        let element = document.createElement();
-        element.innerHTML = `<a class="topHeroes-block" id="heroe-${i}" href="detail.html?id=${i+1}">${topHeroes[i].name}</a>`
-        dashboardHeroes.appendChild(element);
+    debugger;
+    const dashboardHeroes = document.getElementById('dashboard-heroes');
+    if (topHeroes) {
+        for (let index = 0; index < 4; index++) {
+            let element = document.createElement('a');
+            element.innerHTML = `<a class="topHeroes-block" id="heroe-${index}" href="detail.html?id=${index+1}">${topHeroes[index].name}</a>`
+            dashboardHeroes.appendChild(element);
+        }
     }
 }
 
