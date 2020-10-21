c4a5f5a55ba8f5b3a3dcd7e353623bfa0e4d8a0d
Jheavi <javierlaso213@hotmail.com>
2020-10-21 12:04:40 +0200

    added function to get json file

--- javi-laso/ToH-vanilla/View/Dashboard/dashboard.js
+++ javi-laso/ToH-vanilla/View/Dashboard/dashboard.js
@@ -2,3 +2,5 @@ const store = new Store(heroes);
 
 const dashboard = document.getElementById('dashboard-list');
 store.updateHTMLHeroesDashboard(dashboard);
+
+store.loadHeroes('../../api/superHeroData.json', 'json', store.logData);
--- javi-laso/ToH-vanilla/store.js
+++ javi-laso/ToH-vanilla/store.js
@@ -87,6 +87,23 @@ class Store {
     updateValueHtml(element, property, value) {
         element[property] = !!value ? value : '-';
     }
+
+    loadHeroes(url, type, callback) {
+        let xhr = new XMLHttpRequest();
+
+        xhr.open('GET', url);
+        xhr.responseType = type;
+
+        xhr.onload = function() {
+            callback(xhr.response);
+        }
+
+        xhr.send();
+    }
+
+    logData(data) {
+        console.log(data);
+    }
 }
 
 module.exports = Store;
