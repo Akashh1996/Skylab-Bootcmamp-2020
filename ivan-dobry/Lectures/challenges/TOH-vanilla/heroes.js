f18d376f3c34635087f143f9dac05c734f5d242a
Ivan Dobry <ivandobry@hotmail.es>
2020-10-21 16:12:42 +0200

    TOh update

--- ivan-dobry/Lectures/challenges/TOH-vanilla/heroes.js
+++ ivan-dobry/Lectures/challenges/TOH-vanilla/heroes.js
@@ -6,14 +6,16 @@ const heroesList = document.getElementById("heroes-list");
 
 
   function makeList(array) {
+    let index= 0;
     for (let i = 0; i < array.length; i++) {
+      debugger;
+      if (array[index].id === i +1) {
       let cell = document.createElement("a");
       heroesList.appendChild(cell).className = "heroes__list";
-      if (array[i].id !== i +1) {
-        i++;
-      }
-      cell.innerHTML = array[i].id + '&nbsp' + '&nbsp' + '&nbsp' + '&nbsp' + array[i].name
-      cell.href=`details.html?heroId=${i + 1}`
+      cell.innerHTML = array[index].id + '&nbsp' + '&nbsp' + '&nbsp' + '&nbsp' + array[index].name;
+      cell.href=`details.html?heroId=${i + 1}`;
+      index++;
+    }
     };
   };
   
