cf9647505f0db570c0e210944956b3f3e28ca671
Darina Rybalchenko <dari.ryb@gmail.com>
2020-10-21 15:18:40 +0200

    pending of last changes in detail page

--- Darina-Rybalchenko/Challenges/Toh-vanila/detail.js
+++ Darina-Rybalchenko/Challenges/Toh-vanila/detail.js
@@ -1,12 +1,21 @@
 function detailList() {
     let locationId = getIdFromUrlParams(location.search)
-    let hero = store.getHeroById(locationId)
+    const hero = store.getHeroById(locationId)
     let detailTittle = document.getElementById('detail-title')
     let idElement = document.getElementById('hero-id__value')
     let nameElement = document.getElementById('hero-name__input')
     detailTittle.innerHTML = hero.name
     idElement.innerHTML = hero.id
     nameElement.value = hero.name
+    let detailImage = hero.images.sm
+    document.getElementById('photo').src = detailImage
+
+
+    let appereanceDetail = hero.appearance
+    for (properties in appereanceDetail) {
+        document.getElementById(`apperence__value-${properties}`).innerHTML = appereanceDetail[properties]
+    }
+
 }
 detailList()
 
@@ -15,6 +24,9 @@ function getIdFromUrlParams(locationSearch) {
     let heroId = locationSearchSplitted[1]
     return +heroId
 }
+
+
+
 
 
 
