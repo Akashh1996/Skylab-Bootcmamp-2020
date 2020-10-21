cb8dd4c07e91fdc6c60996e85aacb2b9686a3eee
Ivansannicolas <ivohan8@gmail.com>
2020-10-21 09:41:19 +0200

    detail automatic done

--- ivan-san-nicolas/Challenges/2020-10-19/ToH-vanilla/detail.js
+++ ivan-san-nicolas/Challenges/2020-10-19/ToH-vanilla/detail.js
@@ -10,6 +10,8 @@ function heroesInfo(id, store) {
     const actualHero = store.getHeroeById(id);
     debugger;
     const heroDetail = document.getElementById('hero-detail__info');
+    let heroTitle = document.getElementById('hero-detail-title');
+    heroTitle.innerHTML = actualHero.name;
     for (property in actualHero) {
         if (typeof actualHero[property] !== 'object') {
             let element = document.createElement('div');
@@ -28,12 +30,12 @@ function heroesInfo(id, store) {
             heroDetail.appendChild(element);
             document.getElementById(`hero-detail-group-${property}`).innerHTML = property;
             for (value in actualHero[property]) {
-                let element = document.createElement('div');
-                element.innerHTML = `<div class="hero-detail__info__block">
+                let subElement = document.createElement('div');
+                subElement.innerHTML = `<div class="hero-detail__info__block">
                     <span class="hero-detail__info__block__property-name" id="hero-detail-${value}"></span>
                     <span class="hero-detail__info__block__property-value" id="hero-detail-${actualHero[property][value]}"></span>
                 </div>`;
-                heroDetail.appendChild(element);
+                element.appendChild(subElement);
                 document.getElementById(`hero-detail-${value}`).innerHTML = value;
                 document.getElementById(`hero-detail-${actualHero[property][value]}`).innerHTML = actualHero[property][value];
             }
