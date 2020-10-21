2a055a3f0a6547c3c4233c64113da467beb0e249
Alma-Co <edith.cantegrit@gmail.com>
2020-10-21 17:37:23 +0200

    changes

--- edith-cantegrit/challenge/ToH/view/detail/detail.js
+++ edith-cantegrit/challenge/ToH/view/detail/detail.js
@@ -30,37 +30,6 @@ function printHero() {
     connectionHero.setAttribute("style","white-space: pre; margin: 30px 10px;")
     let powerStats = document.createElement("section");
     powerStats.setAttribute("class","statisticsBlock")
-    // hero.powerstats.forEach(function(stat){
-    //     let gauge = document.createElement("div");
-    //     gauge.setAttribute("class","powerstats")
-    //     gauge.setAttribute("id","intelligence")
-    //     let gaugeSvg = document.createElement("svg")
-    //     gaugeSvg.setAttribute("style","height=100 width=100")
-    //     let gaugecircle = document.createElement("circle")
-    //     gaugecircle.setAttribute("style","cx=50 cy=50 r=37")
-    //     let gaugecircle2 = document.createElement("circle")
-    //     gaugecircle2.setAttribute("style","cx=50 cy=50 r=37")
-    //     let spanName = document.createElement("svg");
-    //     let spanNum = document.createElement("svg");
-    //     spanName.setAttribute("class","powerstats__name")
-    //     spanNum.setAttribute("class","powerstats__number")
-    //     spanNum.setAttribute("id","intelligence-number")
-    //     gauge.appendChild(gaugeSvg)
-    //     gaugeSvg.appendChild(gaugecircle)
-    //     gaugeSvg.appendChild(gaugecircle2)
-    //     gauge.appendChild(spanName)
-    //     gauge.appendChild(spanNum)
-    //     getDashOffSetfromPercent(gaugecircle2, stat.intelligence) {
-    //         let radius = circle.getAttribute('r');
-    //         let circumference = Math.PI * radius * 2;
-    //         percent = percent < 0 ? 0 : percent > 100 ? 100 : percent;
-    //         let dashOffSet = circumference - percent / 100 * circumference;
-    //         return dashOffSet;
-    //     }
-    //     setstrokeDashoffsetInCircle(circle, percent) {
-    //         circle.style.strokeDashoffset = this.getDashOffSetfromPercent(circle, percent);
-    //     }
-    // });
     
 
     document.getElementById("heroesDetail").appendChild(divHeader)
