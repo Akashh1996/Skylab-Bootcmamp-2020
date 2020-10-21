447246a17540f1cbcaaea2855cc2933cd56e5b95
Akash Sapkota <akashh.sapkota@gmail.com>
2020-10-21 09:19:32 +0200

    work in progress

---
+++ akash-sapkota/challenges/week3/heroes-final/view/detail/detail.js
@@ -0,0 +1,11 @@
+class DetailComponent {
+    constructor(hero) {
+        this.hero = hero;
+    }
+
+    updateHtmlValue(element, property, value) {
+        element[property] = value;
+    }
+}
+
+module.exports = DetailComponent;
