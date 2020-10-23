e44182defe6a27403b08698fe6a79abfd53a88db
Marco-Reg <marcoregueiro@gmail.com>
2020-10-23 08:57:15 +0200

    just one test

--- Marco-reg/Pokemon/store.test.js
+++ Marco-reg/Pokemon/store.test.js
@@ -4,36 +4,13 @@ describe('store', () => {
 	test('should create', () => {
 		expect(store).toBeDefined();
 	});
+	test("should return a promise",()=>{
+		global.fetch=jest.fn().mockImplementationOnce(()=>{
+			Promise.resolve({
+				json:fn().mockReturnValueOnce
+			})
+		})
+	})
 
-	describe('getPokemon', () => {
-		test('should call getPokemon and return an array of pokemons', () => {
-			expect(store.getPokemon()).toEqual([
-				{ id: 11, name: 'Dr Nice' },
-				{ id: 12, name: 'Narco' },
-				{ id: 13, name: 'Bombasto' },
-				{ id: 14, name: 'Celeritas' },
-				{ id: 15, name: 'Magneta' },
-				{ id: 16, name: 'RubberMan' },
-				{ id: 17, name: 'Dynama' },
-				{ id: 18, name: 'Dr IQ' },
-				{ id: 19, name: 'Magma' },
-				{ id: 20, name: 'Tornado' }
-			]);
-		});
-	});
-	describe('getPokemonById', () => {
-		test('should call getPokemonById and return one pokemon', () => {
-			expect(store.getPokemonById(12)).toEqual({ id: 12, name: 'Narco' });
-		});
-	});
-	describe('getTopPokemon', () => {
-		test('should call getTopPokemon and return ', () => {
-			expect(store.getTopPokemon()).toEqual([
-				{ id: 12, name: 'Narco' },
-				{ id: 13, name: 'Bombasto' },
-				{ id: 14, name: 'Celeritas' },
-				{ id: 15, name: 'Magneta' }
-			]);
-		});
-	});
+	
 });
