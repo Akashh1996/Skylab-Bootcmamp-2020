13ec529a861715e74aa024f0da48b1e7aa308a62
DanielMartinezLozano <dmartinez1604@gmail.com>
2020-10-23 15:36:10 +0200

    comments deleted

--- daniel-martinez/Challenges/pokedex/src/poke-store.test.js
+++ daniel-martinez/Challenges/pokedex/src/poke-store.test.js
@@ -155,15 +155,3 @@ describe('async functions', () => {
         })
     })
 })
-
-
-
-
-/* test('should load one pokemon details from api', () => {
-    const json = jest.fn().mockReturnValueOnce({ id: 12, name: 'Narco'});
-    const fetchImplementation = () => {Promise.resolve({json: json})};
-    global.fetch = jest.fn().mockImplementationOnce(fetchImplementation);
-    return store.loadPokemonsFromAPIById(12).then(() => {
-        expect(store.getPokemon()).toEqual({ id: 12, name: 'Narco' });
-    })
-}) */
