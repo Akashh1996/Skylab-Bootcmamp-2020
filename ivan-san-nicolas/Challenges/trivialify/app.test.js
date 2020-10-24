b68980abe523d6c66ab7eaf040d7f26a52c5c225
Ivansannicolas <ivohan8@gmail.com>
2020-10-24 17:41:36 +0200

    app test created

---
+++ ivan-san-nicolas/Challenges/trivialify/app.test.js
@@ -0,0 +1,9 @@
+describe('Stop button', () => {
+    test('should hide game-body and show game-end', () => {
+        // act
+        const result = endGame(gameBody, gameEnd);
+        // assert
+        expect(gameBody.style.display).toBe('none');
+        expect(gameEnd.style.display).toBe('flex')
+    });
+})
