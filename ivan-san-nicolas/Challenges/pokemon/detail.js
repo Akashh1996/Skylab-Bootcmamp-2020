12d6be1f0b942caabeb6b8c010b8b0572b160d1e
Ivansannicolas <ivohan8@gmail.com>
2020-10-24 12:43:13 +0200

    abilty description pending

--- ivan-san-nicolas/Challenges/pokemon/detail.js
+++ ivan-san-nicolas/Challenges/pokemon/detail.js
@@ -6,24 +6,46 @@ function getNameFromURL(location) {
 }
 
 async function pokemonInfo(name, pokeStore) {
-    debugger;
     await pokeStore.loadPokemonFromAPIByName(name);
     const actualPokemon = await pokeStore.getPokemon();
     const pokemonStats = document.getElementById('pokemon__detail__info__stats');
-    const pokemonDetail = document.getElementById('pokemon__detail__info');
     const pokemonTitle = document.getElementById('pokemon-detail-title')
     pokemonTitle.innerHTML = name;
+    const pokemonAbilities = document.getElementById('pokemon-abilities');
+    const pokemonAbilityTitle = document.getElementById('')
     for (property in actualPokemon) {
-        if (typeof actualPokemon[property] !== 'object' && property !== 'name' && property !== 'is_default' && property !== 'order') {
+        if ((typeof actualPokemon[property] !== 'object') && (property === 'base_experience' || property === 'height' || property === 'id' || property === 'weight')) {
             let element = document.createElement('div');
             element.innerHTML = `<div class="pokemon-detail__info__block">
                     <span class="pokemon-detail__info__block__property-name" id="pokemon-detail-${property}"></span>
                     <span class="pokemon-detail__info__block__property-value" id="pokemon-detail-${actualPokemon[property]}"></span>
-                </div>
-                `;
+                </div>`;
             pokemonStats.appendChild(element);
             document.getElementById(`pokemon-detail-${property}`).innerHTML = property;
             document.getElementById(`pokemon-detail-${actualPokemon[property]}`).innerHTML = actualPokemon[property];
+        } else if (property === 'sprites') {
+            const imageBlock = document.getElementById('pokemon-image')
+            const image = actualPokemon[property].front_default;
+            imageBlock.innerHTML = `<img src="${image}" id="pokemon-detail-${property}"></img>`;
+        } else if (property === 'abilities') {
+            for (index in actualPokemon[property]) {
+                const abilityName = actualPokemon[property][index].ability.name;
+                await pokeStore.loadAbilityFromAPIByName(abilityName);
+                const actualAbility = pokeStore.getAbility();
+                const actualAbilityName = actualAbility.name
+                let element = document.createElement('div');
+                element.innerHTML = `<div class="pokemon-detail__info__block">
+                <span class="pokemon-detail__info__block__property-name pokemon-ability" id="pokemon-detail-ability-${[index]}-name" onclick></span>
+                <span class="pokemon-detail__info__block__property-value pokemon-ability" id="pokemon-detail-ability-${[index]}" onclick></span>
+                </div>`;
+                pokemonStats.appendChild(element);
+                document.getElementById(`pokemon-detail-ability-${[index]}-name`).innerHTML = `ability:`
+                document.getElementById(`pokemon-detail-ability-${[index]}`).innerHTML = actualAbilityName;
+                document.getElementById(`pokemon-detail-ability-${[index]}-name`).addEventListener('click', (actualAbilityName) => {
+                    pokemonAbilities.style.display = 'flex';
+                    pokemonAbilityTitle.innerHTML = actualAbilityName;
+                });
+            }
         }
     }
 }
--- ivan-san-nicolas/Challenges/pokemon/poke-store.js
+++ ivan-san-nicolas/Challenges/pokemon/poke-store.js
@@ -1,5 +1,6 @@
 let _pokemons = [];
 let _pokemon;
+let _ability;
 
 class PokeStore {
 
@@ -11,6 +12,10 @@ class PokeStore {
         return _pokemon;
     }
 
+    getAbility() {
+        return _ability;
+    }
+
     async loadPokemonsFromAPI() {
         let url = 'https://pokeapi.co/api/v2/pokemon?limit=100&offset=200';
         const response = await fetch(url);
@@ -20,9 +25,26 @@ class PokeStore {
 
     async loadPokemonFromAPIByName(name) {
         let url = `https://pokeapi.co/api/v2/pokemon/${name}`;
-        const response = await fetch(url);
-        const pokemon = await response.json();
-        _pokemon = pokemon;
+        try {
+            const response = await fetch(url);
+            const pokemon = await response.json();
+            _pokemon = pokemon;
+        } catch {
+            alert('Error 404: Pokemon not found. Please, wait and try again or try with another pokemon');
+            window.location = 'http://127.0.0.1:5500/ivan-san-nicolas/challenges/pokemon/dashboard.html';
+        }
+    }
+
+    async loadAbilityFromAPIByName(abilityName) {
+        let url = `https://pokeapi.co/api/v2/ability/${abilityName}`
+        try {
+            const response = await fetch(url);
+            const ability = await response.json();
+            _ability = ability;
+        } catch {
+            alert('Error 404: Ability not found. Please, wait and try again or try with another pokemon');
+            window.location = 'http://127.0.0.1:5500/ivan-san-nicolas/challenges/pokemon/dashboard.html';
+        }
     }
 
 }
