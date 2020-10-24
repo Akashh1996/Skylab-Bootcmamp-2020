247be97f7a959a6abdb26b87dc5469e3f40f6e36
Ivansannicolas <ivohan8@gmail.com>
2020-10-24 21:39:21 +0200

    changed things for test to work but not accomplished

--- ivan-san-nicolas/Challenges/trivialify/app.js
+++ ivan-san-nicolas/Challenges/trivialify/app.js
@@ -7,12 +7,19 @@ let player = {
 function endGame(gameBody, gameEnd) {
     gameBody.style.display = 'none';
     gameEnd.style.display = 'flex';
+    return gameBody, gameEnd;
 }
 
-function playAgain(player) {
+function resetPlayer(player) {
     player.points = 0;
     player.correctAnswers = 0;
     player.incorrectAnswers = 0;
+}
+
+function playAgain(player, resetPlayer) {
+    resetPlayer(player);
     gameEnd.style.display = 'none';
     gameBody.style.display = 'flex';
 }
+
+module.exports = player, endGame, resetPlayer, playAgain;
