51701e047975df6717387201076256393dcc3b47
Ivansannicolas <ivohan8@gmail.com>
2020-10-25 01:14:45 +0200

    added get top tracks and fixed arguments of functions

--- ivan-san-nicolas/Challenges/trivialify/spotiStore.js
+++ ivan-san-nicolas/Challenges/trivialify/spotiStore.js
@@ -2,6 +2,7 @@ const cliend_id = '856170a8252b42118e031134ded4900e';
 const _cliend_secret_id = '3654b3089dc14cca8c5a7efd86b2fab5';
 let _token;
 let _artist;
+let _topTracks = [];
 
 
 class SpotifyStore {
@@ -19,7 +20,8 @@ class SpotifyStore {
         return _token;
     }
 
-    async getArtist(url) {
+    async getArtist(id) {
+        const url = `https://api.spotify.com/v1/artists/${id}`
         const response = await fetch(url, {
             method: 'GET',
             headers: {
@@ -27,17 +29,25 @@ class SpotifyStore {
             }
         })
         const data = await response.json();
-        return _artist = data;
+        _artist = data;
+        return _artist;
+    }
+
+    async getTopTracks(id) {
+        const url = `https://api.spotify.com/v1/artists/${id}/top-tracks?country=us`
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                Authorization: 'Bearer ' + _token
+            }
+        })
+        const data = await response.json();
+        _topTracks = data;
+        return _topTracks;
     }
 }
 
 const trivialify = new SpotifyStore();
-
-(async () => {
-    await trivialify.getToken()
-    await trivialify.getArtist('https://api.spotify.com/v1/artists/5IH6FPUwQTxPSXurCrcIov');
-    console.log(_artist);
-})()
 
 
 module.exports = spotiStore;
