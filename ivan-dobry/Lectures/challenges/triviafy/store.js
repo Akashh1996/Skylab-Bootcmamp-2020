da584babe6a1b40d99dada3b67f87aa690ccdfc5
Ivan Dobry <ivandobry@hotmail.es>
2020-10-25 09:47:29 +0100

    detail spotft

--- ivan-dobry/Lectures/challenges/triviafy/store.js
+++ ivan-dobry/Lectures/challenges/triviafy/store.js
@@ -1,7 +1,7 @@
 let _spotifyData;
 const cliend_id = '522f6a81070e477d87135a0c669e9bd5';
 const _cliend_secret_id = 'cb68d9cfbc9d489eb30fd35a21e7a005';
-let token ='BQAQXwOi9qbowIQD7ipXwgsyw27sQSarwxgKSLcqvoRFzx7-ZzMPpGlSGNh7IMEH3GpHrmZ8AiJJ35aZv5LhTTHjC4Pa3Op-rexZco9eQOi0Gk-OSW6okyIXft1mcls3AiaA62qwO8vqnuWtA6YyWc_YGsM9a8wwi2bTaK4'
+let token ='BQA8qbTuTryvUIzA8jn_dfkknXA6HlyXQkarX6Epljy1YUgizCsUVW0S6D_MjC4NOZJra4S8KlmZsuAbLZUHlIBGDyTKD3LOkkMfOtBIvC2kJcw1YqZT0e2xbQmouVSjKMQNXbTRankTzhDWlrO0jfbgfhLvuC6Uh4Egeq8'
 let itemArray = [];
 let artistArray = [];
 
--- ivan-dobry/Lectures/challenges/triviafy/view/detail/detail.js
+++ ivan-dobry/Lectures/challenges/triviafy/view/detail/detail.js
@@ -4,8 +4,10 @@ let button1 = document.getElementById('butons_answer1');
 let button2 = document.getElementById('butons_answer2');
 let button3 = document.getElementById('butons_answer3');
 let button4 = document.getElementById('butons_answer4');
+let buttonArray = [button1, button2, button3, button4]
 let randomArtistsArray = [];
 let randomButtons = [];
+let index = 0;
 
 (async function spoty() {
     await store.getSpotifyData();
@@ -21,7 +23,7 @@ let randomButtons = [];
 
 function randomFour (arr) {
     while(arr.length < 4){
-        var r = Math.floor(Math.random() * 4) + 1;
+        var r = Math.floor(Math.random() * 4);
         if(arr.indexOf(r) === -1) arr.push(r);
     }
 }
@@ -33,21 +35,28 @@ function randomArtists (arr) {
     }
 }
 
-const buttons = document.querySelector('.section__butons');
-buttons.addEventListener('click', (event) => {
-    if (event.target.className === 'butons__answer' ) {
-        if (event.target.innerHTML === artistName.innerHTML) {
-            console.log('correcto!')
-        }
-    }
-});
+
 
 randomArtists (randomArtistsArray);
 randomFour (randomButtons);
 
 function updateDisplay() {
-    artistName.innerHTML = artistArray[randomArtistsArray[0]][0];
-    artistImage.src = artistArray[randomArtistsArray[0]][2];
-    
+    artistName.innerHTML = artistArray[randomArtistsArray[index]][0];
+    artistName.className = artistArray[randomArtistsArray[index]][1];
+    artistImage.src = artistArray[randomArtistsArray[index]][2];
+    buttonArray[randomButtons[0]].innerHTML = artistArray[randomArtistsArray[index]][1];
+    for (let i = 1; i < randomButtons.length; i++) {
+        buttonArray[randomButtons[i]].innerHTML = artistArray[randomArtistsArray[i]][1];
+    }
 }
 
+const buttons = document.querySelector('.section__butons');
+buttons.addEventListener('click', (event) => {
+
+    if (event.target.innerHTML === artistName.className) {
+        console.log('correcto!')
+        index ++;
+        updateDisplay();
+    }
+
+});
