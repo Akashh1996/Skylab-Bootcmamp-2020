985e4f772cd813964af41f9d8ef98f3ed98bad3d
Ivansannicolas <ivohan8@gmail.com>
2020-10-25 20:10:03 +0100

    added try catch

--- ivan-san-nicolas/Challenges/trivialify/spotiStore.js
+++ ivan-san-nicolas/Challenges/trivialify/spotiStore.js
@@ -7,43 +7,59 @@ let _topTracks = [];
 
 class SpotifyStore {
     async getToken() {
-        const response = await fetch('https://accounts.spotify.com/api/token', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                Authorization: 'Basic ' + btoa(cliend_id + ':' + _cliend_secret_id)
-            },
-            body: 'grant_type=client_credentials'
-        });
-        const data = await response.json();
-        _token = data.access_token;
-        return _token;
+        try {
+            const response = await fetch('https://accounts.spotify.com/api/token', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    Authorization: 'Basic ' + btoa(cliend_id + ':' + _cliend_secret_id)
+                },
+                body: 'grant_type=client_credentials'
+            });
+            const data = await response.json();
+            _token = data.access_token;
+            return _token;
+        } catch {
+            alert(`Error: It was not possible to get Spotify API token. Please, wait and try again later.`);
+        }
     }
 
     async getArtist(id) {
         const url = `https://api.spotify.com/v1/artists/${id}`
-        const response = await fetch(url, {
-            method: 'GET',
-            headers: {
-                Authorization: 'Bearer ' + _token
-            }
-        })
-        const data = await response.json();
-        _artist = data;
-        return _artist;
+        try {
+            const response = await fetch(url, {
+                method: 'GET',
+                headers: {
+                    Authorization: 'Bearer ' + _token
+                }
+            })
+            const data = await response.json();
+            _artist = data;
+            return _artist;
+        } catch {
+            alert(`Error: It was not possible to get the Artist: ${id}`);
+            console.log('Artist id error:');
+            console.log(id);
+        }
     }
 
     async getTopTracks(id) {
         const url = `https://api.spotify.com/v1/artists/${id}/top-tracks?country=us`
-        const response = await fetch(url, {
-            method: 'GET',
-            headers: {
-                Authorization: 'Bearer ' + _token
-            }
-        })
-        const data = await response.json();
-        _topTracks = data;
-        return _topTracks;
+        try {
+            const response = await fetch(url, {
+                method: 'GET',
+                headers: {
+                    Authorization: 'Bearer ' + _token
+                }
+            })
+            const data = await response.json();
+            _topTracks = data;
+            return _topTracks;
+        } catch {
+            alert(`Error: It was not possible to get the Artist top Tracks. Artist id: ${id}`);
+            console.log('Artist id error:');
+            console.log(id);
+        }
     }
 }
 
