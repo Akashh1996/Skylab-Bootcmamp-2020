5c49570937ed1140ea03ef9bd20cc97604e6b3f9
Marco-Reg <marcoregueiro@gmail.com>
2020-10-25 15:12:18 +0100

    added one artist

--- Marco-reg/challenge-spotify/store.js
+++ Marco-reg/challenge-spotify/store.js
@@ -2,6 +2,9 @@ let _songs;
 const cliend_id = 'bd87b7392f22490abb0b38a155d9f304';
 const _cliend_secret_id = '993cbe46a8aa47b4a9bf0ce7ea5c5087';
 let token;
+
+
+let artistsArray = [];
 class SpotifyStore {
     async getToken() {
         const response = await fetch('https://accounts.spotify.com/api/token', {
@@ -34,6 +37,9 @@ const spotify = new SpotifyStore();
     await spotify.getToken();
     await spotify.playlist();
     
+    let songsList = _songs.tracks.items;
+    let imageArray = [];
+    
     console.log(_songs);
     let songlist=_songs.tracks.items;
 
@@ -42,5 +48,21 @@ const spotify = new SpotifyStore();
         console.log(songlist[i].track.name);
         songsArray.push(songlist[i].track.name)
     }
+    function generateRandomIndex() {
+        let randomIndex = Math.floor(Math.random() * songsArray.length);
+        return randomIndex;
+    }
+    randomIndex = generateRandomIndex();
+    console.log(randomIndex);
+    function displayRandomValues() {
+        console.log(songsList[randomIndex].track.artists[0].name);
+        document.getElementById("artist-1").innerHTML = songsList[randomIndex].track.artists[0].name;
+        document.getElementById("artist-2").innerHTML = songsList[randomIndex].track.artists[0].name;
+        document.getElementById("artist-3").innerHTML = songsList[randomIndex].track.artists[0].name;
+        //document.getElementById("song-image").src = imageArray[randomIndex].url;
+        //document.querySelector("button").innerHTML = randomIndex;
+    }
+    displayRandomValues();
 })();
+
 module.exports = spotify;
