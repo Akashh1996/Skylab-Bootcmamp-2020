9a9b856374b81277e286c0350cdd153e977956f6
Ivan Dobry <ivandobry@hotmail.es>
2020-10-25 11:15:59 +0100

    set interval

--- ivan-dobry/Lectures/challenges/triviafy/store.js
+++ ivan-dobry/Lectures/challenges/triviafy/store.js
@@ -1,7 +1,7 @@
 let _spotifyData;
 const cliend_id = '522f6a81070e477d87135a0c669e9bd5';
 const _cliend_secret_id = 'cb68d9cfbc9d489eb30fd35a21e7a005';
-let token ='BQA8qbTuTryvUIzA8jn_dfkknXA6HlyXQkarX6Epljy1YUgizCsUVW0S6D_MjC4NOZJra4S8KlmZsuAbLZUHlIBGDyTKD3LOkkMfOtBIvC2kJcw1YqZT0e2xbQmouVSjKMQNXbTRankTzhDWlrO0jfbgfhLvuC6Uh4Egeq8'
+let token ='BQACRKipwIyaQxa5Nbfa0eMzooVKhE3oOk7vmWIJ1OCHK9F1bmYeqy87NUzSw6w_AZpceaa0y63s_LOWXOftQN332CVZWMn1tqz6C2PZNDH1mPz1Gmr4jOBbnXbMp44jlnRs3lucq3B37qA3MbU4ywFLkjx8H6hH2pAbbrs'
 let itemArray = [];
 let artistArray = [];
 
--- ivan-dobry/Lectures/challenges/triviafy/view/detail/detail.js
+++ ivan-dobry/Lectures/challenges/triviafy/view/detail/detail.js
@@ -4,9 +4,12 @@ let button1 = document.getElementById('butons_answer1');
 let button2 = document.getElementById('butons_answer2');
 let button3 = document.getElementById('butons_answer3');
 let button4 = document.getElementById('butons_answer4');
-let buttonArray = [button1, button2, button3, button4]
+let timerBox = document.getElementById('timer');
+let wrongAnswers = ['Paranoid', 'Master of Puppets', 'Ace of Spades', 'Crazy Train', 'The Number of the Beast', 'Highway Star', 'Highway to Hell', 'Cowboys from Hell', 'Rainbow in the Dark', 'Running with the Devil', '2 Minutes to Midnight', 'Bark at the Moon', 'Roots Bloody Roots', 'Cemetery Gates', 'Fear of the Dark', 'Whiplash', 'Symphony of Destruction']
+let buttonArray = [button1, button2, button3, button4];
 let randomArtistsArray = [];
 let randomButtons = [];
+let time = 10;
 let index = 0;
 
 (async function spoty() {
@@ -36,18 +39,20 @@ function randomArtists (arr) {
 }
 
 
-
 randomArtists (randomArtistsArray);
-randomFour (randomButtons);
+
 
 function updateDisplay() {
     artistName.innerHTML = artistArray[randomArtistsArray[index]][0];
     artistName.className = artistArray[randomArtistsArray[index]][1];
     artistImage.src = artistArray[randomArtistsArray[index]][2];
+    randomButtons = [];
+    randomFour (randomButtons);
     buttonArray[randomButtons[0]].innerHTML = artistArray[randomArtistsArray[index]][1];
     for (let i = 1; i < randomButtons.length; i++) {
-        buttonArray[randomButtons[i]].innerHTML = artistArray[randomArtistsArray[i]][1];
+        buttonArray[randomButtons[i]].innerHTML = wrongAnswers[Math.floor(Math.random() * 17)];
     }
+    time = 10;
 }
 
 const buttons = document.querySelector('.section__butons');
@@ -57,6 +62,23 @@ buttons.addEventListener('click', (event) => {
         console.log('correcto!')
         index ++;
         updateDisplay();
+    } else {
+        console.log('Fail!')
+        index ++;
+        updateDisplay();
     }
 
 });
+
+
+
+let timer = setInterval (function () {
+    time --;
+    timerBox.innerHTML = `time: ${time}`;
+
+    if (time === 0) {
+        index ++;
+        updateDisplay();
+    }
+
+}, 1000); 
