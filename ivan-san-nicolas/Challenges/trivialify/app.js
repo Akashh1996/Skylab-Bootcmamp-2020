fc1aae9a95e656d3f45bf52c4b2232af996201be
Ivansannicolas <ivohan8@gmail.com>
2020-10-25 19:14:31 +0100

    everything done but timer

--- ivan-san-nicolas/Challenges/trivialify/app.js
+++ ivan-san-nicolas/Challenges/trivialify/app.js
@@ -3,7 +3,10 @@ let player = {
     correctAnswers: 0,
     incorrectAnswers: 0,
     usedArtists: [],
-    usedOptions: []
+    optionsLeft: [1, 2, 3, 4],
+    correctAnswer: '',
+    answer: false,
+    rounds: 0
 }
 
 let timeLeft = 20;
@@ -18,12 +21,15 @@ function countdown() {
     timer.innerHTML = `Timer: ${timeLeft}`;
     timeLeft--;
     timer.innerHTML = `Timer: ${timeLeft}`;
-    //if (timeLeft === 0) alert('Timeout!!');
+    if (timeLeft === 0) {
+        debugger;
+        return checkAnswer(`option-5`);
+    }
 }
 
 function getRandomArtist(artists) {
-    player.usedOptions = [];
-    const randomNumber = Math.floor(Math.random() * 101);
+    player.optionsLeft = [1, 2, 3, 4];
+    const randomNumber = Math.floor(Math.random() * 120);
     if (player.usedArtists.length !== 0) {
         for (let index = 0; index < player.usedArtists.length; index++) {
             if (player.usedArtists[index] === randomNumber) {
@@ -39,7 +45,6 @@ function getRandomArtist(artists) {
 async function setArtist(randomArtistId) {
     const authorName = document.getElementById('game-author-name__title');
     const authorImage = document.getElementById('album-picture');
-    await trivialify.getToken();
     let actualArtist = await trivialify.getArtist(randomArtistId);
     authorName.innerHTML = actualArtist.name;
     authorImage.innerHTML = `<img src="${actualArtist.images[1].url}"
@@ -48,19 +53,18 @@ async function setArtist(randomArtistId) {
 }
 
 async function setArtistSong(randomArtistId) {
-    let randomOption = Math.floor(Math.random() * 4) + 1;
-    player.usedOptions.push(randomOption);
+    let randomOption = Math.floor(Math.random() * player.optionsLeft.length) + 1;
+    player.optionsLeft.splice(randomOption - 1, 1);
     const song = document.getElementById(`option-${randomOption}`);
-    await trivialify.getToken();
     const artistTrack = await trivialify.getTopTracks(randomArtistId);
+    player.correctAnswer = `option-${randomOption}`;
     song.innerHTML = artistTrack.tracks[0].name;
 }
 
 async function fillOtherOptions() {
-    debugger;
 
     function getWrongArtist(artists) {
-        const randomNumber = Math.floor(Math.random() * 101);
+        const randomNumber = Math.floor(Math.random() * 120);
         for (let index = 0; index < player.usedArtists.length; index++) {
             if (player.usedArtists[index] === randomNumber) {
                 return getWrongArtist(artists);
@@ -71,32 +75,41 @@ async function fillOtherOptions() {
         return setWrongArtist(randomArtistId);
 
     }
+
     async function setWrongArtist(randomArtistId) {
-        await trivialify.getToken();
-        let actualArtist = await trivialify.getArtist(randomArtistId);
-        let randomOption = getOtherOptions();
-        debugger;
-        const song = document.getElementById(`option-${randomOption}`);
-        await trivialify.getToken();
+        const song = document.getElementById(`option-${player.optionsLeft[0]}`);
+        player.optionsLeft.splice(0, 1);
         const artistTrack = await trivialify.getTopTracks(randomArtistId);
         song.innerHTML = artistTrack.tracks[0].name;
-        debugger;
     }
-
-    function getOtherOptions() {
-        debugger;
-        let randomOption = Math.floor(Math.random() * 4) + 1;
-        for (let index = 0; index < player.usedOptions; index++) {
-            if (player.usedOptions[index] === randomOption) {
-                return getOtherOptions();
-            }
-        }
-        debugger;
-        player.usedOptions.push(randomOption);
-        return randomOption;
-    }
-    for (let iteration = 0; iteration < 4; iteration++) {
+    for (let iteration = 0; iteration < 3; iteration++) {
         getWrongArtist(artists);
+    }
+}
+
+function checkAnswer(optionId) {
+    if (player.answer === false) {
+        player.answer = true;
+        document.getElementById('next').style.display = 'flex';
+        if (optionId === player.correctAnswer) {
+            player.correctAnswers++;
+            player.points += 10;
+            document.getElementById('total-points').innerHTML = `total points: ${player.points}`;
+            for (let index = 1; index < 5; index++) {
+                document.getElementById(`option-${index}`).className = 'button game-body-options grey';
+            }
+            const actualOption = document.getElementById(optionId);
+            actualOption.className = 'button game-body-options right';
+        } else {
+            player.incorrectAnswers++;
+            for (let index = 1; index < 5; index++) {
+                if (`option-${index}` === player.correctAnswer) {
+                    document.getElementById(`option-${index}`).className = 'button game-body-options right';
+                } else document.getElementById(`option-${index}`).className = 'button game-body-options grey';
+            }
+            const actualOption = document.getElementById(optionId);
+            actualOption.className = 'button game-body-options wrong';
+        }
     }
 }
 
@@ -105,22 +118,46 @@ function endGame() {
     let gameEnd = document.getElementById('game-end');
     gameBody.style.display = 'none';
     gameEnd.style.display = 'flex';
-    clearInterval(countdownLeft);
+    document.getElementById('endgame-totalpoints').innerHTML = `total points: ${player.points}`;
+    document.getElementById('endgame-correct-answers').innerHTML = `correct answers: ${player.correctAnswers}`;
+    document.getElementById('endgame-incorrect-answers').innerHTML = `incorrect answers: ${player.incorrectAnswers}`;
+
+    //clearInterval(myVar);
 }
 
-function resetPlayer(player) {
-    player.points = 0;
-    player.correctAnswers = 0;
-    player.incorrectAnswers = 0;
-}
 
-function playAgain(player, resetPlayer) {
-    resetPlayer(player);
+function playAgain() {
+    function resetPlayer() {
+        player.points = 0;
+        player.correctAnswers = 0;
+        player.incorrectAnswers = 0;
+    }
+    resetPlayer();
     gameEnd.style.display = 'none';
     gameBody.style.display = 'flex';
 }
 
-setPlayerPoints(player);
-getRandomArtist(artists);
-//fillOtherOptions();
-let countdownLeft = setInterval(countdown, 1000);
+async function nextRound() {
+    player.rounds++;
+    if (player.rounds === 20) {
+        return endGame();
+    } else {
+        document.getElementById('next').style.display = 'none';
+        player.answer = false;
+        for (let index = 1; index < 5; index++) {
+            document.getElementById(`option-${index}`).className = 'button game-body-options'
+        }
+        setPlayerPoints(player);
+        await getRandomArtist(artists);
+        fillOtherOptions();
+        timer = 20;
+    }
+}
+
+(async () => {
+    await trivialify.getToken();
+    setPlayerPoints(player);
+    await getRandomArtist(artists);
+    fillOtherOptions();
+    //var myVar = setInterval(countdown, 1000);
+})()
