c20fb7fbe1aa4e6047d57b6783c7ca9f679caefc
Ivan Dobry <ivandobry@hotmail.es>
2020-10-25 18:28:27 +0100

    some test

--- ivan-dobry/Lectures/challenges/triviafy/store.js
+++ ivan-dobry/Lectures/challenges/triviafy/store.js
@@ -1,7 +1,7 @@
 let _spotifyData;
 const cliend_id = '522f6a81070e477d87135a0c669e9bd5';
 const _cliend_secret_id = 'cb68d9cfbc9d489eb30fd35a21e7a005';
-let token ='BQDynhvkN1ByV2-09yQDhhuhYzPfyU3tDMHw2-QOYqjAmwz_9aF6BJ24fIhtur0hMaSbhISgx2DspGQ7YF-rzlItwGUpJF7BFtiE39r4__dPtWUkDm2we5PvbcK4uJyg3XTcCaFPIdX__3JzUvkw14cZitqQvBZyC3kfEdU'
+let _token ='BQDiIA2_nPXOnFTG09LnX5VWBjR8KCE-rYQk5IlsGISe-ZdONOc-BuwUi51E4tRdrxhloYvXptRSJ69A19bwdZe9kVwpar81PP-uwRT5ndj2pHdB0sNbY02VDJ9SiuHv-4Fu20SOF__-sAB1COmyM7QWFfUj4lAFvHIpqu4'
 let itemArray = [];
 let artistArray = [];
 
@@ -17,20 +17,22 @@ class Store {
 			body: 'grant_type=client_credentials'
 		});
 		const data = await response.json();
-		return (token = data.access_token);
+		return (_token = data.access_token);
 	}
 
     async getSpotifyData() {
         let url = 'https://api.spotify.com/v1/playlists/3RMpYJp5SZZfzQiRI5vfzB'
         const response = await fetch(url, {
             method: 'GET',
-            headers: { Authorization: 'Bearer ' + token }
+            headers: { Authorization: 'Bearer ' + _token }
         });
         const spotifyData = await response.json();
 		_spotifyData = spotifyData;
-		itemArray = _spotifyData.tracks.items;
-    }
+		return itemArray = _spotifyData.tracks.items;
+	}
 	
 }      
 
 const store = new Store();
+
+module.exports = store;
