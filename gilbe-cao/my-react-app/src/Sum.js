8861743e5a7a41cc43c64e96802ef6b4f28216a1
Gilberto Cao <gilbe.cao@gmail.com>
2020-10-26 15:27:20 +0100

    add my react app

---
+++ gilbe-cao/my-react-app/src/Sum.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+/*
+function Sum(props) {
+	return (
+		<h1>
+			{props.a} + {props.b} = {props.a + props.b}
+		</h1>
+	);
+}
+*/
+
+class Sum extends React.Component {
+	componentWillMount() {
+		console.info('>>>>>>>>will mount    ');
+	}
+	componentDidMount() {
+		console.info('>>>>>>>>did mount    ');
+	}
+	render() {
+		console.info('>>>>>>>>rendear');
+		return (
+			<h1>
+				{this.props.a} + {this.props.b} = {this.props.a + this.props.b}
+			</h1>
+		);
+	}
+}
+
+export default Sum;
