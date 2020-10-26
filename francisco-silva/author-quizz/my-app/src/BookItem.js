578ace59e67e61abdfa56d5669479762ba6a3161
francisco ximenes <fximenessilva@gmail.com>
2020-10-26 18:18:35 +0100

    final author quiz

--- francisco-silva/author-quizz/my-app/src/BookItem.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './App.css';
-
-function BookItem(props) {
-    return<li>{props.title}</li>;
-}
-
-export default BookItem;
--- francisco-silva/author-quizz/my-app/src/Header.js
+++ francisco-silva/author-quizz/my-app/src/Header.js
@@ -1,41 +1,15 @@
-import React from "react";
-
-import BookList from "./BookList"
-import './App.css';
-
-
-function Header(){
-    const books = [
-        "The Shining",
-        "The adventures of Huckleberry Finn",
-        "Macbeth",
-        "IT"
-    ]; 
-    const otraLista = [
-        "a",
-        "a",
-        "a",
-        "a",
-        "a",
-        "a",
-        "a",
-
-    ]
+import React from 'react';
+import './Header.css';
+class Header extends React.Component{
+    render(){
         return(
-            <>
             <div class="header">
-                <h1>Author Quiz</h1>
-                <p>Select the book written by the author shown</p>
+                <h1>
+                    Author Quiz
+                </h1>
+                <p>Select the book written by the Author shown</p>
             </div>
-            <div class="main">
-                <img alt="author"/>
-                <BookList books={books}/>
-                <BookList books={otraLista}/>
-                <BookList books={[...otraLista, ...books]}/>
-            </div>
-            </>
-        );
+        )
+    }
 }
-
-
 export default Header;
