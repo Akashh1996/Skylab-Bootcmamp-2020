50da22f3de6a8f6a3b1b45777d68d10292cf2663
Alberto <algmebe1@gmail.com>
2020-10-26 19:09:12 +0100

    dashboard js file created

---
+++ alberto-gomez/challenges/toh-react/src/components/dashboard-component/Dashboard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Dashboard() {
+	return (
+		<>
+			<header>
+				<h1>Tour of Heroes</h1>
+				<button>Dashboard</button>
+				<button>List</button>
+			</header>
+			<main>
+				<h3>Top Heroes</h3>
+			</main>
+		</>
+	);
+}
+
+export default Dashboard;
