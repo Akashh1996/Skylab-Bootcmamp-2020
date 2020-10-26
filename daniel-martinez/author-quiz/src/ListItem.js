3140702ca9784ddfc3676e85255d028802f4b14b
DanielMartinezLozano <dmartinez1604@gmail.com>
2020-10-26 17:39:24 +0100

    author-quiz background color added

--- daniel-martinez/author-quiz/src/ListItem.js
+++ daniel-martinez/author-quiz/src/ListItem.js
@@ -1,8 +1,19 @@
 import React from 'react';
+//import Block from './Block';
 import './ListItem.css'
 
 function ListItem({ title }){
-return <li id="author-option">{title}</li>
+    function isCorrect(){
+        if (title==="The adventures of Huckleberry Finn"){
+            document.getElementById("block").style.backgroundColor = "green";
+            document.getElementById("button__continue").style.display = "block";
+        } else {
+            document.getElementById("block").style.backgroundColor = "red";
+            document.getElementById("button__continue").style.display = "none";
+
+        }
+    }
+return <li id="author-option" onClick={isCorrect}>{title}</li>
 }
 
 export default ListItem;
