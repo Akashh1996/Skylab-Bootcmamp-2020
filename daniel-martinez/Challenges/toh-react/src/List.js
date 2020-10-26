35d32fc223fd2aafe304283bca1288b3bd539d9c
DanielMartinezLozano <dmartinez1604@gmail.com>
2020-10-26 20:57:55 +0100

    list app done

---
+++ daniel-martinez/Challenges/toh-react/src/List.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './List.css'
+import ListItem from './ListItem';
+
+function List(){
+    const heroes = [
+        { id: 11, name: 'Dr Nice' },
+        { id: 12, name: 'Narco' },
+        { id: 13, name: 'Bombasto' },
+        { id: 14, name: 'Celeritas' },
+        { id: 15, name: 'Magneta' },
+        { id: 16, name: 'RubberMan' },
+        { id: 17, name: 'Dynama' },
+        { id: 18, name: 'Dr IQ' },
+        { id: 19, name: 'Magma' },
+        { id: 20, name: 'Tornado' }
+      ];
+    return <section id="list">
+            <h2 id="list__title">My Heroes </h2>
+<ul id="list__ul">{heroes.map(hero => {
+    return ListItem(hero.id, hero.name)
+})}</ul>
+        </section>
+}
+
+export default List;
