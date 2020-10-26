ce85037b7b00782219a7c07a030539e526737137
Eric Martinez <martinezjimenezeric@gmail.com>
2020-10-26 17:03:52 +0100

    propTypes

--- eric-martinez/lecture-React/my-app/src/index.js
+++ eric-martinez/lecture-React/my-app/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import ClickCounter from './Click';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import Sum from './Sum';
 
 const props = {
   a: 6,
@@ -11,6 +12,7 @@ const props = {
 
 ReactDOM.render(
   <React.StrictMode>
+    <Sum {...props}/>
    <ClickCounter />
   </React.StrictMode>,
   document.getElementById('root')
