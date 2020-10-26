19fedca38ae0aef8e91480b5e7db959bbca7b00a
Ivan Dobry <ivandobry@hotmail.es>
2020-10-26 13:47:52 +0100

    lecture react

--- ivan-dobry/my-app/src/index.js
+++ ivan-dobry/my-app/src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Hello from './Hello';
+import Sum from './Sum';
+import ClickCounter from './clickCounter';
+
+const props = {
+	a: 4,
+	b: 2
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Hello now={Date()} />
+		<Sum a={4} b={2} />
+		<Sum {...props} />
+		<ClickCounter />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
