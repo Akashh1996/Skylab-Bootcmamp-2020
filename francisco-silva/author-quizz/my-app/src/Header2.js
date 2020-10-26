5bdef89a68b894f503be8207bd881c48e554ef8d
francisco ximenes <fximenessilva@gmail.com>
2020-10-26 17:15:55 +0100

    updated author quiz

---
+++ francisco-silva/author-quizz/my-app/src/Header2.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+    return(
+        <header>
+            <h1>Author Quiz</h1>
+            <p>Select the book written by the author shown</p>
+        </header>
+    )
+}
+
+export default Header;
