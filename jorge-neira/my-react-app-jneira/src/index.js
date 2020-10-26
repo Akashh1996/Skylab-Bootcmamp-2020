800dc01e21173e59d1b375e5265dc2d6341b5476
Jorge <jorgengarriga@gmail.com>
2020-10-26 14:26:57 +0100

    React Lecture

--- jorge-neira/my-react-app-jneira/src/index.js
+++ jorge-neira/my-react-app-jneira/src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
+import Hello from './Hello';
+import Sum from './Sum';
+// import Clickcounter from './Clickcounter';
+import ClickCounter from './Clickcounter';
+
+// const props = {
+// 	a: 2,
+// 	b: 5
+// };
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<ClickCounter />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
