92a62f65d2312e1f695d5d64f6f5995613e79a6b
francisco ximenes <fximenessilva@gmail.com>
2020-10-26 19:08:07 +0100

    updated ToH

---
+++ francisco-silva/ToH-react/my-app/src/Dashboard.js
@@ -0,0 +1,55 @@
+import './index.css';
+
+
+function Dashboard() {
+
+    let heroes = [
+        { id: 11, name: 'Dr Nice' },
+        { id: 12, name: 'Narco' },
+        { id: 13, name: 'Bombasto' },
+        { id: 14, name: 'Celeritas' },
+        { id: 15, name: 'Magneta' },
+        { id: 16, name: 'RubberMan' },
+        { id: 17, name: 'Dynama' },
+        { id: 18, name: 'Dr IQ' },
+        { id: 19, name: 'Magma' },
+        { id: 20, name: 'Tornado' }
+];
+    return (
+        
+
+        
+    <div className="main">
+      <div className="title__buttons">
+        <h1 className="dashboard_title">Tour of Heroes</h1>
+        <button className="dashboard_btn">Dashboard</button>
+        <button className="heroes_btn">Heroes</button>
+      </div>
+      <div className= "top_heroes">
+          <h2 className="top_heroes_title">Top Heroes</h2>
+          <div className="top_heroes_btn_wrapper"></div>
+          <button className="top_heroes_btn"></button>
+          <button className="top_heroes_btn"></button>
+          <button className="top_heroes_btn"></button>
+          <button className="top_heroes_btn"></button>
+      </div>
+    </div>
+    );  
+    function PrintHeroes() {
+        let buttons = document.getElementsByClassName("top_heroes_btn");
+        for(let i=0; i<4; i++) {
+            buttons[i].innerHTML = heroes[i].name 
+        }
+     
+
+    }
+    
+  }
+
+
+
+
+
+
+
+export default Dashboard;
