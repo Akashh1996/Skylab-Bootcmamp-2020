f007c75bd036eaec7c9239aa238dde958dd70510
Akash Sapkota <akashh.sapkota@gmail.com>
2020-10-26 14:29:43 +0100

    my first react app created

---
+++ akash-sapkota/my-app/src/Sum.js
@@ -0,0 +1,58 @@
+import React from 'react';
+
+/* function Sum(props) {
+	return (
+		<h1>
+			{' '}
+			{props.a}+ {props.b} = {props.a + props.b}{' '}
+		</h1>
+	);
+} */
+/* function Sum({ a, b }) {
+	return (
+		<h1>
+			{' '}
+			{a} + {b} = {a + b}{' '}
+		</h1>
+	);
+}
+ */
+/* class Sum extends React.Component {
+	render() {
+		console.log('render ............. ');
+		return (
+			<h1>
+				{this.props.a} + {this.props.b} = {this.props.a + this.props.b}
+			</h1>
+		);
+	}
+	componentDidMount() {
+		console.log(
+			'did     ............ mount ? is mounted ? yes than console me'
+		);
+	}
+	componentWillMount() {
+		console.log('will .............. mount ? yes , than render');
+	}
+}
+ */
+
+class ClickCounter extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { clicks: 0 };
+	}
+	render() {
+		return (
+			<div
+				onClick={() => {
+					this.setState({ clicks: this.state.clicks + 1 });
+				}}
+			>
+				This div has been clicked {this.state.clicks} times
+			</div>
+		);
+	}
+}
+
+export default ClickCounter;
--- akash-sapkota/my-app/src/index.js
+++ akash-sapkota/my-app/src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ClickCounter from './Sum.js';
 
+const props = {
+	a: 9,
+	b: 2
+};
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		{/* 	<Sum a={10} b={3} />
+		 */}
+		{/* <Sum {...props} /> */}
+		<ClickCounter />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
