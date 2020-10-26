74aa379fb09dd7897fe9bf6130bd960cd17972b5
Ivan Dobry <ivandobry@hotmail.es>
2020-10-26 16:37:16 +0100

    challenge react

---
+++ ivan-dobry/Lectures/challenges/my-app-react/my-app/src/AuthorQuiz.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import BookList from './BookList';
+import Header from './Header';
+
+function AuthorQuiz() {
+	const books = [
+		'The shining',
+		'The adventures of Huckleberry Finn',
+		'Macbeth',
+		'IT'
+	];
+	return (
+		<>
+			<Header />
+			<div className="main-content">
+				<img alt="author" />
+				<BookList books={books} />
+			</div>
+		</>
+	);
+}
+
+export default AuthorQuiz;
