eadc270f971db158c0f8167d7074d2ce26718131
Marco-Reg <marcoregueiro@gmail.com>
2020-10-26 18:17:51 +0100

    added color and hover

--- Marco-reg/challenge-marco-react/src/Main.js
+++ Marco-reg/challenge-marco-react/src/Main.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import './Header.css';
 
 function MainSection(){
     const books=["the Shinning","The adventures of Huckleberry Finn","IT","Macbeth"];
     
+    
 
     return(
-        <div class="authors">
-            <img alt="author"></img>
-            <ul>
+        <div className="authors" id="authors">
+            <img alt="author" src="https://www.biografiasyvidas.com/biografia/t/fotos/twain_mark.jpg"></img>
+            <ul className="list-authors">
                 {books.map((titles)=>{
-                   return <li>{titles}</li>
+                   return <li className="titles" onClick={function changeColor(){
+
+                    let mainDiv=document.getElementById('authors');
+                       if(titles==="The adventures of Huckleberry Finn"){
+                        mainDiv.style.backgroundColor="green";
+                       }else{
+                        mainDiv.style.backgroundColor="red";
+                       }
+                    
+                   }}>{titles}</li>
 
                 })}
             </ul>
