43c371c1c71aa4fd5cecd1fd9605c12706b2f881
Jennifer <j.pichperez@gmail.com>
2020-10-28 00:01:34 +0100

    challenge react airfrance

--- Jennifer/forms-react/src/components/Box.jsx
+++ Jennifer/forms-react/src/components/Box.jsx
@@ -3,19 +3,27 @@ import SelectInput from './SelectInput';
 import Input from './Input';
 import Button from './Button';
 import Toggle from './Toggle';
+import './Box.css';
 
 function Box(props) {
 	return (
-		<section>
-			<SelectInput optionValues={props.optionValues} />
-			<SelectInput optionValues={props.optionPassengers} />
-			<SelectInput optionValues={props.optionClassType} />
-			<Input type="text" placeholder={'Salida de*'} />
-			<Input type="text" placeholder={'Llegada a*'} />
-			<Input type="date" />
-			<Button search={'Buscar Vuelos'} />
-			<Button id={'toggle'} search={'Viaje Profesional'} />
-			<Toggle />
+		<section id="section">
+			<div id="div-selects">
+				<SelectInput optionValues={props.optionValues} />
+				<SelectInput optionValues={props.optionPassengers} />
+				<SelectInput optionValues={props.optionClassType} />
+			</div>
+			<div id="div-inputs">
+				<Input type="text" placeholder={'Salida de*'} />
+				<Input type="text" placeholder={'Llegada a*'} />
+			</div>
+			<div id="div-inputDate">
+				<Input type="date" />
+			</div>
+			<div id="buttons">
+				<Button search={'Buscar Vuelos'} />
+				<Toggle id="toggle" />
+			</div>
 		</section>
 	);
 }
