bfe8a056dc7bfcf0aaaec3cebec34285161eaad5
Verónica Marull <vero@dieresys.com.ar>
2020-10-27 23:01:59 +0100

    add changes

--- veronica-marull/my-react-app/src/TextInput.jsx
+++ veronica-marull/my-react-app/src/TextInput.jsx
@@ -3,9 +3,9 @@ import React, { useState } from 'react';
 function TextInput() {
     const [idValue, setIdValue] = useState('12');
     const [nameValue, setNameValue] = useState('Narco');
-    const [hero, setHero] = useState({id: 12, name: 'Narco'});
+    //const [hero, setHero] = useState({id: 12, name: 'Narco'});
 
-    const onHeroChange = ({ target: { value}}, setValue, property)
+    
 
     const  onIdChange = (event => {
         setIdValue(event.target.value);
@@ -19,11 +19,11 @@ function TextInput() {
         <>
             <p>
                 id: 
-                <inpt type="text" value={idValue} onChange={onIdChange}/>
+                <input type="text" value={idValue} onChange={onIdChange}/>
             </p>
             <p>
                 name: 
-                <inpt type="text" value={nameValue} onChange={onNameChange}/>
+                <input type="text" value={nameValue} onChange={onNameChange}/>
             </p>
         </>
     )
