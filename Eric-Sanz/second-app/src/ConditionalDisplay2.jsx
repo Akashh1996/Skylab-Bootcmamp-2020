3f28fce63e5993f2da8a2ad9cbbcebff1271b74a
Eric Sanz <eric_sanz_rodriguez@hotmail.com>
2020-10-27 13:06:16 +0100

    lecture updated

---
+++ Eric-Sanz/second-app/src/ConditionalDisplay2.jsx
@@ -0,0 +1,21 @@
+import React, {useState} from 'react';
+
+function ConditionalDisplay2(props) {
+    const [isVisible, setIsVisible] = useState(props.isVisible);
+
+    return (
+        <div>
+            <p>
+                {isVisible ? props.children : React.createElement('h2', null, '"Ahora no me ves!"')}
+            </p>
+            <button onClick={() => {
+                setIsVisible(!isVisible);
+            }}
+            >
+                Toggle Children 2
+            </button>
+        </div>
+    );
+}
+
+export default ConditionalDisplay2;
---
+++ Eric-Sanz/second-app/src/index.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import App from './App';
+// import Hello from './Hello';
+// import Sum from './Sum';
+// import ClickCounter from './ClickCounter';
+import reportWebVitals from './reportWebVitals';
+// import myProps from './data';
+import ConditionalDisplay from './ConditionalDisplay';
+import ConditionalDisplay2 from './ConditionalDisplay2';
+
+
+ReactDOM.render(
+  <React.StrictMode> 
+      <ConditionalDisplay isVisible={true}>
+        <div>La suma es 0</div>
+        <div>La suma es mayor que 0</div>
+      </ConditionalDisplay>
+      <ConditionalDisplay2 isVisible={true}>
+        <h2>Ahora me ves!</h2>
+      </ConditionalDisplay2>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+
+// const props = {
+//   a:10,
+//   b:10,
+//   showAlert: function() {
+//     alert(`El resultado es ${this.a + this.b}`);
+//   }
+// }
+
+// const sum = React.createElement(Sum, {...props}, null);
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     {sum}
+//     <ClickCounter />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// al tener el isVisible, pintara el div.
+
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Hello dateNow = {Date()}/>
+//     <App />
+//     <Sum a={10} b= {10}/>
+//     <Sum {...props}/>
+//     <ClickCounter />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
