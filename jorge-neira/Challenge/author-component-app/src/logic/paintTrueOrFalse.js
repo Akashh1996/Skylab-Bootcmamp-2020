f57fe2adbe4c3744dc466f11a0a181639d938d34
Jorge <jorgengarriga@gmail.com>
2020-10-27 11:41:41 +0100

    Puto quien lo lee

--- jorge-neira/Challenge/author-component-app/src/logic/paintTrueOrFalse.js
+++
@@ -1,8 +0,0 @@
-function PaintWrongAnswer(props) {
-	const correctAnswer = 'The Adventure of Huckleberry Finn';
-	const listOfItems = document.querySelectorAll('.listItem');
-	console.log(props);
-
-}
-
-export default PaintWrongAnswer;
