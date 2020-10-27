1deca53e5ef332bcfba07c8dbedf4d002de8b2cb
Eric Martinez <martinezjimenezeric@gmail.com>
2020-10-27 11:41:41 +0100

    add detail

---
+++ eric-martinez/ToH-React/toh-app/src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+	return (
+		<header>
+			<h1>Tour of heroes</h1>
+			<button>Dashboard</button>
+			<button>Heroes</button>
+		</header>
+	);
+}
+
+export default Header;
