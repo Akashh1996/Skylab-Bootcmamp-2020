a630bc9fba1f7eb098f28ed381549625cb7121c7
Eric Sanz <eric_sanz_rodriguez@hotmail.com>
2020-10-27 10:58:19 +0100

    lecture updated

--- Eric-Sanz/second-app/src/index.jsx
+++ Eric-Sanz/second-app/src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import Hello from './Hello';
+// import App from './App';
+// import Hello from './Hello';
 import Sum from './Sum';
 import ClickCounter from './ClickCounter';
 import reportWebVitals from './reportWebVitals';
@@ -11,20 +11,30 @@ const props = {
   a:10,
   b:10,
   showAlert: function() {
-    alert(`El resultado es ${this.a + this.b}`);
+    alert(`El resultado es ${props.a + props.b}`);
   }
 }
 
+const sum = React.createElement(Sum, {...props}, null);
+
 ReactDOM.render(
   <React.StrictMode>
-    <Hello dateNow = {Date()}/>
-    <App />
-    <Sum a={10} b= {10}/>
-    <Sum {...props}/>
+    {sum}
     <ClickCounter />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Hello dateNow = {Date()}/>
+//     <App />
+//     <Sum a={10} b= {10}/>
+//     <Sum {...props}/>
+//     <ClickCounter />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
