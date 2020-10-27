2089cd493b8fe49c886a6136d4f67458fdf1489f
Jorge <jorgengarriga@gmail.com>
2020-10-27 19:12:49 +0100

    Create folder for files

--- jorge-neira/Daily Challenge/ReactForms/challenge-react-forms/src/index.js
+++ jorge-neira/Daily Challenge/ReactForms/challenge-react-forms/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+
+
+
 ReactDOM.render(
 	<React.StrictMode></React.StrictMode>,
 	document.getElementById('root')
