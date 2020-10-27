cc9fd88268e27c098a173e51043c66100dceaa66
Starkin28 <victoreq00@gmail.com>
2020-10-27 09:27:13 +0100

    changes in header

--- victor-escartin/Challenge-TourOfHeroes/tour-of-heroes-react/src/Header/Header.js
+++ victor-escartin/Challenge-TourOfHeroes/tour-of-heroes-react/src/Header/Header.js
@@ -4,20 +4,20 @@ import '../Styles/dashboard.css'
 function Header() {
     return (
         <div id="general-container">
-        <div id="main-container" >
-            <header>
-                <div className="title">Tour of Heroes</div>
-                <div className="options">
-                    <div id="option-dashboard">
-                        <a href="../dashboard/dashboard.html">Dashboard</a>
+            <div id="main-container" >
+                <header>
+                    <div className="title">Tour of Heroes</div>
+                    <div className="options">
+                        <div id="option-dashboard">
+                            <a href="../dashboard/dashboard.html">Dashboard</a>
+                        </div>
+                        <div id="option-heroes">
+                            <a href="../list/list.html">Heroes</a>
+                        </div>
                     </div>
-                    <div id="option-heroes">
-                        <a href="../list/list.html">Heroes</a>
-                    </div>
-                </div>
-            </header>
+                </header>
+            </div>
         </div>
-    </div>
     )
 }
 
