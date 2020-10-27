b6751f7ba891b971f4daa948af0a1b8513e0715b
ivand150 <67744602+ivand150@users.noreply.github.com>
2020-10-27 10:59:50 +0100

    Update store.js

--- ivan-dobry/Lectures/challenges/pokemon/async-pokemon/store.js
+++ ivan-dobry/Lectures/challenges/pokemon/async-pokemon/store.js
@@ -6,7 +6,6 @@ let abilityId;
 class Store {
 
     async getPokemonData() {
-        debugger;
         let url = 'https://pokeapi.co/api/v2/pokemon?limit=20&offset=0';
             const response = await fetch(url);
             const pokemon = await response.json();
