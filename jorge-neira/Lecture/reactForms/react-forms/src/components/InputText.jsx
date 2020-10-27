09455b80e20ea87a2a70e528a7d180ca4001f023
Jorge <jorgengarriga@gmail.com>
2020-10-27 18:50:24 +0100

    Create File for Challenge

---
+++ jorge-neira/Lecture/reactForms/react-forms/src/components/InputText.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+
+ function TextInput() {
+  const [inputValue, setInputValue] = useState('12')
+  const [nameValue, setNameValue] = useState('Narco')
+
+  // const [id, setId] = useState()
+
+  return (
+  <>
+  <p>
+    id: 
+  <input type="text" value={inputValue} onChange={(e) => {
+    setInputValue(e.target.value)
+    console.log(e.target.value)
+  }}></input>
+
+  </p>
+  <p>
+    name: <input type="text" value={nameValue} onChange={(e) => {
+      setNameValue(e.target.value)
+    }}></input>
+  </p>
+
+  </>
+  )
+}
+export default TextInput
