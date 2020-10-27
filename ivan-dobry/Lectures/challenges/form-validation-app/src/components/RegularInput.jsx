4dfc6036dd997b09cacc483ccc35903faf2270d5
Ivan Dobry <ivandobry@hotmail.es>
2020-10-27 18:00:42 +0100

    forms validations inputs

---
+++ ivan-dobry/Lectures/challenges/form-validation-app/src/components/RegularInput.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function RegularInput () {
+    return(
+        <label htmlFor="pasajeros"><input type="text" value='1 Pasajero' id='pasajeros'/></label>
+        
+    )
+}
+
+export default RegularInput;
---
+++ ivan-dobry/Lectures/challenges/form-validation-app/src/components/SelectionInput.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function SelectionInput () {
+    return (
+    <select name="Viaje" id="Viaje">
+        <option value="Ida y Vuelta">Ida y Vuelta</option>
+        <option value="Ida">Ida</option>
+        <option value="Vuelta">Vuelta</option>
+    </select>
+    )
+}
+
+export default SelectionInput;
