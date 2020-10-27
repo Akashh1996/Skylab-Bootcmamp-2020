91237342dbaac26dcb2fc59c9d2f99687f925bf5
Aleix Moya Leiva <aleixmoya99@gmail.com>
2020-10-27 17:20:43 +0100

    lecture input text

---
+++ Aleix_Moya/lecture-React2-Aleix/tutorial2-app/src/ConditionalDisplay.jsx
@@ -0,0 +1,22 @@
+import React, {useState} from 'react';
+
+function ConditionalDisplay(props){
+    debugger;
+    const[isVisible, setIsVisible] = useState(props.isVisible);
+    
+    return (
+        <div>
+            <p>{isVisible ? props.children : React.createElement('h1', null, 'Clica en el boton')}</p>
+            <button 
+                onClick={() => {
+                    setIsVisible(!isVisible);
+                }}
+            >
+            Toggle children
+            </button>
+      </div>
+    );
+}
+
+
+export default ConditionalDisplay;
