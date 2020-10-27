c4b9fb1973609c8fd3375f1c7c96fb0672893f49
Jorge <jorgengarriga@gmail.com>
2020-10-27 12:31:09 +0100

    update lecture

--- jorge-neira/Challenge/toh-react-app/src/index.js
+++ jorge-neira/Challenge/toh-react-app/src/index.js
@@ -4,17 +4,9 @@ import './index.css';
 import DasboardHero from './components/dashboardComponent/dashboardHero';
 import DetailHero from './components/detailComponent/detailHero';
 import ListHero from './components/listComponent/listHero';
-import Sum from './components/sum';
-const props = {
-	a: 4,
-	b: 5,
-	showAlert: function () {
-		console.log('hola');
-	}
-};
+
 ReactDOM.render(
 	<React.StrictMode>
-		React.createElement({(Sum, props, null)})
 		<DasboardHero />
 		<DetailHero />
 		<ListHero />
