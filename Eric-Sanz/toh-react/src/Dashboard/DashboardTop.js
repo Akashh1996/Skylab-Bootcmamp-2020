dc3679dc470209297d58bee0435cae766c9ff63c
Eric Sanz <eric_sanz_rodriguez@hotmail.com>
2020-10-27 07:38:30 +0100

    detail heroes

--- Eric-Sanz/toh-react/src/Dashboard/DashboardTop.js
+++ Eric-Sanz/toh-react/src/Dashboard/DashboardTop.js
@@ -3,7 +3,7 @@ import '../TohStyles.css';
 
 function DashboardTop({heroName}) {
     return (
-        <a className="top-btn" href="../detail/detail.html?heroId=${hero.id}">{heroName}></a>
+        <button className="top-btn">{heroName}</button>
     )
 }
 
--- Eric-Sanz/toh-react/src/List/ListItem.js
+++ Eric-Sanz/toh-react/src/List/ListItem.js
@@ -4,10 +4,10 @@ import '../TohStyles.css';
 
 function ListItem ({heroId, heroName}) {
     return (
-        <a className="list-heroes__btn" href="../Detail/Detail.html?heroId=${hero.id}" alt="heroes">
+        <button className="list-heroes__btn" alt="heroes">
             <span className="list-heroes__btn-id">{heroId}</span>
             <span className="list-heroes__btn-name">{heroName}</span>
-        </a>
+        </button>
     )
 }
 
