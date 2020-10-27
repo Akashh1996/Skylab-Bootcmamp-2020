b86eebf46c04024e0834ef6462b2b48d9dc6d2b3
Starkin28 <victoreq00@gmail.com>
2020-10-27 11:02:33 +0100

    lecture with intro to working with props

--- victor-escartin/Lecture-react/new-app/src/index.js
+++ victor-escartin/Lecture-react/new-app/src/index.js
@@ -3,18 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Sum from './Sum';
+import myProps from './data';
 
-const props={
-  a: 4,
-  b: 2,
-  showAlert: function(){
-    console.log(`El resultado es ${this.a+this.b}`)
-  }
-};
+const props={...props};
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <Sum {...props}/>
+    {React.createElement(
+      'h1', 
+      null,
+      React.createElement(Sum,{...props},null)
+    )}
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -23,3 +24,8 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// {React.createElement(
+//   'h1', 
+//   null,
+//   React.createElement(Sum,{...props},null)
