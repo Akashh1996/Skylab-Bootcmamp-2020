791f112d3a0fe4f9f802ba0700520597c323d7f6
David Mato <dmatochoya@gmail.com>
2020-10-28 18:33:20 +0100

    flux lecture

--- david-mato/form-validation/form-react/src/components/Main.jsx
+++ david-mato/form-validation/form-react/src/components/Main.jsx
@@ -1,33 +1,88 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Select from './Select';
 import TextInput from './TextInput';
 import Textarea from './Textarea';
 import EmailInput from './EmailInput';
 import TelInput from './TelInput';
 import ButtonInput from './ButtonInput';
+import {
+	loadOptions,
+	loadFlight,
+	deleteFlight
+} from '../actions/action-creators';
+import optionsStore from '../stores/options-store';
+import flightsStore from '../stores/flights-store';
 
 function Main() {
-    const selectTypes = [
-        {type: 'Viaje', options: ['Ida y vuelta', 'Solo ida', 'Destinos múltiples']}, 
-        {type: 'Pasajeros *', options: ['Adulto', 'Niño (2 - 11 años)', 'Bebé (0 - 23 meses)', 'Joven (18 - 24 años)', 'Senior (65 años o más)', 'Estudiante nivel superior (18 - 29 años)']}, 
-        {type: 'Clase', options: ['ECONOMY', 'PREMIUM ECONOMY', 'BUSINESS', 'LA PREMIÈRE']}
-    ];
+	const [options, setOptions] = useState(optionsStore.getOptions());
+	const [flight, setFlight] = useState(flightsStore.getFlight(''));
 
-    return (
-        <main>
+	useEffect(() => {
+		optionsStore.addEventListener(onChange);
+		if (!options) {
+			loadOptions();
+		}
+
+		return () => {
+			optionsStore.removeEventListener(onChange);
+		};
+	}, [options]);
+
+	function onChange() {
+		const options = optionsStore.getOptions();
+		setOptions(options);
+	}
+
+	useEffect(() => {
+		console.log('entroo');
+		flightsStore.addEventListener(onChangeFlight);
+
+		return () => {
+			flightsStore.removeEventListener(onChangeFlight);
+		};
+	}, [flight]);
+
+	function onChangeFlight() {
+		const flight = flightsStore.getFlight();
+		setFlight(flight);
+	}
+
+	return (
+		<main>
 			<a href="#">Book for 10+ passengers</a>
 			<form id="form" action="">
-                <div className="select">
-                    {selectTypes.map((object) => <Select type={object.type} options={object.options}/>)}
-                </div>
-				<TextInput />
+				<div className="select">
+					{options &&
+						options.map((object) => (
+							<Select type={object.type} options={object.options} />
+						))}
+				</div>
+				<TextInput flight={flight} />
 				<Textarea />
 				<EmailInput />
 				<TelInput />
 				<ButtonInput />
+				<p>
+					<button
+						onClick={() => {
+							loadFlight();
+						}}
+					>
+						Add flight
+					</button>
+				</p>
+				<p>
+					<button
+						onClick={() => {
+							deleteFlight();
+						}}
+					>
+						Delete flight
+					</button>
+				</p>
 			</form>
 		</main>
-    )
+	);
 }
 
 export default Main;
