426a50d83d6bb5ab34be7cbd6e0204f2ec3ac983
Eric Sanz <eric_sanz_rodriguez@hotmail.com>
2020-10-28 22:02:42 +0100

    hero store load action

---
+++ Eric-Sanz/toh-flux-app/src/stores/hero-store.js
@@ -0,0 +1,42 @@
+import EventEmitter from 'events';
+import dispatcher from '../dispatcher/dispatcher';
+import typeActions from '../actions/type-actions';
+
+const CHANGE = 'CHANGE';
+let _heroes = [];
+
+class HeroStore extends EventEmitter{
+
+    getHeroes() {
+        return _heroes;
+    }
+
+    addEventListener(callback){
+        this.on(CHANGE, callback); 
+    }
+
+    removeEventListener(callback){
+        this.remove(CHANGE, callback);
+    }
+
+    emitChange(){
+        this.emitChange(CHANGE);
+    }
+}
+
+const storeHeroes = new HeroStore();
+
+dispatcher.register((action)=> {
+    switch (action.type) {
+        case typeActions.LOAD_HEROES:
+            _heroes = action.data
+            storeHeroes.emitChange();
+            break;
+    
+        default:
+            break;
+    }
+})
+
+
+export default storeHeroes;
