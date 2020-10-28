005604cfcdeb8223684a4fdfad170619a11187ac
Jorge <jorgengarriga@gmail.com>
2020-10-28 06:45:06 +0100

    delete console.log

--- jorge-neira/Lecture/reactForms/react-forms/src/components/InputText.jsx
+++ jorge-neira/Lecture/reactForms/react-forms/src/components/InputText.jsx
@@ -1,28 +1,32 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
- function TextInput() {
-  const [inputValue, setInputValue] = useState('12')
-  const [nameValue, setNameValue] = useState('Narco')
+function TextInput() {
+	const [inputValue, setInputValue] = useState('12');
+	const [nameValue, setNameValue] = useState('Narco');
 
-  // const [id, setId] = useState()
-
-  return (
-  <>
-  <p>
-    id: 
-  <input type="text" value={inputValue} onChange={(e) => {
-    setInputValue(e.target.value)
-    console.log(e.target.value)
-  }}></input>
-
-  </p>
-  <p>
-    name: <input type="text" value={nameValue} onChange={(e) => {
-      setNameValue(e.target.value)
-    }}></input>
-  </p>
-
-  </>
-  )
+	return (
+		<>
+			<p>
+				id:
+				<input
+					type="text"
+					value={inputValue}
+					onChange={(e) => {
+						setInputValue(e.target.value);
+					}}
+				></input>
+			</p>
+			<p>
+				name:{' '}
+				<input
+					type="text"
+					value={nameValue}
+					onChange={(e) => {
+						setNameValue(e.target.value);
+					}}
+				></input>
+			</p>
+		</>
+	);
 }
-export default TextInput
+export default TextInput;
