7668a16eb4f33ec64cd59fa61c12d7390d805df2
Ivansannicolas <ivohan8@gmail.com>
2020-10-28 15:29:43 +0100

    created airfrance-flux folder

---
+++ ivan-san-nicolas/Challenges/2020-10-28/airfrance-flux/src/main/body/search-button-div/SearchButtonDiv.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './searchButtonDiv.css';
+import SearchButtonItem from './search-button-item/SearchButtonItem';
+
+function SearchButtonDiv() {
+
+    return (
+        <div class="row search-button">
+            <SearchButtonItem />
+        </div>
+    )
+}
+
+export default SearchButtonDiv;
---
+++ ivan-san-nicolas/Challenges/2020-10-28/airfrance-flux/src/main/body/search-button-div/search-button-item/SearchButtonItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './searchButtonItem.css';
+
+function SearchButtonItem() {
+    
+    return (
+        <div class="button">Buscar vuelos</div>
+    )
+
+}
+
+export default SearchButtonItem;
