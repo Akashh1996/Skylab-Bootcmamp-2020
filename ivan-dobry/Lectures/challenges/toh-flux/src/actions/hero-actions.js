6d551cf856e812ec0b625e0239b6b0ceb2ad4dd5
Ivan Dobry <ivandobry@hotmail.es>
2020-10-29 17:15:40 +0100

    more toh

--- ivan-dobry/Lectures/challenges/toh-flux/src/actions/hero-actions.js
+++ ivan-dobry/Lectures/challenges/toh-flux/src/actions/hero-actions.js
@@ -2,7 +2,7 @@ import actionTypes from './action-types';
 import dispatcher from '../dispatcher/dispatcher';
 
 export async function loadHeroes() {
-	const response = await fetch('api/heroes.json');
+	const response = await fetch('/api/heroes.json');
 	const heroes = await response.json();
 
 	dispatcher.dispatch({
