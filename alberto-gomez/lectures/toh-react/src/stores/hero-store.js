306977c51b644ddbaee58e000d31f79afc6e1812
Alma-Co <65400483+Alma-Co@users.noreply.github.com>
2020-10-29 21:59:05 +0100

    Update hero-store.js

--- alberto-gomez/lectures/toh-react/src/stores/hero-store.js
+++ alberto-gomez/lectures/toh-react/src/stores/hero-store.js
@@ -28,7 +28,6 @@ class HeroStore extends EventEmitter {
 const heroStore = new HeroStore();
 
 dispatcher.register((action) => {
-	debugger;
 	switch (action.type) {
 		case actionTypes.LOAD_HEROES:
 			_heroes = action.payload;
