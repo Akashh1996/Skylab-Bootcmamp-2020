76c711a21327e0dda35dbe6a926084a0985f1b2c
Gilberto Cao <gilbe.cao@gmail.com>
2020-10-29 16:43:31 +0100

    add routes toh

---
+++ gilbe-cao/toh-react/src/components/Dashboard.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { loadHeroes } from '../actions/hero-actions';
+import heroStore from '../stores/hero-store';
+
+const AMOUNT = 4;
+function Dashboard() {
+  const [heroes, setHeroes] = useState([]);
+
+  function handleChange() {
+    setHeroes(heroStore.sliceHeroes(AMOUNT));
+  }
+
+  useEffect(() => {
+    heroStore.addEventListener(handleChange);
+
+    if (!heroes || !heroes.length) {
+      loadHeroes();
+    }
+
+    return () => { heroStore.removeEventListener(handleChange); };
+  });
+  return (
+    <>
+      <ul>
+        {heroes.map((hero) => <li key={hero.id}><Link to={`/heroes/${hero.id}`}>{hero.name}</Link></li>)}
+      </ul>
+    </>
+  );
+}
+
+export default Dashboard;
