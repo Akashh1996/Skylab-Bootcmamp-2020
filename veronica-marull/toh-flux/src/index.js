8d0eeccfe19914ed0c14e7fc8f167b67c983199a
Verónica Marull <vero@dieresys.com.ar>
2020-10-29 12:09:05 +0100

    add changes

--- veronica-marull/toh-flux/src/index.js
+++ veronica-marull/toh-flux/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './index.css';
 import HeroList from './components/HeroList';
 import HeroDetails from './components/HeroDetails';
