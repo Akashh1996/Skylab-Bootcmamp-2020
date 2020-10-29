df2e3c2f8b9450eeb0799e734b6a4ff7325169e7
Jorge <jorgengarriga@gmail.com>
2020-10-29 04:43:30 +0100

    Add Header navigation component

---
+++ jorge-neira/Lecture/react-flux-app/src/view/components/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function HeaderNavigation() {
+	return (
+		<header className="header-nav">
+			<h1>Tour of Heroes</h1>
+			<nav className="nav">
+				<div className="nav-buttons">
+					<button className="nav-button">Dashboard</button>
+					<button className="nav-button">Heroes</button>
+				</div>
+			</nav>
+		</header>
+	);
+}
