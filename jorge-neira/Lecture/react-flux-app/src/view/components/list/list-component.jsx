9e1e6fe567837073a3e77ece21cb0393da6357ed
Jorge <jorgengarriga@gmail.com>
2020-10-29 05:12:30 +0100

    Add list component

---
+++ jorge-neira/Lecture/react-flux-app/src/view/components/list/list-component.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import { loadHeroes } from '../../../actions/action-creators';
+import heroStore from '../../../stores/hero-store';
+
+export default function HeroList(props) {
+	const [heroes, setHeroes] = useState(heroStore.getHeroes());
+
+	useEffect(() => {
+		heroStore.addEventListener(handleChange);
+		if (!heroes && !heroes.length) {
+			loadHeroes();
+		}
+		return () => {
+			heroStore.removeEventListener(handleChange);
+		};
+	}, [heroes]);
+
+	function handleChange() {
+		setHeroes(heroStore.getHeroes());
+	}
+
+	return (
+		<section className="list-heroes-container">
+			{(!heroes || !heroes.length) && <h2>There are no heroes available!</h2>}
+			{heroes &&
+				heroes.length &&
+				heroes.map((hero, index) => <li key={index}>{hero.name}</li>)}
+		</section>
+	);
+}
