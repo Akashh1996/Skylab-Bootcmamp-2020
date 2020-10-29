469c182fc74c5c647895e5458264cf3e59fba9b6
Gerard Ramon <gerardramonp@gmail.com>
2020-10-29 12:30:46 +0100

    fix form not reloading

--- daniel-martinez/Challenges/airfrance/src/components/Form.jsx
+++ daniel-martinez/Challenges/airfrance/src/components/Form.jsx
@@ -46,76 +46,80 @@ function Form() {
 	}
 
 	return (
-		<article>
-			<Header />
-			<form key="form">
-				<section className="passengers10" key="passengers10">
-					<p>Book for 10+ passengers</p>
-				</section>
-				<section className="row first" key="first-row">
-					<Select
-						name="Viaje:"
-						id="viajeForm"
-						key="viajeForm"
-						options={viajeForm}
-					/>
-					<Select
-						name="Pasajeros:"
-						id="pasajerosForm"
-						key="pasajerosForm"
-						options={pasajerosForm}
-					/>
-					<Select
-						name="Clase:"
-						id="claseForm"
-						key="claseForm"
-						options={claseForm}
-					/>
-				</section>
-				<section className="row second" key="second-row">
-					<span className="material-icons" key="material-icons-takeoff">
-						flight_takeoff
-					</span>
-					<InputText label="Salida de:" />
-					<span className="material-icons" key="material-icons-land">
-						flight_land
-					</span>
-					<InputText label="Llegada a:" />
-				</section>
-				<section className="dateAndLabel" key="date-and-label">
-					<InputDate label="Fechas de viaje:" />
-				</section>
-				<section className="checkboxPro" key="checkboxPro">
-					<label className="switch" key="switch">
-						<input type="checkbox" />
-						<span className="slider round"></span>
-					</label>
-					<p className="pro">Viaje profesional</p>
-				</section>
-				<section className="row info">
-					<div className="name">
-						<InputText label="Nombre:" text={form?.nombre} />
-					</div>
-					<div className="apellido">
-						<InputText label="Apellido:" />
-					</div>
-					<div className="email">
-						<InputText label="Email:" />
-					</div>
-					<div className="num">
-						<InputText label="Nº teléfono:" />
-					</div>
-				</section>
-				<section className="buttonBuscar" key="buttonBuscar">
-					<button type="button" className="buscar">
-						Rellenar auto.
-					</button>
-					<button type="button" className="buscar">
-						Borrar campos
-					</button>
-				</section>
-			</form>
-		</article>
+		<>
+			{form && (
+				<article>
+					<Header />
+					<form key="form">
+						<section className="passengers10" key="passengers10">
+							<p>Book for 10+ passengers</p>
+						</section>
+						<section className="row first" key="first-row">
+							<Select
+								name="Viaje:"
+								id="viajeForm"
+								key="viajeForm"
+								options={viajeForm}
+							/>
+							<Select
+								name="Pasajeros:"
+								id="pasajerosForm"
+								key="pasajerosForm"
+								options={pasajerosForm}
+							/>
+							<Select
+								name="Clase:"
+								id="claseForm"
+								key="claseForm"
+								options={claseForm}
+							/>
+						</section>
+						<section className="row second" key="second-row">
+							<span className="material-icons" key="material-icons-takeoff">
+								flight_takeoff
+							</span>
+							<InputText label="Salida de:" />
+							<span className="material-icons" key="material-icons-land">
+								flight_land
+							</span>
+							<InputText label="Llegada a:" />
+						</section>
+						<section className="dateAndLabel" key="date-and-label">
+							<InputDate label="Fechas de viaje:" />
+						</section>
+						<section className="checkboxPro" key="checkboxPro">
+							<label className="switch" key="switch">
+								<input type="checkbox" />
+								<span className="slider round"></span>
+							</label>
+							<p className="pro">Viaje profesional</p>
+						</section>
+						<section className="row info">
+							<div className="name">
+								<InputText label="Nombre:" text={form.nombre} />
+							</div>
+							<div className="apellido">
+								<InputText label="Apellido:" />
+							</div>
+							<div className="email">
+								<InputText label="Email:" />
+							</div>
+							<div className="num">
+								<InputText label="Nº teléfono:" />
+							</div>
+						</section>
+						<section className="buttonBuscar" key="buttonBuscar">
+							<button type="button" className="buscar">
+								Rellenar auto.
+							</button>
+							<button type="button" className="buscar">
+								Borrar campos
+							</button>
+						</section>
+					</form>
+				</article>
+			)}
+		</>
 	);
 }
 
