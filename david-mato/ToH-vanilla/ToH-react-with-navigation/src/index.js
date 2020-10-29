07e658c61b1f5b61668bbb57e168c815eafc3c37
David Mato <dmatochoya@gmail.com>
2020-10-29 18:32:21 +0100

    react navigation

--- david-mato/ToH-vanilla/ToH-react-with-navigation/src/index.js
+++ david-mato/ToH-vanilla/ToH-react-with-navigation/src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Header from './Header/Header.js';
-import List2 from './List/List2';
+import List from './List/List';
+import Dashboard from './Dashboard/Dashboard';
+import Detail from './Detail/Detail.js';
+import NotFound from './Detail/NotFound.js';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<Header />
-		<List2 />
+		<BrowserRouter>
+			<Header />
+			<Switch>
+				<Route path="/" exact component={Dashboard} />
+				<Route path="/heroes" exact component={List} />
+				<Route path="/heroes/:heroId" component={Detail} />
+				<Route component={NotFound} />
+			</Switch>
+		</BrowserRouter>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
