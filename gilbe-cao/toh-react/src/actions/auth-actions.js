ff63b29f21ea0c90f90f5539aac4b7f3db4050ba
Gilberto Cao <gilbe.cao@gmail.com>
2020-11-05 13:02:23 +0100

    add firebase config and actions

---
+++ gilbe-cao/toh-react/src/actions/auth-actions.js
@@ -0,0 +1,36 @@
+import './firebase/firebaseIndex';
+import firebase from 'firebase';
+import dispatcher from '../dispatcher/dispatcher';
+import actionTypes from './action-types';
+
+export async function signIn(email, password) {
+  try {
+    const { user } = await firebase.auth().signInWithEmailAndPassword(email, password);
+    dispatcher.dispatch({
+      type: actionTypes.AUTH_LOGIN,
+      payload: {
+        displayName: user.displayName,
+        email: user.email,
+        phoneNumber: user.phoneNumber,
+        photoURL: user.photoURL,
+      },
+    });
+  } catch (error) {
+    dispatcher.dispatch({
+      type: actionTypes.AUTH_LOGIN_ERROR,
+    });
+  }
+}
+
+export async function signOut() {
+  try {
+    await firebase.auth().signOut();
+    dispatcher.dispatch({
+      type: actionTypes.AUTH_SIGNOUT,
+    });
+  } catch (error) {
+    dispatcher.dispatch({
+      type: actionTypes.AUTH_SIGNOUT_ERROR,
+    });
+  }
+}
--- gilbe-cao/toh-react/src/components/Header.jsx
+++ gilbe-cao/toh-react/src/components/Header.jsx
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { signIn, signOut } from '../actions/auth-actions';
 
 const hero = {
   id: 12,
 };
 
 function Header() {
+  const [isLogged] = useState(false);
+
+  function isSignInVisible() {
+    return isLogged
+      ? <button type="button" onClick={(event) => { event.preventDefault(); signOut(); }}>Sign Out</button>
+      : <button type="button" onClick={(event) => { event.preventDefault(); signIn('gilbe.cao@gmail.com', '12345678'); }}>Sign In</button>;
+  }
+
   return (
     <>
-      <h1>Tour of heroes</h1>
+      <h1>TOUR of HEROES</h1>
       <nav className="toh-navigation">
         <Link to="/">Dashboard</Link>
         {' | '}
         <Link to="/heroes">Heroes</Link>
         {' | '}
         <Link to={`/heroes/${hero.id}`}>Narco details</Link>
+        {' | '}
+        {isSignInVisible()}
       </nav>
     </>
   );
