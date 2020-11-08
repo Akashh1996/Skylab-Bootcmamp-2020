dda65069e9a2915cd3e3d84315f9a9c9c705616c
francisco ximenes <fximenessilva@gmail.com>
2020-11-08 22:56:30 +0100

    added gilbe toh react

---
+++ francisco-silva/toh-react-gilbe/src/actions/hero-actions.js
@@ -0,0 +1,43 @@
+import actionTypes from './action-types';
+import dispatcher from '../dispatcher/dispatcher';
+
+export async function loadHeroes() {
+  const response = await fetch('/api/heroes.json');
+  const heroes = await response.json();
+
+  dispatcher.dispatch({
+    type: actionTypes.LOAD_HEROES,
+    payload: heroes,
+  });
+}
+
+export function deleteHero(heroId) {
+  dispatcher.dispatch({
+    type: actionTypes.DELETE_HERO,
+    payload: heroId,
+  });
+}
+
+export function createHero(name) {
+  if (!name.trim()) {
+    return;
+  }
+
+  dispatcher.dispatch({
+    type: actionTypes.ADD_HERO,
+    payload: {
+      id: Date.now(), // unique value for the id
+      name,
+    },
+  });
+}
+
+export async function loadHeroById(heroId) {
+  const response = await fetch('/api/heroes.json');
+  const heroes = await response.json();
+  const hero = heroes.find((heroFind) => heroFind.id === heroId);
+  dispatcher.dispatch({
+    type: actionTypes.LOAD_HERO,
+    payload: hero,
+  });
+}
---
+++ francisco-silva/toh-react-gilbe/src/stores/hero-store.js
@@ -0,0 +1,87 @@
+/* eslint-disable class-methods-use-this */
+import { EventEmitter } from 'events';
+import dispatcher from '../dispatcher/dispatcher';
+import actionTypes from '../actions/action-types';
+
+const CHANGE = 'CHANGE';
+let heroes = [];
+let hero;
+
+export class HeroStore extends EventEmitter {
+  getHeroes() {
+    return heroes;
+  }
+
+  getHero() {
+    return hero;
+  }
+
+  getHeroById(heroId) {
+    return heroes.find((heroFind) => heroFind.id === heroId);
+  }
+
+  deleteHero(heroId) {
+    return heroes.filter((heroFilter) => heroFilter.id !== heroId);
+  }
+
+  updateHero(updatedHero) {
+    return heroes.map((heroMap) => {
+      if (updatedHero.id === heroMap.id) {
+        return updatedHero;
+      }
+      return hero;
+    });
+  }
+
+  sliceHeroes(amount) {
+    if (!amount) {
+      return [];
+    }
+
+    return heroes.slice(0, amount);
+  }
+
+  addEventListener(callback) {
+    this.on(CHANGE, callback);
+  }
+
+  removeEventListener(callback) {
+    this.removeListener(CHANGE, callback);
+  }
+
+  emitChange() {
+    this.emit(CHANGE);
+  }
+}
+
+const heroStore = new HeroStore();
+
+dispatcher.register((action) => {
+  switch (action.type) {
+    case actionTypes.LOAD_HEROES:
+      heroes = action.payload;
+      heroStore.emitChange();
+      break;
+    case actionTypes.DELETE_HERO:
+      heroes = heroStore.deleteHero(action.payload);
+      heroStore.emitChange();
+      break;
+    case actionTypes.ADD_HERO:
+      heroes = [...heroes, action.payload];
+      heroStore.emitChange();
+      break;
+    case actionTypes.UPDATE_HERO:
+      heroes = heroStore.updateHero(action.payload);
+      heroStore.emitChange();
+      break;
+    case actionTypes.LOAD_HERO:
+      hero = action.payload;
+      heroStore.emitChange();
+      break;
+
+    default:
+      break;
+  }
+});
+
+export default heroStore;
