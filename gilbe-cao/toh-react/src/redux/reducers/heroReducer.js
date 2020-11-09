e571409924c3dfcffaf6a8e6a25829863b9a444c
Gilberto Cao <gilbe.cao@gmail.com>
2020-11-09 18:16:00 +0100

    setup redux

---
+++ gilbe-cao/toh-react/src/redux/reducers/heroReducer.js
@@ -0,0 +1,11 @@
+export default function heroReducer(state = [], action) {
+  switch (action.type) {
+    case 'ADD_HERO':
+      return [...state, action.hero];
+    case 'DELETE_HERO':
+      return state.filter((hero) => hero.id !== action.hero.id);
+
+    default:
+      return state;
+  }
+}
