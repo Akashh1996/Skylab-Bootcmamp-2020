189269ef1ed0d6f484ef8bed61b217e2a90f02df
Eric Sanz <eric_sanz_rodriguez@hotmail.com>
2020-11-09 16:06:26 +0100

    changes

--- Eric-Sanz/toh-flux-app/src/components/Header/Header.jsx
+++ Eric-Sanz/toh-flux-app/src/components/Header/Header.jsx
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './HeaderStyles.css';
 import { Link } from 'react-router-dom';
+import { signIn, signOut } from '../../actions/auth-actions';
 
 function Header() {
+	const [isLogged] = useState(false);
+
+	function isSignInVisible() {
+		return isLogged ? (
+			<button
+				className="nav-btn"
+				href="#"
+				onClick={(event) => {
+					event.preventDefault();
+					signOut();
+				}}
+			>
+				Sign Out{' '}
+			</button>
+		) : (
+			<button
+				className="nav-btn"
+				href="#"
+				onClick={(event) => {
+					event.preventDefault();
+					signIn('eric_sanz_rodriguez@hotmail.com', '1234567');
+				}}
+			>
+				Sign In
+			</button>
+		);
+	}
 	return (
 		<nav className="header-container">
 			<h1 className="header-title">Tour of Heroes</h1>
@@ -13,6 +41,8 @@ function Header() {
 			<Link className="nav-btn" to="/List">
 				Heroes
 			</Link>
+			{' | '}
+			{isSignInVisible()}
 		</nav>
 	);
 }
