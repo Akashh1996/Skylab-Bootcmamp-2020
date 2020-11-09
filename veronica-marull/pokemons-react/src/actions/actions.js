902ce3bacfe4bf386734dc4f0b72883b7ca6ae93
Verónica Marull <vero@dieresys.com.ar>
2020-11-09 15:05:41 +0100

    add changes

--- veronica-marull/pokemons-react/src/actions/actions.js
+++ veronica-marull/pokemons-react/src/actions/actions.js
@@ -5,7 +5,7 @@ export async function loadPokemons() {
 	const response = await fetch('https://pokeapi.co/api/v2/pokemon');
 	const pokemonsResponse = await response.json();
 
-	console.log(pokemonsResponse);
+	console.log(pokemonsResponse.results);
 
 	dispatcher.dispatch({
 		type: actionTypes.LOAD_POKEMONS,
