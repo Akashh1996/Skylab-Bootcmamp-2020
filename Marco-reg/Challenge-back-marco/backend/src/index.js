02b7a3dbc63ffa9c437375a947ee801483bae799
Marco-Reg <marcoregueiro@gmail.com>
2020-11-10 18:27:03 +0100

    asdasdasd

---
+++ Marco-reg/Challenge-back-marco/backend/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
+
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
