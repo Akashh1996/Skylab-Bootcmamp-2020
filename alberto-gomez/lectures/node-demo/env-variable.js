47a57f40bba99b469169aaac7e668b4d5d862383
Alberto <algmebe1@gmail.com>
2020-11-10 17:06:15 +0100

    nodeServer

---
+++ alberto-gomez/lectures/node-demo/env-variable.js
@@ -0,0 +1,7 @@
+const { Console } = require('console');
+
+console.log('Current user is: ', process.env.USER);
+
+console.log('\nScript ejecutado con: ');
+console.log('VAL1 equals to:', process.env.VAL1);
+console.log('VAL2 equals to:', process.env.VAL2);
