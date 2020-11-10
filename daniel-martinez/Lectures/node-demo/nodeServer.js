0713793217a276adcfb0353b5cf0acb9d3a5f42c
DanielMartinezLozano <dmartinez1604@gmail.com>
2020-11-10 16:47:12 +0100

    changed nodeServer

--- daniel-martinez/Lectures/node-demo/nodeServer.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-	res.end('Skylab mola un huevo');
-});
-
-server.listen(3000, () => {
-	console.log('Server is running...');
-});
