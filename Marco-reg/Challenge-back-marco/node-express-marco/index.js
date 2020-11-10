1f27a281144af561150b80b37b9bd18432406004
Marco-Reg <marcoregueiro@gmail.com>
2020-11-10 22:42:06 +0100

    added curl

--- Marco-reg/Challenge-back-marco/node-express-marco/index.js
+++ Marco-reg/Challenge-back-marco/node-express-marco/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 3000;
 
 const app = express();
 
@@ -8,5 +10,28 @@ app.use(cors());
 app.get('/', (req, res) => {
 	res.send('Hello World!');
 });
+app.get('/users', (req, res) => {
+	return res.send('GET HTTP method on user resource');
+});
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.post('/users', (req, res) => {
+	return res.send('POST HTTP method on user resource');
+});
+
+app.put('/users', (req, res) => {
+	return res.send('PUT HTTP method on user resource');
+});
+
+app.delete('/users', (req, res) => {
+	return res.send('DELETE HTTP method on user resource');
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+app.put('/users/:userId', (req, res) => {
+	return res.send(`PUT HTTP method on user/${req.params.userId} resource`);
+});
+
+app.delete('/users/:userId', (req, res) => {
+	return res.send(`DELETE HTTP method on user/${req.params.userId} resource`);
+});
