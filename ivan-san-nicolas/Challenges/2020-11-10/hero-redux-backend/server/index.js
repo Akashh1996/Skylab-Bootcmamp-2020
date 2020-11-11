7b8c37ac8e090e414af47c54b4481be1890e628c
Ivan San <ivansan@MacBook-Pro-de-Ivan.local>
2020-11-11 22:22:26 +0100

    backend done

--- ivan-san-nicolas/Challenges/2020-11-10/hero-redux-backend/server/index.js
+++ ivan-san-nicolas/Challenges/2020-11-10/hero-redux-backend/server/index.js
@@ -1,17 +1,18 @@
 const express = require('express');
+const app = express();
+
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const app = express();
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const port = process.env.PORT || 3838;
-
+const port = process.env.PORT || 9999;
 const heroRoutes = require('./routes/heroRoutes')();
+
 app.use('/heroes', heroRoutes);
 
 app.listen(port, () => {
-    console.log(`Server running in port ${port}`);
+	console.log(`Funciona en el puerto ${port}`);
 });
--- ivan-san-nicolas/Challenges/2020-11-10/hero-redux-backend/server/routes/heroRoutes.js
+++ ivan-san-nicolas/Challenges/2020-11-10/hero-redux-backend/server/routes/heroRoutes.js
@@ -1,14 +1,34 @@
 const express = require('express');
-const heroes = require('../api/heroes.json');
-
-const pokeRouter = express.Router();
+const heroRouter = express.Router();
+let heroes = require('../api/heroes.json');
 
 function routes() {
-    pokeRouter.route('/').get((req, res) => {
-            res.status(200);
-            res.send(heroes);
+	heroRouter.route('/').get((req, res) => {
+		res.status(200);
+		res.send(heroes);
+    })
+    .post((req, res) => {
+        const hero = {id: 25, name: 'Iván'};
+        heroes.push(hero);
+		res.status(200);
+		res.send(heroes);
+    })
+    .delete((req, res) => {
+        const heroId = 12;
+        let newHeroes = heroes.filter((hero) => 
+            hero.id !== heroId
+        );
+        heroes = [...newHeroes];
+        res.status(200);
+        res.send(heroes);
+    })
+    
+    heroRouter.route('/:heroId').get((req, res) => {
+        const heroId = +req.params.heroId;
+        const hero = heroes.find((hero) => hero.id === heroId)
+        res.status(200);
+        res.send(hero); 
     });
-    return pokeRouter;
+	return heroRouter;
 }
-
 module.exports = routes;
