2e40060ca5f2ba7d37df02bacd92eaac1da90cb2
fbiayna <ferranbiaynacosta@gmail.com>
2020-11-11 15:58:23 +0100

    backend santi -done

--- Ferran-Biayna/node-server-santi/index.js
+++ Ferran-Biayna/node-server-santi/index.js
@@ -1,21 +1,11 @@
-const { response } = require('express');
 const express = require('express');
+const testRoute = require('./src/routes/routes');
 
 const app = express();
 
 const port = process.env.PORT || 1240;
 
-app.get('/', (req, res) => {
-	res.end('Skylab mola');
-});
-
-app.post('/post', (req, res) => {
-	res.end('Skylab mola');
-});
-
-app.put('/put',(req,res)=>{
-	res.end('Este es el PUT')
-})
+app.use('/', testRoute);
 
 app.listen(port, () => {
 	console.log(`server up and running in port ${port}`);
