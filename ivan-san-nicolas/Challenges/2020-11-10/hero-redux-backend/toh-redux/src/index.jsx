3e58e4258b45b03f29847901bea5baf833b815c4
Ivan San <ivansan@MacBook-Pro-de-Ivan.local>
2020-11-12 00:27:00 +0100

    initialState setted to configureStore in index.jsx

--- ivan-san-nicolas/Challenges/2020-11-10/hero-redux-backend/toh-redux/src/index.jsx
+++ ivan-san-nicolas/Challenges/2020-11-10/hero-redux-backend/toh-redux/src/index.jsx
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import configureStore from './redux/configureStore';
 
-const store = configureStore();
+const store = configureStore({heroesR: { heroesArray: [] }});
 
 ReactDOM.render(
   <React.StrictMode>
