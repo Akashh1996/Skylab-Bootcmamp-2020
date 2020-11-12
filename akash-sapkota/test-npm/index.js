cd3bd768df92626bd49ef08f144a97aa467aee7e
Akash Sapkota <akashh.sapkota@gmail.com>
2020-11-12 10:17:25 +0100

    created npm package and published

---
+++ akash-sapkota/test-npm/index.js
@@ -0,0 +1,8 @@
+export default function sayHi(name){
+    if(!name){
+        name = "stranger"
+    }
+    console.log(`***************`);
+    console.log(`Hi ${name}, welcome to skylab`);
+    console.log(`***************`);
+}
---
+++ akash-sapkota/test-npm/readme.md
@@ -0,0 +1,11 @@
+# say hi from skylab
+
+# Description
+we greet you from `skylab` the best bootcamp of the world!
+
+this is a demo where we show how easy is to publish a node package
+
+## Technologies
+
+- Node.js
+- VS Code
