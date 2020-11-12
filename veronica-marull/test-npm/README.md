924338b301abf1a424b76b5d9f6de9df60da6845
Verónica Marull <vero@dieresys.com.ar>
2020-11-12 22:57:25 +0100

    add changes

---
+++ veronica-marull/test-npm/README.md
@@ -0,0 +1,10 @@
+# Say hi from Skylab 
+
+
+## Description
+we greet you from `Skylab` the best bootcamp of the world!
+
+## Tecnologies
+
+- Node.js
+- Visual Studio Code
---
+++ veronica-marull/test-npm/index.js
@@ -0,0 +1,8 @@
+export default function sayHi(name) {
+	if (!name) {
+		name = 'stranger';
+	}
+	console.log('********************8');
+	console.log(`Hi ${name}m welcome to Skylab!`);
+	console.log('*******************8');
+}
