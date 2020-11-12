7257dbb02430e1449bdbb0692bb4b3a729dbb543
Akash Sapkota <akashh.sapkota@gmail.com>
2020-11-13 00:12:15 +0100

    pending delete and add

--- akash-sapkota/toh-fullstack/front/src/App.js
+++ akash-sapkota/toh-fullstack/front/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import HeroDetail from './components/HeroDetail/HeroDetail';
+import HeroList from './components/HeroList/HeroList';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<HeroList />,
+		<HeroDetail />
+	);
 }
 
 export default App;
--- akash-sapkota/toh-fullstack/front/src/index.js
+++ akash-sapkota/toh-fullstack/front/src/index.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import configureStore from './redux/configureStore';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import HeroDetail from './components/HeroDetail/HeroDetail';
+import HeroList from './components/HeroList/HeroList';
+
+
+const store = configureStore();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Provider store={store}>
+			<BrowserRouter>
+        <Switch>
+            <Route path = "/" exact component = {HeroList} ></Route>
+            <Route path = "/:heroId" exact component = {HeroDetail} ></Route>
+        </Switch>
+      </BrowserRouter>
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
