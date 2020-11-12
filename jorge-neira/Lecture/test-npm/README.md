22b5b9da7d0295d76d941a3a866b2793a862b044
Jorge <jorgengarriga@gmail.com>
2020-11-12 13:06:54 +0100

    Add new package function

--- jorge-neira/Lecture/test-npm/README.md
+++ jorge-neira/Lecture/test-npm/README.md
@@ -1,7 +1,10 @@
-# First test publish package
+# First Package published
 
-Testing function!
+Password generator
+Call function with the length password you want
 
 ## Change log
 
+- version 1.0.0
 - version 1.0.2
+- version 2.0.1
--- jorge-neira/Lecture/test-npm/index.js
+++ jorge-neira/Lecture/test-npm/index.js
@@ -1,8 +1,20 @@
-function sayHello(name) {
-	!name && (name = 'John Doe');
-	console.log(`*******************************`);
-	console.log(`Hello ${name}, wellcome to skylab!`);
-	console.log(`*******************************`);
+function passwordGenerator(numOfCharacters) {
+	let str = '';
+	for (counter = 0; counter <= numOfCharacters; counter++) {
+		let randomNum = 0 + parseInt(Math.random() * 127);
+		randomNum > 33 ? (str += String.fromCharCode(randomNum)) : counter--;
+	}
+	return str;
 }
 
-module.exports = sayHello;
+module.exports = passwordGenerator;
+
+
+
+const arr = [1, 2, 4, 5, 4, 9, 4, 2, 7]; // 8
+
+function sumCheck(arr, result) {}
+
+for (let index = 0; index < arr.length; index++) {
+
+}
