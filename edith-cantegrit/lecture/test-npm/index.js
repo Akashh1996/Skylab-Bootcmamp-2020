eee345454e96964133dc9810a2a8db7a478006a7
Alma-Co <edith.cantegrit@gmail.com>
2020-11-12 11:08:38 +0100

    npm import eric function

--- edith-cantegrit/lecture/test-npm/index.js
+++ edith-cantegrit/lecture/test-npm/index.js
@@ -1,4 +1,4 @@
-export default function sayHi(name) {
+function sayHi(name) {
 	if (!name) {
 		name = 'stranger';
 	}
@@ -6,3 +6,5 @@ export default function sayHi(name) {
 	console.log(`Hi ${name}, welcome to Skylab!`);
 	console.log('*******');
 }
+
+module.exports = sayHi();
