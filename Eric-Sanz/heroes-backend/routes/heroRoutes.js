31e75e1879854bec54a7bd3c46a6e7596ec7c76e
Eric Sanz <eric_sanz_rodriguez@hotmail.com>
2020-11-13 12:02:09 +0100

    routes done

--- Eric-Sanz/heroes-backend/routes/heroRoutes.js
+++ Eric-Sanz/heroes-backend/routes/heroRoutes.js
@@ -7,25 +7,29 @@ function routes() {
 	heroRouter
 		.route('/')
 		.get((request, response) => {
+			response.status(200);
 			response.send(heroes);
 		})
 		.post((request, response) => {
 			// const { id, name } = request.query;
 			const hero = { id: 69, name: 'Ferran' };
 			heroes.push(hero);
+			response.status(200);
 			response.send(heroes);
 		})
 		.put((request, response) => {
 			const newHero = { id: 12, name: 'Momo' };
 			heroes.forEach((hero) => {
 				hero.id === newHero.id ? (hero.name = newHero.name) : hero;
-			}) && response.send(heroes);
+			});
+			response.status(200);
+			response.send(heroes);
 		})
 		.delete((request, response) => {
-			const { id } = req.query;
-			const heroId = heroes.findIndex((hero) => hero.id === +id);
-			heroes.splice(heroId, 1);
-			res.status(200);
+			const { id } = request.query;
+			let hero = heroes.find((hero) => hero.id === id);
+			heroes.splice(heroes.indexOf(hero), 1);
+			response.status(200);
 			response.send(heroes);
 		});
 	// pokeRouter.route('/:pokeid');
