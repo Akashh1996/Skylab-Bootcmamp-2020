2251d6f9620d1ffa8177d5130d93f8778e558aa7
francisco ximenes <fximenessilva@gmail.com>
2020-11-14 00:15:42 +0100

    back funciona nao tocar

---
+++ francisco-silva/ecommerce-fullstack/backend/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const port = process.env.PORT || 3020;
+
+const { v4: uuidv4 } = require('uuid');
+
+const bodyParser = require('body-parser');
+
+const app = express();
+uuidv4();
+
+const productRoute = require('./routes/productsRoutes');
+
+app.use('/products', productRoute);
+
+const testRoute = require('./routes/routes');
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use('/', testRoute);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ francisco-silva/ecommerce-fullstack/frontend/src/redux/actions/heroAction.js
@@ -0,0 +1,71 @@
+import actionTypes from './actionTypes';
+import axios from 'axios';
+
+function requestHeroesSuccess(pokemonList) {
+	return {
+		type: actionTypes.LOAD_HEROES,
+		pokemonList
+	};
+}
+
+function requestHeroesError(error) {
+	
+	return {
+		type: actionTypes.LOAD_HEROES_ERROR,
+		error
+	};
+}
+
+export function requestHeroes() {
+	return async (dispatch) => {
+		const endpoint = 'http://localhost:1400/heroes';
+		try {
+			
+			const pokemons = await axios.get(endpoint) /* {
+				params: {
+					limit: 151,
+					offset: 0
+				}
+			}); */
+			
+			dispatch(requestHeroesSuccess(pokemons.data));
+		} catch (error) {
+			
+			dispatch(requestHeroesError(error));
+		}
+	};
+}
+
+function requestHeroDetailSuccess(heroDetail){
+	return{
+		type: actionTypes.LOAD_HERO_DETAIL,
+		heroDetail
+	}
+}
+function requestheroDetailError(error) {
+	
+	return {
+		type: actionTypes.LOAD_HERO_DETAIL_ERROR,
+		error
+	};
+}
+
+export function requestHeroDetail(id) {
+	return async (dispatch) => {
+		const endpoint = `http://localhost:1400/heroes/${id}`;
+		try {
+			
+			const hero = await axios.get(endpoint, {
+				params: {
+					heroId : id
+				}
+			})
+			dispatch(requestHeroDetailSuccess(hero.data));
+		} catch (error) {
+			
+			dispatch(requestheroDetailError(error));
+		}
+	};
+}
+
+
