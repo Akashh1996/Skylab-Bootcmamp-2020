742907a84f3f18f409fd5c1afb56a7f99c7de6b6
francisco ximenes <fximenessilva@gmail.com>
2020-11-13 11:24:58 +0100

    lkawjed

---
+++ francisco-silva/back-express/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const debug = require('debug')('app');
+const chalk = require('chalk');
+const morgan = require('morgan');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(morgan('tiny'));
+
+
+app.use(express.static(path.join(__dirname, '/public')))
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+
+
+
+app.get('/', (req, res) => {
+    
+    res.sendFile(path.join(__dirname, 'src/views/', 'index.html'));
+})
+
+
+
+
+app.listen(port, function (errorListen) {
+    console.log(`Server is running on port ${chalk.blue(port)}`)
+})
