f24f5e60561cb9babde61ecd3acef1714ae1ff41
Akash Sapkota <akashh.sapkota@gmail.com>
2020-11-13 14:34:35 +0100

    on progress

--- akash-sapkota/challenges/toh-react-v2/src/components/HeroDetail.jsx
+++ akash-sapkota/challenges/toh-react-v2/src/components/HeroDetail.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import { loadHeroes } from '../actions/hero-actions';
 import heroStore from '../stores/hero-store';
 
