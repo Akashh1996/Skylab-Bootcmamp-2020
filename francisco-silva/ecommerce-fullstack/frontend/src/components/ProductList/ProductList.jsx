f233b8edf5d01e911486722d10a8b75cab7d0500
francisco ximenes <fximenessilva@gmail.com>
2020-11-14 18:33:26 +0100

    depois de akash foder tudo

--- francisco-silva/ecommerce-fullstack/frontend/src/components/ProductList/ProductList.jsx
+++ francisco-silva/ecommerce-fullstack/frontend/src/components/ProductList/ProductList.jsx
@@ -4,35 +4,27 @@ import { connect } from 'react-redux';
 import { requestProducts } from '../../redux/actions/productAction';
 import { Link } from 'react-router-dom';
 import './HeroList.css';
-
-
-
 function ProductList({ productList, dispatch}) {
     debugger;
-    if (!productList && !productList?.length) {
+    if (productList.length <= 0) {
         dispatch(requestProducts());
     }
     console.log(productList)
     debugger;
     return (
-        
         <div className = "list-wrapper">
-
             {productList &&
                 productList.length &&
-                productList.map((product) => <p key = {product.id}>
-                  
-                     <span><Link to= {`/${product.id}`}>{product.name}</Link></span>
-                </p> )}
+                productList[0].map((product) => {
+                return <p>{product["product-name"]}</p>
+                })}
         </div>
-        
     );
-    
 }
 function mapStateToProps(state) {
    debugger
     return {
-        productList: state.productReducer.productList
+        productList: state.productReducer
     };
 }
 function mapDispatchToProps(dispatch) {
--- francisco-silva/ecommerce-fullstack/frontend/src/redux/actions/productAction.js
+++ francisco-silva/ecommerce-fullstack/frontend/src/redux/actions/productAction.js
@@ -1,70 +1,54 @@
 import actionTypes from './actionTypes';
 import axios from 'axios';
-
 function requestProductsSuccess(productList) {
-	debugger;
-	// console.log(productList[0]["product-name"])
-	return {
-		type: actionTypes.LOAD_PRODUCTS,
-		productList
-		
-	};
+    debugger;
+    return {
+        type: actionTypes.LOAD_PRODUCTS,
+        productList
+    };
 }
-
 function requestProductsError(error) {
-	
-	return {
-		type: actionTypes.LOAD_PRODUCTS_ERROR,
-		error
-	};
+    return {
+        type: actionTypes.LOAD_PRODUCTS_ERROR,
+        error
+    };
 }
-
 export function requestProducts() {
-	return async (dispatch) => {
-		debugger;
-		const endpoint = 'http://localhost:3020/products';
-		try {
-		
-			const products = await axios.get(endpoint) 
-					
-			dispatch(requestProductsSuccess(products.data));
-		} catch (error) {
-			
-			dispatch(requestProductsError(error));
-		}
-	};
+    return async (dispatch) => {
+        debugger;
+        const endpoint = 'http://localhost:3020/products';
+        try {
+            const products = await axios.get(endpoint)
+            dispatch(requestProductsSuccess(products.data));
+        } catch (error) {
+            dispatch(requestProductsError(error));
+        }
+    };
 }
-
 function requestProductDetailSuccess(productDetail){
-	return{
-		type: actionTypes.LOAD_PRODUCT_DETAIL,
-		productDetail
-	}
+    return{
+        type: actionTypes.LOAD_PRODUCT_DETAIL,
+        productDetail
+    }
 }
 function requestProductDetailError(error) {
-	
-	return {
-		type: actionTypes.LOAD_PRODUCT_DETAIL_ERROR,
-		error
-	};
+    return {
+        type: actionTypes.LOAD_PRODUCT_DETAIL_ERROR,
+        error
+    };
 }
-
 export function requestProductDetail(id) {
-	return async (dispatch) => {
-		const endpoint = `http://localhost:1400/heroes/${id}`;
-		try {
-			
-			const product = await axios.get(endpoint, {
-				params: {
-					productId : id
-				}
-			})
-			dispatch(requestProductDetailSuccess(product.data));
-		} catch (error) {
-			
-			dispatch(requestProductDetailError(error));
-		}
-	};
+    return async (dispatch) => {
+        const endpoint = `http://localhost:1400/heroes/${id}`;
+        try {
+            const product = await axios.get(endpoint, {
+                params: {
+                    productId : id
+                }
+            })
+            dispatch(requestProductDetailSuccess(product.data));
+        } catch (error) {
+            dispatch(requestProductDetailError(error));
+        }
+    };
 }
-
-
