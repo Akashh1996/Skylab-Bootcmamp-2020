f9e0a9a0eca61634bc3d8635b6fb4b1bfd6b8e65
Jorge <jorgengarriga@gmail.com>
2020-11-14 19:31:06 +0100

    Server package conf

--- jorge-neira/Challenge/FullStack-Ecommerce/root/server/index.js
+++ jorge-neira/Challenge/FullStack-Ecommerce/root/server/index.js
@@ -3,11 +3,13 @@ const debug = require('debug')('asusApp');
 const morgan = require('morgan');
 const chalk = require('chalk');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const asusRouter = require('./src/routes/asusRouter.js')();
 
 const asusApp = express();
 const port = process.env.PORT || 5000;
 
+asusApp.use(cors());
 asusApp.use(morgan('tiny'));
 asusApp.use(bodyParser.urlencoded({ extended: true }));
 asusApp.use(bodyParser.json());
