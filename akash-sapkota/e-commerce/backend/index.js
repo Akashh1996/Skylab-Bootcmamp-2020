fc1d5157b776642badf362940990617ce9e5e99d
Akash Sapkota <akashh.sapkota@gmail.com>
2020-11-15 16:04:10 +0100

    added cors

--- akash-sapkota/e-commerce/backend/index.js
+++ akash-sapkota/e-commerce/backend/index.js
@@ -1,3 +1,4 @@
+const cors = require('cors');
 const express = require('express');
 const path = require('path');
 const debug = require('debug')('app');
@@ -11,6 +12,7 @@ const app = express();
 const port = process.env.PORT || 6000;
 
 app.use(morgan('tiny'));
+app.use(cors);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
