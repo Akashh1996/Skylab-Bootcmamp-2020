126d65fbb2c82ef51d802103c5722241103c54a4
Ivan San <ivansan@MacBook-Pro-de-Ivan.local>
2020-11-15 18:52:58 +0100

    delete product from cart petition added to cartRoutes

--- ivan-san-nicolas/Challenges/e-commerce/backend/routes/cartRoutes.js
+++ ivan-san-nicolas/Challenges/e-commerce/backend/routes/cartRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cart = require('../api/cart.json');
+let cart = require('../api/cart.json');
 
 const cartRouter = express.Router();
 
@@ -10,8 +10,24 @@ function routes() {
         res.send(cart);
     })
     .post((req, res) => {
+        const product = req.query.productName;
+        res.status(200);
+        res.send(cart);
+    })
+    .delete((req, res) => {
         console.log(req);
-        const product = req.query.product;
+        const productName = req.query.productName;
+        const newCart = [];
+        let repeatedProducts = 0;
+        cart[0]["product-list"].map((product) => {
+            if(product["product-name"] !== productName || repeatedProducts > 0) {
+                newCart.push(product);
+            } else {
+                repeatedProducts++;
+            }
+        });
+        console.log(newCart);
+        cart[0]["product-list"] = newCart;
         res.status(200);
         res.send(cart);
     });
