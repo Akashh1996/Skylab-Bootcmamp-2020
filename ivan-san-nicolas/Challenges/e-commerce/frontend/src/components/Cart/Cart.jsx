a2d7a5217357421eb0af5a1c1d7950e82eac7de7
Ivan San <ivansan@MacBook-Pro-de-Ivan.local>
2020-11-15 18:32:14 +0100

    cart component created, product list working and final buy in process

--- ivan-san-nicolas/Challenges/e-commerce/frontend/src/components/Cart/Cart.jsx
+++ ivan-san-nicolas/Challenges/e-commerce/frontend/src/components/Cart/Cart.jsx
@@ -5,18 +5,61 @@ import requestCart from '../../redux/actions/cartActions';
 import './Cart.css';
 
 function Cart({ cart, dispatch }) {
-    if(!cart || !cart["product-list"]) {
+    if(!cart[0] || !cart[0]["product-list"]) {
         dispatch(requestCart());
     }
+
+    let totalPrice = 0;
+
+    let totalNames = [];
+
     return (
         <>
-            {cart[0] ? cart["product-list"] ? (
-                <p>{cart[0]["product-list"]["product-name"]}</p>
+        <div className="cart__products">
+            {cart[0] ? cart[0]["product-list"] ? (
+                cart[0]["product-list"].map((product) => {
+                    totalPrice += product["product-price"];
+                    totalNames.push(product["product-name"]);
+                    return (
+                        <div className="cart__product__row">
+                            <div className="cart__product__row__section product__image">
+                                <img src={product["product-image-url"]} 
+                                alt={product["product-name"] + "image"}
+                                id="cart__product__row__image"/>
+                            </div>
+                            <div className="cart__product__row__section product__name">
+                                <p>{product["product-name"]}</p>
+                            </div>
+                            <div className="cart__product__row__section product__color">
+                                <p>Color: {product["actual-color"]}</p>
+                            </div>
+                            <div className="cart__product__row__section product__price">
+                                <p>Price: {product["product-price"]}$</p>
+                            </div>
+                        </div>
+                    );
+                })
             ) : (
                 <p>Im sorry friend</p>
             ) : (
                 <p>Sooo sorry</p>
             )}
+        </div>
+        <div className="cart__final-buy">
+            <div className="cart_final-buy__totalNames">
+                <p>Products:</p>
+                {totalNames.length > 0 ? (
+                    totalNames.map((name) => {
+                    return <p>{name}</p>
+                    })
+                ) : (
+                    <p>There are no products</p>
+                )}
+            </div>
+            <div>
+                <p>Total Price: {totalPrice}$</p>
+            </div>
+        </div>
         </>
     );
 }
