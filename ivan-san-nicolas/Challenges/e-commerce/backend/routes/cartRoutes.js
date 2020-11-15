2ea19aa585a1207aa291b26403bc1259d59a4fca
Ivan San <ivansan@MacBook-Pro-de-Ivan.local>
2020-11-15 21:25:24 +0100

    add product to cart post petition added in cartRoutes and working

--- ivan-san-nicolas/Challenges/e-commerce/backend/routes/cartRoutes.js
+++ ivan-san-nicolas/Challenges/e-commerce/backend/routes/cartRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 let cart = require('../api/cart.json');
+let sabers = require('../api/sabers.json');
 
 const cartRouter = express.Router();
 
@@ -10,7 +11,18 @@ function routes() {
         res.send(cart);
     })
     .post((req, res) => {
-        const product = req.query.productName;
+        console.log(req);
+        const productName = req.query.productName;
+        const productColor = req.query.productColor;
+        let cartProduct = null;
+        sabers.map((product) => {
+            if(product["product-name"] === productName) {
+                let newProduct = product;
+                newProduct["actual-color"] = productColor;
+                cartProduct = newProduct;
+            }
+        });
+        cart[0]["product-list"].push(cartProduct);
         res.status(200);
         res.send(cart);
     })
