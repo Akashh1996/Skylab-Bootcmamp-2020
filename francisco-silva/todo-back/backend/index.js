cf565cad24cd4e7cd7a192289f2d2eeaaa15ea9b
francisco ximenes <fximenessilva@gmail.com>
2020-11-17 18:38:42 +0100

    asnjd

--- francisco-silva/todo-back/backend/index.js
+++ francisco-silva/todo-back/backend/index.js
@@ -1,24 +1,26 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const debug = require('debug')('app');
 const chalk = require('chalk');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const Todo = require('./src/models/todoModel');
-const heroRouter = require('./src/routes/todoRouter')(Todo);
+// const Todo = require('./src/models/todoModel');
+// const todoRouter = require('./src/routes/todoRouter')(Todo);
 
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 7777;
 
-mongoose.connect('mongodb://localhost/heroesdb');
+mongoose.connect('mongodb://localhost/tododb');
 
 app.use(morgan('tiny'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use('/heroes', heroRouter);
+// app.use('/todo', todoRouter);
 
 app.listen(port, () => {
   debug(`Server is running on port ${chalk.blue(port)}`);
---
+++ francisco-silva/todo-back/backend/src/controllers/todoController.js
@@ -0,0 +1,7 @@
+function todoController(Todo) {
+  function getMethod(req, res) {
+    res.json(req.hero);
+  }
+}
+
+module.exports = todoController;
