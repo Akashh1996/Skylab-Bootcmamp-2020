370213dbf6547f8acbdc2a95880aefe758ed33d9
Jorge <jorgengarriga@gmail.com>
2020-11-17 20:12:47 +0100

    Setup index.js redux

---
+++ jorge-neira/Challenge/todolist/root/client/src/redux/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import todosReducer from './todosReducer';
+
+const rootReducer = combineReducers(
+  {
+    todosReducer,
+  },
+);
+
+export default rootReducer;
