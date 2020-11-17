9a2d72486ae15820e4f532d0f0f8d2c122794f22
Jorge <jorgengarriga@gmail.com>
2020-11-17 18:03:27 +0100

    add model and configure index.js

--- jorge-neira/Challenge/todolist/root/server/index.js
+++ jorge-neira/Challenge/todolist/root/server/index.js
@@ -3,15 +3,20 @@ const debug = require('debug')('todoApp');
 const morgan = require('morgan');
 const chalk = require('chalk');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const cors = require('cors');
 
 const todoApp = express();
 const port = process.env.PORT || 5000;
 
+mongoose.connect('mongodb://localhost:27017/todoslistdb', { useNewUrlParser: true });
+
 todoApp.use(cors());
 todoApp.use(morgan('tiny'));
 todoApp.use(bodyParser.urlencoded({ extended: true }));
 todoApp.use(bodyParser.json());
+
+todoApp.use('/')
 
 todoApp.listen(port, () => {
   debug(`Server is running on port ${chalk.yellowBright(port)}`);
