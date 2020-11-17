a686c0b268c04abf197601a67bd907c2519d6a42
Marco-Reg <marcoregueiro@gmail.com>
2020-11-17 20:48:52 +0100

    getMethod tested, aun no he cenado y estoy de mala leche

--- Marco-reg/Challenge-Todo/backend/src/controllers/inputController.test.js
+++ Marco-reg/Challenge-Todo/backend/src/controllers/inputController.test.js
@@ -1,5 +1,30 @@
-function inputController(Input) {
-  function getMethod(req, res) {
+const { italic } = require('chalk');
+const Input = require('../models/inputModel');
+const inputController = require('./inputController')(Input);
 
-  }
-}
+describe('InputController', () => {
+  it('should call response send on getMethod whe find throws an error', () => {
+    const res = {
+      send: jest.fn(),
+    };
+
+    Input.find = jest.fn().mockImplementationOnce((query, callback) => {
+      callback(true, {});
+    });
+    inputController.getMethod({ input: null }, res);
+
+    expect(res.send).toHaveBeenCalled();
+  });
+  it('should call response send on getMethod whe find throws an error', () => {
+    const res = {
+      json: jest.fn(),
+    };
+
+    Input.find = jest.fn().mockImplementationOnce((query, callback) => {
+      callback(false, {});
+    });
+    inputController.getMethod({ input: null }, res);
+
+    expect(res.json).toHaveBeenCalled();
+  });
+});
