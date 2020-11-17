1f9cba801d22da94745ec2d7cedc1a18d7d4222a
francisco ximenes <fximenessilva@gmail.com>
2020-11-17 21:14:22 +0100

    djfnkm

--- francisco-silva/todo-back/backend/index.js
+++ francisco-silva/todo-back/backend/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const path = require('path');
+
 const cors = require('cors');
 const debug = require('debug')('app');
 const chalk = require('chalk');
---
+++ francisco-silva/todo-back/backend/src/controllers/todoController.test.js
@@ -0,0 +1,79 @@
+/* eslint-disable no-undef */
+const Todo = require('../models/todoModel');
+const todoController = require('./todoController')(Todo);
+
+describe('deleteMethod', () => {
+  test('should call send when there is an error', () => {
+    const res = { send: jest.fn() };
+    Todo.findByIdAndRemove = jest.fn().mockImplementationOnce((query, callback) => {
+      callback(true);
+    });
+    todoController.deleteMethod({}, res);
+    expect(res.send).toHaveBeenCalled();
+  });
+});
+
+describe('getMethod', () => {
+  test('should call send when there is an error', () => {
+    const res = { send: jest.fn() };
+
+    Product.find = jest.fn().mockImplementationOnce((query, callback) => {
+      callback(true, {});
+    });
+
+    productController.getMethod({}, res);
+    expect(res.send).toHaveBeenCalled();
+  });
+  test('should call json when there is not an error', () => {
+    const res = { json: jest.fn() };
+
+    Product.find = jest.fn().mockImplementationOnce((query, callback) => {
+      callback(false, {});
+    });
+    productController.getMethod({}, res);
+    expect(res.json).toHaveBeenCalled();
+  });
+});
+
+describe('deleteMethod', () => {
+  test('should call send when there is an error', () => {
+    const res = { send: jest.fn() };
+    const req = {
+      params: {
+        productId: 2,
+      },
+    };
+    Product.deleteOne = jest.fn().mockImplementationOnce((query, callback) => {
+      callback(true, {});
+    });
+    productController.deleteMethod(req, res);
+    expect(res.send).toHaveBeenCalled();
+  });
+  test('should call send when there is not an errror', () => {
+    const res = { send: jest.fn() };
+    const req = {
+      params: {
+        productId: 2,
+      },
+    };
+    Product.deleteOne = jest.fn().mockImplementationOnce((query, callback) => {
+      callback(false, {});
+    });
+    productController.deleteMethod(req, res);
+    expect(res.send).toHaveBeenCalled();
+  });
+});
+
+describe('allMiddleware', () => {
+  test('should call next', () => {
+    const req = {
+      params: {
+        productId: 3,
+      },
+    };
+    const res = { send: jest.fn() };
+    const next = jest.fn();
+    productController.allMiddleware(req, res, next);
+    expect(next).toHaveBeenCalled();
+  });
+});
