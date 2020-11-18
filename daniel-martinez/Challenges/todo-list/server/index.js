f322dfad442fa9b8c45b8d02d5b9c06386c08990
DanielMartinezLozano <dmartinez1604@gmail.com>
2020-11-18 13:04:25 +0100

    refactored server/index.js

--- daniel-martinez/Challenges/todo-list/server/index.js
+++ daniel-martinez/Challenges/todo-list/server/index.js
@@ -3,14 +3,15 @@ const debug = require('debug')('app');
 const morgan = require('morgan');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const { connect } = require('mongoose');
 const TvShows = require('./src/models/tvShowModel');
 const routes = require('./src/routes/routes')(TvShows);
 
 const app = express();
 const port = process.env.PORT || 5500;
 
-mongoose.connect('mongodb://localhost/todolist', { useNewUrlParser: true, useUnifiedTopology: true });
+const mongoURL = process.env.URL || 'mongodb://localhost/todolist';
+connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(morgan('tiny'));
 app.use(cors());
