cf350bde5d6ab009bd722ba5e46a8cd92bd48ad3
Jorge <jorgengarriga@gmail.com>
2020-11-18 20:55:53 +0100

    Change cart model schema properties

--- jorge-neira/Challenge/FullStack-Ecommerce/root/server/src/models/cartModel.js
+++ jorge-neira/Challenge/FullStack-Ecommerce/root/server/src/models/cartModel.js
@@ -3,9 +3,7 @@ const mongoose = require('mongoose');
 const { Schema, model } = mongoose;
 
 const cartShema = new Schema({
-  cartId: { type: Number },
-  productName: { type: String },
-  productModel: { type: Object },
+  quantity: { type: Number },
   price: { type: Number },
 });
 
