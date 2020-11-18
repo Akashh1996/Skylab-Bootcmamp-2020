dad53eafa0b70f98ed071985cdce82744ae758b3
Jorge <jorgengarriga@gmail.com>
2020-11-18 07:30:52 +0100

    test implementation in progress

--- jorge-neira/Challenge/todolist/root/client/src/redux/__test__/todosActions.test.js
+++ jorge-neira/Challenge/todolist/root/client/src/redux/__test__/todosActions.test.js
@@ -1,63 +1,36 @@
 import axios from 'axios';
-import fetchMock from 'fetch-mock';
-import { apiMiddleware, ApiError } from 'redux-api-middleware';
 import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import fetchMock from 'fetch-mock';
+import * as actions from '../actions/todosActions';
 import actionTypes from '../actions/actionTypes';
-import * as todosActions from '../actions/todosActions';
-import * as actions from '../reducers';
 
-jest.mock('axios');
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 
-// const URL = 'http://localhost:5000/todos';
-
-const createStore = configureMockStore([apiMiddleware]);
-const store = createStore(actions.initialState);
-
-describe('todos Actions', () => {
+describe('async actions', () => {
+  let testData;
+  beforeEach(() => {
+    testData = { todoId: 1, todoDescription: 'String' };
+  });
   afterEach(() => {
-    fetchMock.reset();
     fetchMock.restore();
   });
 
-  test('loadTodos', async () => {
-    // arrange
-    const response = {
-      todoId: 1,
-      todoDescription: 'string',
-    };
+  test('creates FETCH_TODOS_SUCCESS when fetching todos has been done', async () => {
+    fetchMock.getOnce('/todos', {
+      body: { todos: ['do something'] },
+      headers: { 'content-type': 'application/json' },
+    });
 
-    fetchMock.getOnce('/todos',
-      { body: { results: response } });
-
-    const expectedActions = { type: actionTypes.LOAD_TODOS, todos: response };
-    axios.get.mockImplementationOnce(() => Promise.resolve(response));
-    expect(store.getActions()).toEqual(expectedActions);
+    const expectedActions = [
+      { type: actionTypes.LOAD_TODOS, body: { todos: ['do something'] } },
+    ];
+    const store = mockStore({ todos: [] });
+    axios.get.mockImplementationOnce(() => Promise.resolve('done'));
+    await store.dispatch(actions.loadTodos()).then(() => {
+      // return of async actions
+      expect(store.getActions()).toEqual(expectedActions);
+    });
   });
-
-  // describe('loadTodo', () => {
-  //   beforeEach(() => {
-  //     mockObj = { data: { todoId: 1, todoDescription: 'String' } };
-  //   });
-  //   test('Should call axios GET method', async () => {
-  //     axios.get.mockImplementationOnce(() => Promise.resolve(mockObj));
-  //     await store.dispatch(todosActions.loadTodos());
-  //     expect(axios.get).toHaveBeenCalledWith(URL);
-  //   });
-  //   test('Should call axios GET method and return happy path ', async () => {
-  //     axios.post.mockImplementationOnce(() => Promise.resolve(mockObj));
-  //     await store.dispatch(todosActions.loadTodos());
-  //     expect(store.dispatch(todosActions.loadTodosSuccess(mockObj))).toEqual({
-  //       type: 'LOAD_TODOS',
-  //       todos: mockObj,
-  //     });
-  //   });
-  //   test('Should call axios GET method and return happy path ', async () => {
-  //     axios.post.mockImplementationOnce(() => Promise.reject(Error));
-  //     await store.dispatch(todosActions.loadTodos());
-  //     expect(store.dispatch(errorRequest())).toEqual({
-  //       type: actionTypes.ERROR_REQUEST,
-  //       error: 'error',
-  //     });
-  //   });
-  // });
 });
