4eb3e397f9f6525e2f766922cd5ee0ce5190849f
Alma-Co <65400483+Alma-Co@users.noreply.github.com>
2020-11-18 11:58:47 +0100

    Update toDoListRouter.js

--- edith-cantegrit/challenge/toDoList/backend/src/routes/toDoListRouter.js
+++ edith-cantegrit/challenge/toDoList/backend/src/routes/toDoListRouter.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const { Router } = require('express');
 const toDoListController = require('../controllers/toDoListController');
 const itemDeleteController = require('../controllers/itemDeleteController');
 
 function toDoListRouter(Items) {
-    const router =  express.Router();
+    const router =  Router();
     const todolist = toDoListController(Items);
     const itemdelete = itemDeleteController(Items)
 
