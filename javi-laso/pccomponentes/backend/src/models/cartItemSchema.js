91266dd647650097048c3df0f0050d953ec468e7
Jheavi <javierlaso213@hotmail.com>
2020-11-18 22:44:11 +0100

    deleted previous cartSchema and added rest of properties to itemSchema

--- javi-laso/pccomponentes/backend/src/models/cartItemSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const { Schema, model } = mongoose;
-
-const cartItemSchema = new Schema({
-  cartList: { type: Object },
-});
-
-module.exports = model('carts', cartItemSchema);
--- javi-laso/pccomponentes/backend/src/models/itemSchema.js
+++ javi-laso/pccomponentes/backend/src/models/itemSchema.js
@@ -3,13 +3,16 @@ const mongoose = require('mongoose');
 const { Schema, model } = mongoose;
 
 const itemSchema = new Schema({
-  id: { type: String },
-  manufacturer: { type: String },
-  price: { type: String },
-  'product-name': { type: String },
-  'product-type': { type: String },
-  'product-url': { type: String },
-  'product-image': { type: String },
+  id: String,
+  manufacturer: String,
+  price: String,
+  'product-name': String,
+  'product-type': String,
+  'product-url': String,
+  'product-image': String,
+  inches: String,
+  mhz: String,
+  'capacity-gb': String,
 });
 
-module.exports = model('items', itemSchema);
+module.exports = model('Item', itemSchema);
