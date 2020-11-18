ad0f8ca0a12447ce7d7c042e87ab767b4f99c15a
Jorge <jorgengarriga@gmail.com>
2020-11-18 21:01:22 +0100

    Change Server Name / Destructuring of mongoose connect / Add variable for mongodb URL /

--- jorge-neira/Challenge/FullStack-Ecommerce/root/server/index.js
+++ jorge-neira/Challenge/FullStack-Ecommerce/root/server/index.js
@@ -4,26 +4,27 @@ const morgan = require('morgan');
 const chalk = require('chalk');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const mongoose = require('mongoose');
+const { connect } = require('mongoose');
 const Products = require('./src/models/laptopModel');
 const Carts = require('./src/models/cartModel');
 const productsListRouter = require('./src/routes/marketRouter')(Products);
 const productsDetailRouter = require('./src/routes/marketRouter')(Products);
 const shoppingCart = require('./src/routes/cartRouter')(Carts);
 
-const asusApp = express();
+const ecommerceApp = express();
 const port = process.env.PORT || 5000;
-asusApp.use(morgan('tiny'));
+const dbURL = process.env.DBURL || 'mongodb://localhost/marketdb';
+ecommerceApp.use(morgan('tiny'));
 
-mongoose.connect('mongodb://localhost:27017/marketdb', { useNewUrlParser: true });
+connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true });
 
-asusApp.use(cors());
-asusApp.use(bodyParser.urlencoded({ extended: true }));
-asusApp.use(bodyParser.json());
-asusApp.use('/', productsListRouter);
-asusApp.use('/', productsDetailRouter);
-asusApp.use('/', shoppingCart);
+ecommerceApp.use(cors());
+ecommerceApp.use(bodyParser.urlencoded({ extended: true }));
+ecommerceApp.use(bodyParser.json());
+ecommerceApp.use('/', productsListRouter);
+ecommerceApp.use('/', productsDetailRouter);
+ecommerceApp.use('/', shoppingCart);
 
-asusApp.listen(port, () => {
+ecommerceApp.listen(port, () => {
   debug(`Server is running on port ${chalk.yellowBright(port)}`);
 });
