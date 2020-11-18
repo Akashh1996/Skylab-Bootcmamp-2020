ca37122578549f871cd03abd3010b66ffa58df49
Ivan Dobry <ivandobry@hotmail.es>
2020-11-18 12:33:42 +0100

    remove logs, comments and resolve suggestions

--- ivan-dobry/Lectures/challenges/ToDoList/server/index.js
+++ ivan-dobry/Lectures/challenges/ToDoList/server/index.js
@@ -2,14 +2,21 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const { connect } = require('mongoose');
 const items = require('./models/listModel');
 const listRoutes = require('./routes/listRoutes')(items);
 
 const app = express();
 const port = process.env.PORT || 1978;
 
-mongoose.connect('mongodb://localhost/listdb');
+const dataBase = process.env.LISTDB || 'mongodb://localhost/listdb';
+connect(dataBase, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+connect(dataBase);
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
