6a4f8916d61ee5be80805b1cb74e199076984700
Jorge <jorgengarriga@gmail.com>
2020-11-18 13:02:52 +0100

    Fix dependencies location

--- jorge-neira/Challenge/todolist/root/client/src/index.jsx
+++ jorge-neira/Challenge/todolist/root/client/src/index.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import { Provider } from 'react-redux';
 import configureStore from './redux/configureStore';
-import App from './App';
+import TodoListComponent from './components/TodoListComponent';
 
 const store = configureStore();
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <TodoListComponent />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
