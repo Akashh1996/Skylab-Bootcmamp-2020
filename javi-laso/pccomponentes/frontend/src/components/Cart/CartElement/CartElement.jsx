e8b20433ecd5e74a499706f4f4c93e9eb0746003
Jheavi <javierlaso213@hotmail.com>
2020-11-18 22:50:09 +0100

    fixed CartElement component to render the cart information

--- javi-laso/pccomponentes/frontend/src/components/Cart/CartElement/CartElement.jsx
+++ javi-laso/pccomponentes/frontend/src/components/Cart/CartElement/CartElement.jsx
@@ -6,27 +6,27 @@ import { connect } from 'react-redux';
 import './cartElement.css';
 import { putItemInCart, deleteItemFromCart } from '../../../redux/actions/actions';
 
-function CartElement({ arrayOfItems, actions }) {
+function CartElement({ product, actions }) {
   return (
     <div className="d-flex cart-element">
-      <Link to={`/${arrayOfItems[0].id}`}>
-        <img src={arrayOfItems[0]['product-image']} alt={arrayOfItems[0]['product-name']} className="cart-element__image" />
+      <Link to={`/${product.product.id}`}>
+        <img src={product.product['product-image']} alt={product.product['product-name']} className="cart-element__image" />
       </Link>
       <div className="d-flex flex-column">
-        <Link to={`/${arrayOfItems[0].id}`}>
-          <span className="cart-element__title">{`${arrayOfItems[0].manufacturer} ${arrayOfItems[0]['product-name']}`}</span>
+        <Link to={`/${product.product.id}`}>
+          <span className="cart-element__title">{`${product.product.manufacturer} ${product.product['product-name']}`}</span>
         </Link>
-        <span className="cart-element__type">{`${arrayOfItems[0]['product-type']}`}</span>
+        <span className="cart-element__type">{`${product.product['product-type']}`}</span>
         <div className="flex-1" />
-        <span>{`${arrayOfItems[0].price}€`}</span>
+        <span>{`${product.product.price}€`}</span>
         <div className="flex-1" />
         <div className="d-flex">
-          <button type="button" className="quantity-square square-button" id="delete-btn" onClick={() => { actions.deleteItemFromCart(arrayOfItems[0]); }}>-</button>
-          <span className="quantity-square">{`${arrayOfItems.length}`}</span>
-          <button type="button" className="quantity-square square-button" id="add-btn" onClick={() => { actions.putItemInCart(arrayOfItems[0]); }}>+</button>
+          <button type="button" className="quantity-square square-button" id="delete-btn" onClick={() => { actions.deleteItemFromCart(product.product); }}>-</button>
+          <span className="quantity-square">{`${product.quantity}`}</span>
+          <button type="button" className="quantity-square square-button" id="add-btn" onClick={() => { actions.putItemInCart(product.product); }}>+</button>
         </div>
         <div className="flex-1" />
-        <span>{`Total: ${(arrayOfItems[0].price * arrayOfItems.length).toFixed(2)}€`}</span>
+        <span>{`Total: ${(product.product.price * product.quantity).toFixed(2)}€`}</span>
         <div className="flex-1" />
       </div>
     </div>
@@ -34,14 +34,17 @@ function CartElement({ arrayOfItems, actions }) {
 }
 
 CartElement.propTypes = {
-  arrayOfItems: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.string,
-    'product-name': PropTypes.string,
-    'product-type': PropTypes.string,
-    'product-image': PropTypes.string,
-    manufacturer: PropTypes.string,
-    price: PropTypes.string,
-  })).isRequired,
+  product: PropTypes.shape({
+    product: PropTypes.shape({
+      id: PropTypes.string,
+      'product-name': PropTypes.string,
+      'product-type': PropTypes.string,
+      'product-image': PropTypes.string,
+      manufacturer: PropTypes.string,
+      price: PropTypes.string,
+    }).isRequired,
+    quantity: PropTypes.number.isRequired,
+  }).isRequired,
   actions: PropTypes.shape({
     putItemInCart: PropTypes.func.isRequired,
     deleteItemFromCart: PropTypes.func.isRequired,
