30e3899900cd51583b5f31a1b9e38d67631c47ef
Alma-Co <65400483+Alma-Co@users.noreply.github.com>
2020-11-18 11:46:34 +0100

    Update itemDeleteController.js

--- edith-cantegrit/challenge/toDoList/backend/src/controllers/itemDeleteController.js
+++ edith-cantegrit/challenge/toDoList/backend/src/controllers/itemDeleteController.js
@@ -1,8 +1,8 @@
 function itemDeleteController(Items) {
     
     function deleteMethod(req, res) {
-        const query = req.params.id
-        Items.findByIdAndRemove(query, (errorFindItems) => {
+        const { id } = req.params.id
+        Items.findByIdAndRemove(id, (errorFindItems) => {
             if(errorFindItems) {
                 return res.send(errorFindItems)
             }
