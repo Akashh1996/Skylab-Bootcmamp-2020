e0f086341bad51cbbabc3b4eb6597d385c61c0fc
Jorge <jorgengarriga@gmail.com>
2020-11-19 18:25:16 +0100

    Add basic eslint and package.json

--- jorge-neira/Challenge/mongoose-population/root/server/index.js
+++ jorge-neira/Challenge/mongoose-population/root/server/index.js
@@ -6,16 +6,29 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const { connect } = require('mongoose');
 
+const User = require('./src/models/usersModel');
+const Address = require('./src/models/addressModel');
+const Country = require('./src/models/countriesModel');
+
+const usersRouter = require('./src/routes/usersRouter')(User, Address, Country);
+const userRouter = require('./src/routes/userRouter')(User);
+const addressRouter = require('./src/routes/addressRouter')(Address);
+const countryRouter = require('./src/routes/countryRouter')(Country);
+
 const populationApp = express();
 const port = process.env.PORT || 5000;
 const dbURL = process.env.DBURL || 'mongodb://localhost/populationDB';
-populationApp.use(morgan('tiny'));
+connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true });
 
 populationApp.use(cors());
 populationApp.use(morgan('tiny'));
 populationApp.use(bodyParser.urlencoded({ extended: true }));
 populationApp.use(bodyParser.json());
-connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true });
+
+populationApp.use('/', usersRouter);
+populationApp.use('/user', userRouter);
+populationApp.use('/address', addressRouter);
+populationApp.use('/country', countryRouter);
 
 populationApp.listen(port, () => {
   debug(`Server is running on port ${chalk.yellowBright(port)}`);
