85d1a237e2da32888f11d221e759b4069670ee87
Jheavi <javierlaso213@hotmail.com>
2020-11-19 09:56:51 +0100

    created initial server configuration

---
+++ javi-laso/mongoose-population/server/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const chalk = require('chalk');
+const debug = require('debug')('server');
+const { connect } = require('mongoose');
+const userSchema = require('./src/models/userSchema');
+const userRouter = require('./src/routers/userRouter')(userSchema);
+
+const server = express();
+const port = process.env.PORT || 3500;
+const dbUrl = process.env.DBURL || 'mongodb://localhost/mongoosepopulatedb';
+
+connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+
+server.use(morgan('dev'));
+server.use(cors());
+
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+
+server.use('/', userRouter);
+
+server.listen(port, () => {
+  debug(`Server listening on port ${chalk.blueBright(port)}...`);
+});
