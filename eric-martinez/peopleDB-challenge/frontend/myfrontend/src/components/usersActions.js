a67b2cf39466b0ffc8293d27e3a30ff188d80163
Eric Martinez <martinezjimenezeric@gmail.com>
2020-11-19 17:06:12 +0100

    falsa alarma

---
+++ eric-martinez/peopleDB-challenge/frontend/myfrontend/src/components/usersActions.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import actionTypes from './actionsTypes';
+
+const dataBaseUrl = 'http://localhost:5000/';
+
+function requestUserSuccess(users) {
+    return {
+        type: actionTypes.LOAD_USERS,
+        users,
+    }
+}
+
+function requestUserError(error) {
+    return {
+        type: actionTypes.LOAD_USERS_ERROR,
+        error,
+    }
+}
+
+export function loadUsers() {
+    return async (dispatch) => {
+        try {
+            const users = await axios.get(dataBaseUrl);
+            dispatch(requestUserSuccess(users.data));
+        } catch (error) {
+            dispatch(requestUserError(error));
+        }
+    };
+};
---
+++ eric-martinez/peopleDB-challenge/frontend/myfrontend/src/redux/reducers/usersReducer.js
@@ -0,0 +1,12 @@
+import actionTypes from '../actions/';
+
+export default function usersReducers(state = {}, action){
+    switch (action.type) {
+        case actionTypes.L:
+            
+            break;
+    
+        default:
+            break;
+    }
+}
