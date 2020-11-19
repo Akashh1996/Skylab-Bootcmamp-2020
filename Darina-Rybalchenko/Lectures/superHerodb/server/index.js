83653b821b791a2cde79e661c89651aa0c4e6441
Darina Rybalchenko <dari.ryb@gmail.com>
2020-11-19 09:10:51 +0100

    add superherodb

---
+++ Darina-Rybalchenko/Lectures/superHerodb/server/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const debug = require('debug')('app');
+const chalk = require('chalk');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Hero = require('./src/models/superHeroModel');
+const Cart = require('./src/models/cartsModel')
+const heroRouter = require('./src/routes/heroRouter')(Hero, Cart);
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+mongoose.connect('mongodb://localhost/superherodb');
+
+app.use(morgan('tiny'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, '/public/')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src/views', 'index.html'));
+});
+
+app.use('/heroes', heroRouter);
+
+app.listen(port, () => {
+  debug(`Server is running on port ${chalk.blue(port)}`);
+});
---
+++ Darina-Rybalchenko/Lectures/superHerodb/server/src/models/cartsModel.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const { Schema, model } = mongoose;
+
+const powerStatesSchema = new Schema({
+  intelligence: { type: Number },
+  strength: { type: Number },
+  speed: { type: Number },
+  durability: { type: Number },
+  power: { type: Number },
+  combat: { type: Number },
+
+});
+
+const appearanceSchema = new Schema({
+  gender: { type: String },
+  race: { type: String },
+  height: [String],
+  weight: [String],
+  eyeColor: { type: String },
+  hairColor: { type: String },
+});
+
+const cartsSchema = new Schema({
+  id: { type: Number },
+  name: { type: String },
+  slug: { type: String },
+  powerstats: {
+    type: powerStatesSchema,
+    default: {},
+  },
+  appearance: {
+    type: appearanceSchema,
+    default: {},
+
+  },
+});
+
+module.exports = model('carts', cartsSchema);
