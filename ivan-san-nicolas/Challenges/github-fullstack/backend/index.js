3450efed819f977b089b3bea2ec0815ddc15ce27
Ivan San <ivansan@MacBook-Pro-de-Ivan.local>
2020-11-21 00:49:03 +0100

    debug changed for console.log

--- ivan-san-nicolas/Challenges/github-fullstack/backend/index.js
+++ ivan-san-nicolas/Challenges/github-fullstack/backend/index.js
@@ -3,14 +3,13 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const { connect } = require('mongoose');
 const Users = require('./models/userModel');
-const { debug } = require('console');
 const userRouter = require('./routes/userRouter')(Users);
 
 const app = express();
 const port = process.env.PORT || 1240;
 const DataBaseURL = process.env.DBURL || 'mongodb://localhost/githubdb';
 
-connect(DataBaseURL, { useNewUrlParser: true, useUnifiedTopology: true});
+connect(DataBaseURL, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,5 +18,5 @@ app.use(bodyParser.json());
 app.use('/users', userRouter);
 
 app.listen(port, () => {
-    debug(`Server working at port ${port}`);
-})
+  console.log(`Server working at port ${port}`);
+});
