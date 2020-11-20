eab9501a73ad4c2d9bbe85b6ffb226823bf7aae7
Jheavi <javierlaso213@hotmail.com>
2020-11-20 19:45:17 +0100

    added bootstrap and scss to form component

--- javi-laso/mongoose-population/frontend-app/src/components/CreateUser/CreateUser.jsx
+++ javi-laso/mongoose-population/frontend-app/src/components/CreateUser/CreateUser.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { PropTypes } from 'prop-types';
 import { createUser, loadUsers } from '../../redux/actions/actions';
+import './createUser.css';
 
 function CreateUser({ userList, dispatch }) {
   const [name, setName] = useState('');
@@ -29,40 +30,50 @@ function CreateUser({ userList, dispatch }) {
     if (!userList || userList.length === 0) {
       dispatch(loadUsers());
     }
-  }, []);
+  }, [name]);
   return (
-    <div className="d-flex flex-column ">
-      <label htmlFor="name">
-        Name
-        <input type="text" id="name" value={name} onChange={(event) => { setName(event.target.value); }} />
-      </label>
-      <label htmlFor="age">
-        Age
-        <input type="text" id="age" value={age} onChange={(event) => { setAge(event.target.value); }} />
-      </label>
+    <form className="d-flex flex-column new-user-form">
+      <div className="form-row">
+        <label htmlFor="name" className="col">
+          Name:
+          <input type="text" id="name" className="form-control" value={name} onChange={(event) => { setName(event.target.value); }} placeholder="Enter your name" />
+        </label>
+        <label htmlFor="age" className="col">
+          Age:
+          {' '}
+          <input type="text" id="age" className="form-control" value={age} onChange={(event) => { setAge(event.target.value); }} placeholder="Enter your age" />
+        </label>
+      </div>
       <label htmlFor="Street">
-        Street
-        <input type="text" id="Street" value={street} onChange={(event) => { setStreet(event.target.value); }} />
+        Street:
+        {' '}
+        <input type="text" id="Street" className="form-control" value={street} onChange={(event) => { setStreet(event.target.value); }} placeholder="Enter your street name" />
       </label>
       <label htmlFor="Number">
-        Number
-        <input type="text" id="Number" value={number} onChange={(event) => { setNumber(event.target.value); }} />
+        Number:
+        {' '}
+        <input type="text" id="Number" className="form-control" value={number} onChange={(event) => { setNumber(event.target.value); }} placeholder="Enter your street number" />
       </label>
-      <label htmlFor="city">
-        City
-        <input type="text" id="city" value={city} onChange={(event) => { setCity(event.target.value); }} />
-      </label>
-      <label htmlFor="code">
-        Country code
-        <input type="text" id="code" value={code} onChange={(event) => { setCode(event.target.value); }} />
-      </label>
-      <label htmlFor="country-name">
-        Country name
-        <input type="text" id="country-name" value={countryName} onChange={(event) => { setCountryName(event.target.value); }} />
-      </label>
+      <div className="form-row">
+        <label htmlFor="city" className="form-group col-4">
+          City:
+          {' '}
+          <input type="text" id="city" className="form-control" value={city} onChange={(event) => { setCity(event.target.value); }} placeholder="Enter your city" />
+        </label>
+        <label htmlFor="country-name" className="form-group col-4">
+          Country name:
+          {' '}
+          <input type="text" id="country-name" className="form-control" value={countryName} onChange={(event) => { setCountryName(event.target.value); }} placeholder="Enter your country" />
+        </label>
+        <label htmlFor="code" className="form-group col-2">
+          Country code:
+          {' '}
+          <input type="text" id="code" className="form-control" value={code} onChange={(event) => { setCode(event.target.value); }} placeholder="Code" />
+        </label>
+      </div>
       <button
         type="submit"
-        className="btn btn-primary"
+        className="btn btn-primary submit-btn"
         onClick={() => { dispatch(createUser(newUser)); }}
       >
         Submit
@@ -76,7 +87,7 @@ function CreateUser({ userList, dispatch }) {
           </li>
         ))}
       </ul>
-    </div>
+    </form>
   );
 }
 
