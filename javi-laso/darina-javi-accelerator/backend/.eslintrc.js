d4bbafab764c9af6c9669cfeea93175f465d8c1a
Jheavi <javierlaso213@hotmail.com>
2020-11-20 19:58:54 +0100

    created frontend react app

---
+++ javi-laso/darina-javi-accelerator/backend/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    'linebreak-style': 0,
+    'global-require': 0,
+    'eslint linebreak-style': [0, 'error', 'windows'],
+    'no-underscore-dangle': [0],
+  },
+};
