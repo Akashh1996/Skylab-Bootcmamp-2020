bef79aa766a0f28e91630bd5a06688f9223254c3
Marco Regueiro Fernández <marco-reg@MacBook-Pro-de-Marco.local>
2020-11-21 11:27:43 +0100

    created reactApp

--- Marco-reg/github-challenge-bier/backend/index.js
+++ Marco-reg/github-challenge-bier/backend/index.js
@@ -7,10 +7,10 @@ const listRoutes = require('./routes/listRoutes');
 const app = express();
 app.use(cors());
 
-const port = process.env.PORT || 1220;
+const port = process.env.PORT || 3020;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/', listRoutes);
 app.listen(port, () => {
-  debug(`server is running on port ${port}`);
+  console.log(`server is running on port ${port}`);
 });
