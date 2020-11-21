ffa46d63594ddf6adc77ae0103ef63248a7aab89
algmebe1 <67826380+algmebe1@users.noreply.github.com>
2020-11-21 01:49:21 +0100

    Delete actionTypes.js

--- Darina-Rybalchenko/Mongoose-population/frontend/src/redux/actions/actionTypes.js
+++
@@ -1,8 +0,0 @@
-const actionTypes = {
-  LOAD_USER: 'LOAD_USER',
-  LOAD_USER_ERROR: 'LOAD_USER_ERROR ',
-  CREATE_USER: 'CREATE_USER',
-  CREATE_USER_ERROR: 'CREATE_USER_ERROR',
-};
-
-export default actionTypes;
