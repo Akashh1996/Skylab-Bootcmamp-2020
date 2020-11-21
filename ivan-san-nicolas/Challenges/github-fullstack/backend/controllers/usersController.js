c6c4bd857c16ac2d86a87b6ab0793e5af7ed6677
Ivan San <ivohan8@gmail.com>
2020-11-21 12:20:31 +0100

    usersController methods for just one user moved into userController

--- ivan-san-nicolas/Challenges/github-fullstack/backend/controllers/usersController.js
+++ ivan-san-nicolas/Challenges/github-fullstack/backend/controllers/usersController.js
@@ -8,40 +8,8 @@ function usersController(Users) {
     });
   }
 
-  function postMethod(req, res) {
-    const { name, profilePic, githubUrl } = req.body;
-    const newUser = { name, profilePic, githubUrl };
-
-    Users.create(newUser, (errorCreatingUser, users) => {
-      errorCreatingUser ? res.send(errorCreatingUser) : res.json(users);
-    });
-  }
-
-  function patchMethod(req, res) {
-    const { userId, updatedUser } = req.body;
-    const { name, profilePic, githubUrl } = updatedUser;
-    const query = { _id: userId };
-    const conditionToUpdate = { name, profilePic, githubUrl };
-
-    Users.findOneAndUpdate(query, conditionToUpdate, (errorUpdatingUser, user) => {
-      errorUpdatingUser ? res.send(errorUpdatingUser) : res.json(user);
-    });
-  }
-
-  function deleteMethod(req, res) {
-    const { userId } = req.body;
-    const query = { _id: userId };
-
-    Users.deleteOne(query, (errorDeletingUser, user) => {
-      errorDeletingUser ? res.send(errorDeletingUser) : res.json(user);
-    });
-  }
-
   return {
     getMethod,
-    postMethod,
-    patchMethod,
-    deleteMethod,
   };
 }
 
