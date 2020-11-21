0587e87e84eb71c3d2518c312e3b805691df1417
Ivan San <ivohan8@gmail.com>
2020-11-21 17:48:44 +0100

    gitReducer error cases merged into one case

--- ivan-san-nicolas/Challenges/github-fullstack/frontend/src/redux/reducers/gitReducer.js
+++ ivan-san-nicolas/Challenges/github-fullstack/frontend/src/redux/reducers/gitReducer.js
@@ -6,14 +6,11 @@ export default function gitReducer(state = {}, action) {
     case actionTypes.LOAD_PROJECTS:
       newState = { ...state, projectArray: action.projectList };
       break;
-    case actionTypes.LOAD_PROJECTS_ERROR:
+    case actionTypes.LOAD_ERROR:
       newState = { ...state, error: action.error };
       break;
     case actionTypes.LOAD_USERS:
       newState = { ...state, userArray: action.userList };
-      break;
-    case actionTypes.LOAD_USERS_ERROR:
-      newState = { ...state, error: action.error };
       break;
     default:
       newState = { ...state };
