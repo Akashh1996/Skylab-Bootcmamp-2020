5c06a36d6e2b8b415107162df33ffe3ea8487108
algmebe1 <67826380+algmebe1@users.noreply.github.com>
2020-11-21 02:02:41 +0100

    Delete countryModel.js

--- david-mato/populate/server/src/models/countryModel.js
+++
@@ -1,8 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-const countrySchema = new Schema({
-  code: { type: String },
-  name: { type: String },
-});
-
-module.exports = model('Country', countrySchema);
