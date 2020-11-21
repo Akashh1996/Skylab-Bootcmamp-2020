d65651ddf624a0251aecac1982e011bd300376d2
algmebe1 <67826380+algmebe1@users.noreply.github.com>
2020-11-21 01:54:21 +0100

    Delete countryModel.js

--- Darina-Rybalchenko/Mongoose-population/server/src/models/countryModel.js
+++
@@ -1,8 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-const countrySchema = new Schema({
-  code: { type: String },
-  countryName: { type: String },
-});
-
-module.exports = model('countries', countrySchema);
