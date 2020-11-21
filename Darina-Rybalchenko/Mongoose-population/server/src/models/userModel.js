00b7690c30a7578bd5cf82143e58c21ccb2ba419
algmebe1 <67826380+algmebe1@users.noreply.github.com>
2020-11-21 01:54:33 +0100

    Delete userModel.js

--- Darina-Rybalchenko/Mongoose-population/server/src/models/userModel.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-const userSchema = new Schema({
-  name: { type: String },
-  age: { type: Number },
-  address: { type: Schema.Types.ObjectId, ref: 'addresses' },
-});
-
-module.exports = model('users', userSchema);
