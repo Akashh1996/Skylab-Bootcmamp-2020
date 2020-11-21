b3d4365123ec56b1f9f3cf977694595623897676
Ivan San <ivohan8@gmail.com>
2020-11-21 12:19:35 +0100

    userController created

---
+++ ivan-san-nicolas/Challenges/github-fullstack/backend/controllers/userController.js
@@ -0,0 +1,49 @@
+/* eslint-disable no-unused-expressions */
+function userController(Users) {
+  function getMethod(req, res) {
+    const { userId } = req.body;
+    const query = { _id: userId };
+
+    Users.find(query, (errorFindingUsers, users) => {
+      errorFindingUsers ? res.send(errorFindingUsers) : res.json(users);
+    });
+  }
+
+  function postMethod(req, res) {
+    const { name, profilePic, githubUrl } = req.body;
+    const newUser = { name, profilePic, githubUrl };
+
+    Users.create(newUser, (errorCreatingUser, users) => {
+      errorCreatingUser ? res.send(errorCreatingUser) : res.json(users);
+    });
+  }
+
+  function patchMethod(req, res) {
+    const { userId, updatedUser } = req.body;
+    const { name, profilePic, githubUrl } = updatedUser;
+    const query = { _id: userId };
+    const conditionToUpdate = { name, profilePic, githubUrl };
+
+    Users.findOneAndUpdate(query, conditionToUpdate, (errorUpdatingUser, user) => {
+      errorUpdatingUser ? res.send(errorUpdatingUser) : res.json(user);
+    });
+  }
+
+  function deleteMethod(req, res) {
+    const { userId } = req.body;
+    const query = { _id: userId };
+
+    Users.deleteOne(query, (errorDeletingUser, user) => {
+      errorDeletingUser ? res.send(errorDeletingUser) : res.json(user);
+    });
+  }
+
+  return {
+    getMethod,
+    postMethod,
+    patchMethod,
+    deleteMethod,
+  };
+}
+
+module.exports = userController;
