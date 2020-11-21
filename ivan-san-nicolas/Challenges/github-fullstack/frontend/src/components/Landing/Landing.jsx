f798c16f6310e889efd9a4eb6faf8d312924839e
Ivan San <ivohan8@gmail.com>
2020-11-21 20:21:54 +0100

    print test created in Landing component [working]

--- ivan-san-nicolas/Challenges/github-fullstack/frontend/src/components/Landing/Landing.jsx
+++ ivan-san-nicolas/Challenges/github-fullstack/frontend/src/components/Landing/Landing.jsx
@@ -1,7 +1,37 @@
+/* eslint-disable no-debugger */
 import React from 'react';
+import { connect } from 'react-redux';
+import { PropTypes } from 'prop-types';
+import { loadProjects } from '../../redux/actions/gitActions';
 
-function Landing() {
-  return <h1>Landing works!</h1>;
+function Landing({ projectList, dispatch }) {
+  debugger;
+  if (projectList.length === 0) {
+    dispatch(loadProjects());
+  }
+  return (
+    <div>
+      {projectList ? projectList.map((project) => <p>{project.name}</p>) : (
+        <h2>There are no projects :/</h2>
+      )}
+    </div>
+  );
 }
 
-export default Landing;
+function mapStateToProps({ gitReducer }) {
+  debugger;
+  return {
+    projectList: gitReducer.projectArray,
+  };
+}
+
+Landing.propTypes = {
+  projectList: PropTypes.arrayOf(PropTypes.object),
+  dispatch: PropTypes.func.isRequired,
+};
+
+Landing.defaultProps = {
+  projectList: {},
+};
+
+export default connect(mapStateToProps)(Landing);
