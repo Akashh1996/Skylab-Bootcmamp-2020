6af6a59c04333486f6024a236766da7193ce836f
algmebe1 <67826380+algmebe1@users.noreply.github.com>
2020-11-21 01:59:26 +0100

    Delete userListReducer.js

--- david-mato/populate/client/src/redux/reducers/userListReducer.js
+++
@@ -1,8 +0,0 @@
-export default function userListReducer(state = [], action) {
-  switch (action.type) {
-    case 'LOAD_USERLIST':
-      return [...state, ...action.userList];
-    default:
-      return state;
-  }
-}
