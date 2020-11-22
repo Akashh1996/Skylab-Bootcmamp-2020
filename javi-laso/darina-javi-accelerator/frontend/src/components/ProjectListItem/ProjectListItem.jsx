7f8ba7bf07755d507d19d570b0427030aa0548f3
Jheavi <javierlaso213@hotmail.com>
2020-11-23 00:06:58 +0100

    added most of test coverage, added link to card and removed button to log

--- javi-laso/darina-javi-accelerator/frontend/src/components/ProjectListItem/ProjectListItem.jsx
+++ javi-laso/darina-javi-accelerator/frontend/src/components/ProjectListItem/ProjectListItem.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import './ProjectListItem.css';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
-import { requestProjects, requestProjectDetail } from '../../redux/actions/project-actions';
+import { requestProjects } from '../../redux/actions/project-actions';
 import Tags from '../Tags/Tags';
+import './ProjectListItem.css';
 
 function ProjectListItem({ projects, dispatch }) {
   if (!projects || !projects.length) {
@@ -15,12 +14,12 @@ function ProjectListItem({ projects, dispatch }) {
   return (
     <>
       <main className="project-list-container">
-        <h2 className="mb-4">Search your job</h2>
+        <h2 className="mb-4 item-title">Search your job</h2>
         <ul className="project-list">
           {projects && projects.length > 0 && projects.map((project) => (
 
-            <li key={project._id}>
-              <Card>
+            <li key={performance.now() * Math.random()} className="project-link">
+              <Card as={Link} to={`/projects/${project._id}`}>
                 <Card.Header className="project-header">{project.name}</Card.Header>
                 <Card.Body className="d-flex flex-column flex-md-row align-items-center">
                   <Card.Img
@@ -41,15 +40,6 @@ function ProjectListItem({ projects, dispatch }) {
                       <Card.Text className="project-price">{`${project.price}€`}</Card.Text>
                       <Card.Text className="collaborators">{`Actual collaborators: ${project.participants.length}`}</Card.Text>
                     </div>
-                    <Button
-                      as={Link}
-                      to={`/projects/${project._id}`}
-                      onClick={() => dispatch(requestProjectDetail(project._id))}
-                      variant="light"
-                    >
-                      Logged as User
-
-                    </Button>
                   </div>
 
                   {' '}
