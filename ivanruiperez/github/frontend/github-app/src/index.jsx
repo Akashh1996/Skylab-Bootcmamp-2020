5183d29b852061dc5de0cedf43ac59e81500d736
Eric Martinez <martinezjimenezeric@gmail.com>
2020-11-22 17:53:23 +0100

    add detail

--- ivanruiperez/github/frontend/github-app/src/index.jsx
+++ ivanruiperez/github/frontend/github-app/src/index.jsx
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import configureStore from './redux/configureStore';
 import Header from './components/Header/Header';
 import ProjectList from './components/ProjectList/ProjectList';
+import ProjectDetail from './components/Detail/ProjectDetail';
 
 const userStore = configureStore();
 
@@ -15,6 +16,7 @@ ReactDOM.render(
         <Header />
         <Switch>
           <Route path="/" exact component={ProjectList} />
+          <Route path="/project/:projectId" exact component={ProjectDetail} />
         </Switch>
       </BrowserRouter>
     </Provider>
