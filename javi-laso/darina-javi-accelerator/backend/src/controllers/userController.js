5e9f647ef9e03a58d20b8abc49b6aa18d21b13d0
Jheavi <javierlaso213@hotmail.com>
2020-11-22 12:09:21 +0100

    project list layout

--- javi-laso/darina-javi-accelerator/backend/src/controllers/userController.js
+++ javi-laso/darina-javi-accelerator/backend/src/controllers/userController.js
@@ -1,18 +1,18 @@
 function userController(userSchema) {
   function getUsersMethod(req, res) {
     const query = {};
-    const getCallBack = (usersError, users) => (
-      usersError ? res.send(usersError) : res.send(users)
+    const getCallBack = (projectsError, projects) => (
+      projectsError ? res.send(projectsError) : res.send(projects)
     );
     userSchema.find(query, getCallBack);
   }
 
   function postUserMethod(req, res) {
-    const user = req.body;
+    const project = req.body;
     const postCallback = (postError, newUser) => (
       postError ? res.send(postError) : res.send(newUser)
     );
-    userSchema.create(user, postCallback);
+    userSchema.create(project, postCallback);
   }
 
   return { getUsersMethod, postUserMethod };
--- javi-laso/darina-javi-accelerator/backend/src/routers/oauthRouter.js
+++ javi-laso/darina-javi-accelerator/backend/src/routers/oauthRouter.js
@@ -2,29 +2,41 @@ const { Router } = require('express');
 const axios = require('axios');
 const { clientId, clientSecret } = require('../constants/constants');
 
-let token;
+const apiGithubUserUrl = 'https://api.github.com/user';
 
-function oauthRouter() {
+function oauthRouter(userSchema) {
   const router = Router();
 
   router.route('/')
     .get(async (req, res) => {
+      console.log(req.query);
       const body = {
         client_id: clientId,
         client_secret: clientSecret,
         code: req.query.code,
       };
-      console.log('Response body: ', req.query);
       const opts = { headers: { accept: 'application/json' } };
       const response = await axios.post('https://github.com/login/oauth/access_token', body, opts);
-      console.log(response.data);
 
       try {
         const _token = await response.data.access_token;
 
-        token = _token;
-        console.log(token);
-        res.send(token);
+        const headers = {
+          Authorization: `token ${_token}`,
+        };
+
+        const { data } = await axios.get(apiGithubUserUrl, { headers });
+
+        userSchema.findOneAndUpdate({ gitUser: data.login }, {
+          name: data.name,
+          gitUser: data.login,
+          gitPicture: data.avatar_url,
+          token: _token,
+        },
+        { upsert: true },
+        (error, user) => (
+          error ? res.send(error) : res.send(user)
+        ));
       } catch (error) {
         res.send(error);
       }
