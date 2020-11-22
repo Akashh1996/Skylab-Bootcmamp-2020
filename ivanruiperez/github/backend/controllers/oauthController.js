827d9ef2d186a4f80a3c423688053666107ffc98
Ivan Ruiperez Parra <ivan.ruiperez@gmail.com>
2020-11-22 20:38:52 +0100

    add css components front and test detailController backend

--- ivanruiperez/github/backend/controllers/oauthController.js
+++ ivanruiperez/github/backend/controllers/oauthController.js
@@ -14,8 +14,6 @@ function oAuthController(User) {
     const response = await axios.post('https://github.com/login/oauth/access_token', body, opts);
     try {
       const _token = await response.data.access_token;
-      // eslint-disable-next-line no-console
-      console.log(_token);
       const user = await axios({
         url: 'https://api.github.com/user',
         headers: {
--- ivanruiperez/github/frontend/github-app/src/components/ProjectList/ProjectList.jsx
+++ ivanruiperez/github/frontend/github-app/src/components/ProjectList/ProjectList.jsx
@@ -8,6 +8,7 @@ import { PropTypes } from 'prop-types';
 import { loadProjects } from '../../redux/actions/projectActions';
 import './ProjectList.css';
 
+const img = 'https://image.flaticon.com/icons/png/512/69/69926.png';
 function ProjectList({ projectList, dispatch }) {
   useEffect(() => {
     if (!projectList || projectList.length === 0) {
@@ -20,17 +21,32 @@ function ProjectList({ projectList, dispatch }) {
       <h1>Projects</h1>
       {projectList && projectList?.length
         && projectList.map((project) => (
-          <Link to={`/project/${project._id}`}>
-            <li key={`${project._id}_li`}>
-              <span key={`${project._id}_name`}>{project.name}</span>
-              <span key={`${project._id}_descrip`}>{project.description}</span>
-              <span key={`${project._id}_creator`}>
-                Creator:
-                {' '}
-                {project.creator}
-              </span>
-            </li>
-          </Link>
+          <div className="projectBox">
+            <div className="projectsImg"><img src={img} alt="" /></div>
+            <div className="projects">
+              <Link to={`/project/${project._id}`}>
+                <li key={`${project._id}_li`}>
+                  <span key={`${project._id}_name`}>
+                    Project name:
+                    {' '}
+                    <br />
+                    {project.name}
+                  </span>
+                  <span key={`${project._id}_descrip`}>
+                    description:
+                    {' '}
+                    <br />
+                    {project.description}
+                  </span>
+                  <span key={`${project._id}_creator`}>
+                    Creator:
+                    {' '}
+                    {project.creator}
+                  </span>
+                </li>
+              </Link>
+            </div>
+          </div>
         ))}
     </main>
   );
