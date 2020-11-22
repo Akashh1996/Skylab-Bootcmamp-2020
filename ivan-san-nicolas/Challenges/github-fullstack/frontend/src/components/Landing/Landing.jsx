c7c0570988bc577dbad30c644819b684545bb658
Ivan San <ivohan8@gmail.com>
2020-11-22 02:41:41 +0100

    add and remove user from participants working but still in progress

--- ivan-san-nicolas/Challenges/github-fullstack/frontend/src/components/Landing/Landing.jsx
+++ ivan-san-nicolas/Challenges/github-fullstack/frontend/src/components/Landing/Landing.jsx
@@ -15,20 +15,32 @@ function Landing({ projectList, dispatch }) {
 
   const user = {
     _id: '5fb864a81a3b513066e3171a',
-    name: 'Ivan',
+    name: 'Mario',
     profilePic: 'https://trello-attachments.s3.amazonaws.com/5f8ca3639574d3550b3ad495/5fb96768be4f1e1ab90b1a74/d047e04f9babccb3f0d3503745f4e8ad/47951339.png',
     githubUrl: 'https://github.com/Ivansannicolas?tab=repositories',
   };
 
   function addUserToParticipants(newParticipant, project) {
-    debugger;
-    project.participants.push(newParticipant._id);
-    dispatch(updateProject(project._id, project));
+    const updatedProject = { ...project };
+    let wasParticipant = false;
+    project.participants.forEach((participant) => {
+      if (participant._id === newParticipant._id) {
+        updatedProject.participants = project.participants.filter(
+          (filterParticipant) => filterParticipant._id !== newParticipant._id,
+        );
+        wasParticipant = true;
+      }
+    });
+
+    if (!wasParticipant) {
+      updatedProject.participants.push(newParticipant._id);
+    }
+    dispatch(updateProject(project._id, updatedProject));
   }
 
   return (
     <section className="landing">
-      {projectList ? projectList.map((project) => (
+      {projectList.length ? projectList.map((project) => (
         <div className="landing__project">
           <div className="landing__project__top">
             <div className="landing__project__top__details">
