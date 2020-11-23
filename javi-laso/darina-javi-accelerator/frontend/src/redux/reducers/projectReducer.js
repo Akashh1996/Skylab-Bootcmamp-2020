0eecc5553996a343910b3ddc88eee3964de0de95
Jheavi <javierlaso213@hotmail.com>
2020-11-23 16:00:04 +0100

    changed projectReducer order of switch cases

--- javi-laso/darina-javi-accelerator/frontend/src/redux/reducers/projectReducer.js
+++ javi-laso/darina-javi-accelerator/frontend/src/redux/reducers/projectReducer.js
@@ -4,12 +4,11 @@ export default function projectsReducer(state = {}, action) {
   switch (action.type) {
     case actionTypes.LOAD_PROJECTS:
       return { ...state, projects: action.projects };
-
+    case actionTypes.LOAD_PROJECTS_ERROR:
+      return { ...state, errorProject: action.projectsError };
     case actionTypes.LOAD_PROJECT:
       return { ...state, project: action.project };
     case actionTypes.LOAD_PROJECT_ERROR:
-      return { ...state, errorProject: action.projectsError };
-    case actionTypes.LOAD_PROJECTS_ERROR:
       return { ...state, errorProject: action.projectsError };
     case actionTypes.CREATE_PROJECT:
       return { ...state, projects: [...state.projects, action.newProject] };
