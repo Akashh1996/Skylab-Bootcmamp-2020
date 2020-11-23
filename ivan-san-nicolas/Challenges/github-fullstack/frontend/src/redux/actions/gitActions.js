b96eb7d8648f711275afa605325e574aef8435b9
Ivan San <ivohan8@gmail.com>
2020-11-23 15:47:38 +0100

    addUserToParticipants function moved from Landing component to gitActions

--- ivan-san-nicolas/Challenges/github-fullstack/frontend/src/redux/actions/gitActions.js
+++ ivan-san-nicolas/Challenges/github-fullstack/frontend/src/redux/actions/gitActions.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-underscore-dangle */
 import axios from 'axios';
 import actionTypes from './actionTypes';
 
@@ -90,4 +92,41 @@ export function addProject(name, description, url, participants, creator) {
       dispatch(loadError(error));
     }
   };
+}
+
+export function addUserToParticipants(
+  newParticipant, actualProject, actualProjects, projectButton, dispatch,
+) {
+  const updatedProject = { ...actualProject };
+  const updatedProjects = [...actualProjects];
+  const subscribeButton = document.getElementById(projectButton.id);
+
+  let wasParticipant = false;
+
+  actualProject.participants.forEach((participant) => {
+    if (participant._id === newParticipant._id) {
+      updatedProject.participants = actualProject.participants.filter(
+        (filterParticipant) => filterParticipant._id !== newParticipant._id,
+      );
+      wasParticipant = true;
+    }
+  });
+
+  if (!wasParticipant) {
+    updatedProject.participants.push(newParticipant._id);
+    subscribeButton.style.backgroundColor = 'red';
+    subscribeButton.textContent = 'Unsubscribe';
+  } else {
+    subscribeButton.style.backgroundColor = 'rgb(136, 202, 38)';
+    subscribeButton.textContent = 'Subscribe';
+  }
+
+  updatedProjects.forEach((project) => {
+    if (project._id === updatedProject._id) {
+      project = updatedProject;
+    }
+  });
+
+  dispatch(updateProject(actualProject._id, updatedProject));
+  dispatch(loadProjects());
 }
