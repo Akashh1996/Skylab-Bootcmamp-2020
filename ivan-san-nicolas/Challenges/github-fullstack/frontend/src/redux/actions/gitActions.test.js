f7d4f943794604d279ed866f9223a343b5324f4c
Alberto <algmebe1@gmail.com>
2020-11-24 00:19:35 +0100

    continuing actions testing...

--- ivan-san-nicolas/Challenges/github-fullstack/frontend/src/redux/actions/gitActions.test.js
+++ ivan-san-nicolas/Challenges/github-fullstack/frontend/src/redux/actions/gitActions.test.js
@@ -9,22 +9,61 @@ jest.mock('axios');
 const mockStore = configureMockStore([thunk]);
 
 describe('gitActions', () => {
-  let store = null;
-  let fakeData = null;
-  let fakeError = null;
+  describe('functions with store dependancy', () => {
+    let store = null;
+    let fakeData = null;
 
-  beforeEach(() => {
-    store = mockStore();
-    fakeData = { data: 'projects' };
-    fakeError = 'error';
-  });
-  afterEach(() => {
-    jest.resetAllMocks();
-  });
+    beforeEach(() => {
+      store = mockStore();
+      fakeData = { data: 'projects' };
+    });
+    afterEach(() => {
+      jest.resetAllMocks();
+    });
 
-  test('should call dispatch when successful try', async () => {
-    axios.get = jest.fn().mockResolvedValueOnce(fakeData);
-    await store.dispatch(gitActions.loadProjects());
-    expect(store.getActions()[0].type).toBe(actionTypes.LOAD_PROJECTS);
+    test('should call loadProjects function and return an object with propery data', async () => {
+      axios.get = jest.fn().mockResolvedValueOnce(fakeData);
+      await store.dispatch(gitActions.loadProjects());
+      expect(store.getActions()[0].type).toBe(actionTypes.LOAD_PROJECTS);
+    });
+    test('should call loadProjects function and return an error', async () => {
+      axios.get = jest.fn().mockRejectedValueOnce(fakeData);
+      await store.dispatch(gitActions.loadProjects());
+      expect(store.getActions()[0].type).toBe(actionTypes.LOAD_ERROR);
+    });
+
+    test('should call loadProject function and return an object with propery data', async () => {
+      axios.get = jest.fn().mockResolvedValueOnce(fakeData);
+      await store.dispatch(gitActions.loadProject());
+      expect(store.getActions()[0].type).toBe(actionTypes.LOAD_PROJECT);
+    });
+    test('should call loadProject function and return an error', async () => {
+      axios.get = jest.fn().mockRejectedValueOnce(fakeData);
+      await store.dispatch(gitActions.loadProject());
+      expect(store.getActions()[0].type).toBe(actionTypes.LOAD_ERROR);
+    });
+
+    test('should call loadUsers function and return an object with propery data', async () => {
+      axios.get = jest.fn().mockResolvedValueOnce(fakeData);
+      await store.dispatch(gitActions.loadUsers());
+      expect(store.getActions()[0].type).toBe(actionTypes.LOAD_USERS);
+    });
+    test('should call loadUsers function and return an error', async () => {
+      axios.get = jest.fn().mockRejectedValueOnce(fakeData);
+      await store.dispatch(gitActions.loadUsers());
+      expect(store.getActions()[0].type).toBe(actionTypes.LOAD_ERROR);
+    });
+
+    test('should call updateProject function and return an error', async () => {
+      axios.patch = jest.fn().mockRejectedValueOnce(fakeData);
+      await store.dispatch(gitActions.updateProject());
+      expect(store.getActions()[0].type).toBe(actionTypes.LOAD_ERROR);
+    });
+
+    test('should call addProject function and return an error', async () => {
+      axios.post = jest.fn().mockRejectedValueOnce(fakeData);
+      await store.dispatch(gitActions.addProject());
+      expect(store.getActions()[0].type).toBe(actionTypes.LOAD_ERROR);
+    });
   });
 });
