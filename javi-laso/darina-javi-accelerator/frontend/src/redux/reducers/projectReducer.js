9f60090af93ef9bd92c4894e3031110fb4a18fc0
Jheavi <javierlaso213@hotmail.com>
2020-11-24 23:52:08 +0100

    created reducer cases to apply filters and reset filters

--- javi-laso/darina-javi-accelerator/frontend/src/redux/reducers/projectReducer.js
+++ javi-laso/darina-javi-accelerator/frontend/src/redux/reducers/projectReducer.js
@@ -3,7 +3,10 @@ import actionTypes from '../actions/actionTypes';
 export default function projectsReducer(state = {}, action) {
   switch (action.type) {
     case actionTypes.LOAD_PROJECTS:
-      return { ...state, projects: action.projects };
+      const categories = [...new Set(action.projects.map((project) => (
+        project.categories.map((category) => category.toLowerCase())
+      )).flat())];
+      return { ...state, projects: action.projects, categories };
     case actionTypes.LOAD_PROJECT:
       return { ...state, project: action.project };
     case actionTypes.LOAD_PROJECT_ERROR:
@@ -12,6 +15,15 @@ export default function projectsReducer(state = {}, action) {
       return { ...state, errorProject: action.projectsError };
     case actionTypes.CREATE_PROJECT:
       return { ...state, projects: [...state.projects, action.newProject] };
+    case actionTypes.APPLY_FILTERS:
+      const filteredProjects = state.projects.filter((project) => (
+        action.values.every((value) => (
+          project.categories.includes(value)
+        ))
+      ));
+      return { ...state, filteredList: filteredProjects };
+    case actionTypes.RESET_FILTERS:
+      return { ...state, filteredList: null };
     default:
       return state;
   }
