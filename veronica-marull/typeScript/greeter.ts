921aca11761fce889cba7b9f1533ccbf8613f896
Verónica Marull <vero@dieresys.com.ar>
2020-11-25 09:36:59 +0100

    typescript

---
+++ veronica-marull/typeScript/greeter.ts
@@ -0,0 +1,25 @@
+class Student {
+    fullName: string;
+    constructor(
+      public firstName: string,
+      public middleInitial: string,
+      public lastName: string
+    ) {
+      this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+  }
+  
+  interface Person {
+    firstName: string;
+    lastName: string;
+  }
+  
+  function greeter(person: Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+  }
+  
+  let userName = new Student("Jane", "M.", "User");
+  
+  document.body.textContent = greeter(userName);
+
+  
