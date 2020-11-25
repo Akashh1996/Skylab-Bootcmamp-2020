a9bd0952894c75f371118a277e7907844e95d41c
Ivan San <ivohan8@gmail.com>
2020-11-26 00:53:01 +0100

    added some extra styles

--- ivan-san-nicolas/Challenges/tour-of-heroes-angular/src/app/hero.service.ts
+++ ivan-san-nicolas/Challenges/tour-of-heroes-angular/src/app/hero.service.ts
@@ -10,7 +10,6 @@ import { MessageService } from './message.service';
 export class HeroService {
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
   constructor(private messageService: MessageService) { }
