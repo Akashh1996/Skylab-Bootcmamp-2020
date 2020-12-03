074c52e0319893d49e5a639353f2c0b664ec2b9e
Akash Sapkota <akashh.sapkota@gmail.com>
2020-12-04 00:37:08 +0100

    angular toh

--- akash-sapkota/angular-tour-of-heroes/src/app/hero.service.ts
+++ akash-sapkota/angular-tour-of-heroes/src/app/hero.service.ts
@@ -6,9 +6,7 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable({ providedIn: 'root' })
 export class HeroService {
 
   constructor(private messageService: MessageService) { }
@@ -18,4 +16,12 @@ export class HeroService {
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
+
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
+
+  
 }
